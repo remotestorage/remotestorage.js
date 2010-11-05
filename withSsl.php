ef502f2abeaedd842629ec91a0d3a54cf030757b
Michiel@unhosted <michiel@unhosted.org>
2010-11-05 01:08:00 +0100

    still not working, but improving url enconding

--- withSsl.php
+++ withSsl.php
@@ -1,38 +1,35 @@
 <?php
 class Encryptor {
+	const KEY_DIR = '/path/to/';
 	private $keyPair;
 	private $pubKey;
 
 	function createKeyPair($passphrase) {
 		$keyPair = openssl_pkey_new(array('private_key_bits' => 1024, 'private_key_type' => OPENSSL_KEYTYPE_RSA));
 		$pathToKey = 'asdf';//.substr($keyDetails['key'], 27, 20);
-		openssl_pkey_export_to_file($keyPair, $pathToKey.'.pair', $passphrase);
+		openssl_pkey_export_to_file($keyPair, self::KEY_DIR.$pathToKey.'.pair', $passphrase);
 		$keyDetails = openssl_pkey_get_details($keyPair);
-		file_put_contents($pathToKey.'.pub', $keyDetails['key']);
+		file_put_contents(self::KEY_DIR.$pathToKey.'.pub', $keyDetails['key']);
 		$this->keyPair = $keyPair;
 		$this->pubKey = $keyDetails['key'];
 		return $pathToKey;
 	}
 	private function getPrivate($pathToKey, $passphrase) {
-		echo 'getting private:';
-		var_dump(file_get_contents($pathToKey.'.pair'));
-		return openssl_pkey_get_private($pathToKey.'.pair', $passphrase);
+		var_dump(self::KEY_DIR.$pathToKey.'.pair');
+		return openssl_pkey_get_private(self::KEY_DIR.$pathToKey.'.pair', $passphrase);
 	}
 	private function getPublic($pathToKey) {
-		echo 'getting public:';
-		var_dump(file_get_contents($pathToKey.'.pub'));
-		return openssl_pkey_get_public($pathToKey.'.pub');
+		return file_get_contents(self::KEY_DIR.$pathToKey.'.pub');
 	}
 	function decrypt($pathToKey, $passphrase, $encryptedData) {
 		$privateKey = $this->getPrivate($pathToKey, $passphrase);
+var_dump($privateKey);//something's still going wrong here
 		openssl_private_decrypt($encryptedData, $sensitiveData, $privateKey);
 		return $sensitiveData;
 	}
 	function encrypt($pathToKey, $sensitiveData) {
 		$pubKey = $this->getPublic($pathToKey);
-var_dump($pubKey);
 		openssl_public_encrypt($sensitiveData, $encryptedData, $pubKey);
-var_dump($encryptedData);
 		return $encryptedData;
 	}
 }
@@ -48,18 +45,20 @@ class UnhostedProxy {
 		return $this->encryptor->createKeyPair($this->pwd);
 	}
 	function send($ch, $key, $clearData) {
-		$encrypted = $this->encryptor->encrypt($keyPair, $clearData);
-		$cmd = "curl http://localhost/untrustedStore.php?cmd=SET&channel=$ch&key=$key&value=$encrypted";
-		var_dump($cmd);
+		$encrypted = base64_encode($this->encryptor->encrypt($ch, $clearData));
+		var_dump($encrypted);
+		echo ' urlencoded: ';
+		$encrypted = urlencode($encrypted);
+		var_dump($encrypted);
+		$cmd = "curl http://localhost/git/unhosted/untrustedStore.php?cmd=SET\&channel=$ch\&key=$key\&value=$encrypted";
 		$ret = `$cmd`;
-		var_dump($ret);
 	}
 	function receive($ch, $key) {
-		$cmd = "curl http://localhost/untrustedStore.php?cmd=GET&channel=$ch&key=$key";
-		var_dump($cmd);
+		$cmd = "curl http://localhost/git/unhosted/untrustedStore.php?cmd=GET\&channel=$ch\&key=$key";
 		$ret = `$cmd`;
+		echo ' en: ';
 		var_dump($ret);
-		return $this->encryptor->decrypt($keyPair, $this->pwd, $ret);
+		return $this->encryptor->decrypt($ch, $this->pwd, base64_decode($ret));
 	}
 }
 
