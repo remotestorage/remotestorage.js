f3a344d7b93f6d936edfb650e64ee1ca319c0c62
Michiel@unhosted <michiel@unhosted.org>
2010-11-27 12:42:31 +0800

    deleting files that were moved to v0.0/

--- myCloudGet.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set('display_errors', TRUE);
-require_once 'UnhostedStorage.php';
-$backend = new UnhostedStorage();
-//echo $backend->get($_POST['path']);
-echo "getting {$_POST['path']}<br/>";
-var_dump($backend->get($_POST['path']));
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-php send.php > msg.php
-php receive.php
