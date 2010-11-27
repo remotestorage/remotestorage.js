9ae31885412af8cfe7e653aea3cc765562f84ff3
Michiel@unhosted <michiel@unhosted.org>
2010-11-27 12:38:26 +0800

    added SET command

--- v0.1/clientside/unhosted.js
+++ v0.1/clientside/unhosted.js
@@ -1,25 +1,35 @@
 function Unhosted() {
 	var obj={};
 	//private:
-	function sendGetCommand(cmd) {
+	function makeKey(app, pub, cloud, path) {
+		return app + encodeURIComponent("+") + pub + "@" + cloud + "/" + path;
+	}
+	function makeSetCommand(key, value) {
+		return JSON.stringify({'method':'SET', 'key':key, 'value':value});
+	}
+	function makeGetCommand(key) {
+		return JSON.stringify({'method':'GET', 'key':key});
+	}
+	function makePubSign(pri, cmd) {//TODO: implement
+		return 'Y8N'+encodeURIComponent("+")+'vV3B92iZvhMEPcfXFOsPDvvSUNU514Oaf'+encodeURIComponent("+")+'oKpCOxdniminxNQB5z/NjEqZPDwZphI7EBD7fbIIJGJsqYLWuraA1HjE3axW8bYw1SZ8dZcCDj6iTX2ZpRS/hMCJ4H5iIzM1RdPHN0p5PGQZMm3nubpZW1THzSakXNA6Qui7g=';
+	}
+	function sendPost(post) {
 		xmlhttp=new XMLHttpRequest();
 		//xmlhttp.open("POST","http://demo.unhosted.org/",false);
 		xmlhttp.open("POST","http://demo.unhosted.org/git/unhosted/v0.1/serverside/unhosted.php",false);
 		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		xmlhttp.send("protocol=UJ/0.1&cmd="+cmd);
+		xmlhttp.send(post);
 		return xmlhttp.responseText;
-	}
-	function makeGetCommand(key) {
-		var cmd = {
-			'method':'GET',
-			'key':key,
-			}
-		return JSON.stringify(cmd); 
 	}
 	//public:
 	obj.get = function get(app, pub, cloud, path) {
-		key = app + encodeURIComponent("+") + pub + "@" + cloud + "/" + path;
-		return sendGetCommand(makeGetCommand(key));
+		var cmd = makeGetCommand(makeKey(app, pub, cloud, path));
+		return sendPost("protocol=UJ/0.1&cmd="+cmd);
+	}
+	obj.set = function set(app, pub, cloud, path, value) {
+		var cmd = makeSetCommand(makeKey(app, pub, cloud, path), value);
+		var PubSign = makePubSign('pri', cmd);
+		return sendPost("protocol=UJ/0.1&cmd="+cmd+"&PubSign="+PubSign);
 	}
 	//
 	return obj;
