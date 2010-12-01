848def2f92a935c77b70767ab1bbe0c3c4e4cf7c
Michiel@unhosted <michiel@unhosted.org>
2010-12-01 15:27:24 +0800

    moved rng stuff out of unhosted.js and into separate genkey.js

--- v0.1/clientside/unhosted/unhosted.js
+++ v0.1/clientside/unhosted/unhosted.js
@@ -65,43 +65,7 @@ function Unhosted() {
 		return (x.modPowInt(parseInt("10001", 16), n).toString(16).replace(/^1f+00/, '') == sha1.hex(cmd));
 	}
 
-	// Generate a new random private key B bits long, using public expt E
-	function RSAGenerate() {
-	    var rng = new SecureRandom();
-	    var qs = 512>>1;
-	    this.e = parseInt("10001",16);
-	    var ee = new BigInteger("10001",16);
-	    for(;;) {
-	        for(;;) {
-	            p = new BigInteger(512-qs,1,rng);
-	            if(p.subtract(BigInteger.ONE).gcd(ee).compareTo(BigInteger.ONE) == 0 && p.isProbablePrime(10)) break;
-	        }
-	        for(;;) {
-        	    q = new BigInteger(qs,1,rng);
-	            if(q.subtract(BigInteger.ONE).gcd(ee).compareTo(BigInteger.ONE) == 0 && q.isProbablePrime(10)) break;
-        	}
-	        if(p.compareTo(q) <= 0) {
-	            var t = p;
-	            p = q;
-	            q = t;
-	        }
-	        var p1 = p.subtract(BigInteger.ONE);
-	        var q1 = q.subtract(BigInteger.ONE);
-	        var phi = p1.multiply(q1);
-	        if(phi.gcd(ee).compareTo(BigInteger.ONE) == 0) {
-		    bnSeskey=new BigInteger(128,1,rng);//rijndael function we use uses a 128-bit key
-	            return {"p":p.toString(16),"q":q.toString(16),"pubkey":p.multiply(q).toString(16), "seskey":bnSeskey.toString(16)};
-	        }
-	    }
-	}
 	//public:
-	obj.createPub = function(nick, cloud, token) {
-		key = RSAGenerate();
-		key[cloud]=cloud;
-		key[token]=token;
-		keys[nick]=key;
-		return key;
-	}
 	obj.importPub = function(writeCaps, nick) {
 		keys[nick]=writeCaps;
 	}
