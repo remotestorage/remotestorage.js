8f1490c81a37527ee53a19b4f016e01b0522b920
Michiel@unhosted <michiel@unhosted.org>
2010-12-11 02:58:30 +0800

    starting trunk/branch organization

--- v0.0/fetchMyCloudChannelKeys.php
+++
@@ -1,3 +0,0 @@
-<?php
-$POST=unserialize(file_get_contents('/tmp/tmp.txt'));
-echo json_encode($POST);
--- v0.0/myCloudCreateChannel.php
+++
@@ -1,3 +0,0 @@
-<?php
-file_put_contents('/tmp/tmp.txt', serialize($_POST));
-var_dump($_POST);
