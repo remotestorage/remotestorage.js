b1fcc3416e92d4fb01283f7c9817c27d1f5138b9
Michiel de Jong <michiel@unhosted.org>
2011-04-27 15:12:43 +0200

    more improvements, localStorage 'unhosted' obj

--- www/unhosted.js
+++ www/unhosted.js
@@ -1,12 +1,13 @@
-var appBaseUrl = "https://www.myfavouritesandwich.org/";
+var appBaseUrl = "http://www.myfavouritesandwich.org/";
+//var appBaseUrl = "https://www.myfavouritesandwich.org/";
 var loginUrl = appBaseUrl + "login.html";
 
-// app state:
-// ===========
-// unhosted::userName
-// unhosted::davPwd
-// unhosted::cryptoPwd
-// unhosted::davBaseUrl
+// app state shared with login.html:
+// =================================
+// localStorage::"unhosted".userName
+// localStorage::"unhosted".davAuth
+// localStorage::"unhosted".cryptoPwd
+// localStorage::"unhosted".davBaseUrl
 
 
   /////////
@@ -15,15 +16,10 @@ var loginUrl = appBaseUrl + "login.html";
 
 var DAV = function() {
 	var dav = {}
-	var makeBasicAuth = function(user, password) {
-		var tok = user + ':' + password;
-		var hash = Base64.encode(tok);
-		return "Basic " + hash;
-	}
 	keyToUrl = function(key) {
-		var userNameParts = localStorage.getItem("unhosted::userName").split("@");
+		var userNameParts = localStorage.getItem("unhosted").userName.split("@");
 		var resource = document.domain;
-		var url = localStorage.getItem("unhosted::davBaseUrl")
+		var url = localStorage.getItem("unhosted").davBaseUrl
 			+"webdav/"+userNameParts[1]
 			+"/"+userNameParts[0]
 			+"/"+resource
@@ -31,9 +27,12 @@ var DAV = function() {
 		return url;
 	}
 	dav.get = function(key) {
-		var xhr = new XMLHttpRequest();
+return null;
+return "{ingredients:['', '']}";	
+
+	var xhr = new XMLHttpRequest();
 		xhr.open("GET", keyToUrl(key), false);
-		xhr.setRequestHeader("Authorization", makeBasicAuth(localStorage.getItem("unhosted::userName"), localStorage.getItem("unhosted::davPwd")));
+		xhr.setRequestHeader("Authorization", localStorage.getItem("unhosted").davAuth);
 		xhr.withCredentials = "true";
 		xhr.send();
 		if(xhr.status == 200) {
@@ -47,7 +46,7 @@ var DAV = function() {
 	dav.put = function(key, text) {
 		var xhr = new XMLHttpRequest();
 		xhr.open("PUT", keyToUrl(key), false);
-		xhr.setRequestHeader("Authorization", makeBasicAuth(localStorage.getItem("unhosted::userName"), localStorage.getItem("unhosted::davPwd")));
+		xhr.setRequestHeader("Authorization", localStorage.getItem("unhosted").davAuth);
 		xhr.withCredentials = "true";
 		xhr.send(text);
 		if(xhr.status != 200 && xhr.status != 201 && xhr.status != 204) {
@@ -66,21 +65,23 @@ var Unhosted = function() {
 	var unhosted = {};
 	var dav = DAV();
 	unhosted.connect = function() {
-		if(!localStorage.getItem("unhosted::userName")) {
-			window.location(loginUrl);
+		if(!localStorage.getItem("unhosted").userName) {
+			window.location = loginUrl;
 		}
 	}
 	unhosted.getUserName = function() {
-		return localStorage.getItem("unhosted::userName");
+		return localStorage.getItem("unhosted").userName;
 	}
 	unhosted.get = function(key) {
-		return JSON.parse(sjcl.decrypt(localStorage.getItem("unhosted::cryptoPwd"), dav.get(key)));
+		return JSON.parse(dav.get(key));
+		//return JSON.parse(sjcl.decrypt(localStorage.getItem("unhosted").cryptoPwd, dav.get(key)));
 	}
 	unhosted.set = function(key, value) {
-		dav.put(key, sjcl.encrypt(localStorage.getItem("unhosted::cryptoPwd"), JSON.stringify(value)));
+		dav.put(key, JSON.stringify(value));
+		//dav.put(key, sjcl.encrypt(localStorage.getItem("unhosted").cryptoPwd, JSON.stringify(value)));
 	}
 	unhosted.close = function() {
-		localStorage.removeItem("unhosted::userName");
+		localStorage.removeItem("unhosted").userName;
 	}
 
 	return unhosted;
