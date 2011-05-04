e0b24c4c6e18dea41d8c7c8cf325bc29d379afe0
Michiel de Jong <michiel@unhosted.org>
2011-05-04 22:20:03 +0200

    debugging

--- www/unhosted/do.php
+++ www/unhosted/do.php
@@ -1,9 +1,40 @@
 <?php
 require_once 'unhosted.php';
-$unhostedAccount = new UnhostedAccount($_GET["userAddress"], $_GET["pwd"]);
-switch($_GET["action"]) {
-	case "getWallet": echo $unhostedAccount->getWallet($_GET["dataScope"]);break;
-	case "registerLocal": echo $unhostedAccount->registerHosted();break;
-	case "registerWallet": echo $unhostedAccount->registerWallet($_GET["davBaseUrl"], $_GET["davToken"], $_GET["dataScope"]); break;
-	case "addApp": echo $unhostedAccount->addApp($_GET["dataScope"]);break;
+function getString($paramName) {
+        if(!isset($_GET[$paramName])) {
+                die("Parameter $paramName not specified");
+        }
+        return $_GET[$paramName];
 }
+function getUrl($paramName) {
+        $url = getString($paramName);
+        if(!preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url)) {
+                die("Parameter $paramName should be a valid URL");
+        }
+	return $url;
+}
+function getUserAddress($paramName) {
+        $userAddress = getString($paramName);
+        if(!preg_match('|^[a-z0-9-]+(.[a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*$|i', $userAddress)) {
+                die("Parameter $paramName should be a valid user address");
+        }
+	return $userAddress;
+}
+
+$unhostedAccount = new UnhostedAccount(getUserAddress("userAddress"), getString("pwd"));
+
+switch(getString("action")) {
+        case "getWallet":
+                echo $unhostedAccount->getWallet(getUrl("dataScope"));
+                break;
+        case "registerLocal":
+                echo $unhostedAccount->registerHosted();
+                break;
+        case "registerWallet":
+                echo $unhostedAccount->registerWallet(getUrl("davBaseUrl"), getString("davToken"), getUrl("dataScope"));
+                break;
+        case "addApp":
+                echo $unhostedAccount->addApp(getUrl("dataScope"));
+                break;
+}
+
--- www/unhosted/unhosted.php
+++ www/unhosted/unhosted.php
@@ -6,10 +6,9 @@ class UnhostedAccount {
 	private $userAddress, $userName, $userDomain, $pwd;
 	function __construct($userAddress, $pwd) {
 		$this->userAddress = $userAddress;
-		list($this->userName, $this->userDomain) = explode("@", $userAddress);
-		if((!strlen($this->userName)) || (!strlen($this->userDomain))) {
-			die("'$userAddress' not a valid user address");
-		}
+		list($this->userName, $this->userDomain) = explode('@', $userAddress);
+echo($this->userName." is username");
+echo($this->userDomain." is userdomain");
 		$this->pwd = $pwd;
 	}
 	private function createUserDir() {
@@ -25,10 +24,11 @@ class UnhostedAccount {
 			mkdir($userDir);
 		}
 		file_put_contents($userDir."/.pwd", sha1($this->pwd));
+echo "userDir $userDir created";
 		return true;
 	}
-	private function createDav($scope) {
-		$scope = ereg_replace("[^A-Za-z0-9\.]", "", $scope);
+	private function createDav($davProtocol, $davDomain) {
+		$scope = ereg_replace("[^A-Za-z0-9\.]", "", $davDomain);
 		if($scope[0] == '.') {
 				return "invalid-scope";
 		}
@@ -37,21 +37,22 @@ class UnhostedAccount {
 		if(!file_exists($davDir)) {
 			mkdir($davDir, 0600);
 		}
-		file_put_contents("<LimitExcept OPTIONS HEAD GET>\n"
-			."  AuthType Basic"
-			."  AuthName \"http://unhosted.org/spec/dav/0.1\""
-			."  Require valid-user"
-			."  AuthUserFile $davDir/.htpasswd"
-			."</LimitExcept>"
-			."Header always set Access-Control-Allow-Origin \"http://$scope\"", $davDir/.htaccess);
+		file_put_contents($davDir.'/.htaccess', "<LimitExcept OPTIONS HEAD GET>\n"
+			."  AuthType Basic\n"
+			."  AuthName \"http://unhosted.org/spec/dav/0.1\"\n"
+			."  Require valid-user\n"
+			."  AuthUserFile $davDir/.htpasswd\n"
+			."</LimitExcept>\n"
+			."Header always set Access-Control-Allow-Origin \"http://$scope\"\n");
 		$htpasswd = $this->userAddress .':'. crypt($token, base64_encode($token));
-		file_put_contents($htpasswd, $davDir/.htpasswd);
+		file_put_contents($htpasswd, $davDir.'/.htpasswd');
+echo "davDir $davDir created";
 		return $token;
 	}
-	private function createWallet($davBaseUrl, $davToken, $cryptoPwd, $dataScope) {
+	private function createWallet($davProtocol, $davDomain, $davToken, $cryptoPwd, $dataScope) {
 		$wallet = json_encode(array(
 			"userAddress" => $userAddress,
-			"davBaseUrl" => $davBaseUrl,
+			"davBaseUrl" => $davProtocol.'://'.$davDomain,
 			"davAuth" => base64_encode($userAddress .':'. $davToken),
 			"cryptoPwd" => $cryptoPwd
 			));
@@ -68,6 +69,7 @@ class UnhostedAccount {
 			mkdir($walletDir);
 		}
 		file_put_contents($walletDir.'/'.sha1($this->pwd), $wallet);
+echo "walletDir $walletDir created";
 		return $wallet;
 	}
 	public function getWallet($dataScope) {
@@ -80,12 +82,13 @@ class UnhostedAccount {
 	}
 	public function registerHosted() {
 		$this->createUserDir();
-		$davToken = $this->createDav(UnhostedSettings::protocol . '://' . UnhostedSettings::domain . '/');
-		return $this->createWallet(UnhostedSettings::protocol . '://' . UnhostedSettings::domain . '/', $davToken, null, UnhostedSettings::domain);
+		$davToken = $this->createDav(UnhostedSettings::protocol, UnhostedSettings::domain . '/');
+		return $this->createWallet(UnhostedSettings::protocol, UnhostedSettings::domain . '/', $davToken, null, UnhostedSettings::domain);
 	}
 	public function registerWallet($davBaseUrl, $davToken, $dataScope) {
+		list($davProtocol, $davDomain) = explode('://', $davBaseUrl);
 		$cryptoPwd = sha1(mt_rand());
-		return $this->createWallet($davBaseUrl, $davToken, $cryptoPwd, $dataScope);
+		return $this->createWallet($davProtocol, $davDomain, $davToken, $cryptoPwd, $dataScope);
 	}
 	public function addApp($dataScope) {
 		$pwdFile = UnhostedSettings::davDir . "{$this->userDomain}/{$this->userName}/.pwd";
