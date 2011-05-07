0979561e9b0616356f1757ba2eca62944d8cbb20
Michiel de Jong <michiel@unhosted.org>
2011-05-07 22:34:46 +0200

    experimental code to make unhosted.js asynchronous and give support for in-app passwords

--- my-unhosted-website/www/unhosted/unhosted.js
+++ my-unhosted-website/www/unhosted/unhosted.js
@@ -14,35 +14,45 @@ var DAV = function() {
 			+"/"+key;
 		return url;
 	}
-	dav.get = function(key) {
+	dav.get = function(key, cb) {
 		var wallet = getWallet();
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", keyToUrl(key, wallet), false);
-		xhr.send();
-		if(xhr.status == 200) {
-			return xhr.responseText;
-		} if(xhr.status == 404) {
-			return null;
-		} else {
-			alert("error: got status "+xhr.status+" when doing basic auth GET on url "+keyToUrl(key));
+		xhr.open("GET", keyToUrl(key, wallet), true);
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				if(xhr.status == 200) {
+					cb(xhr.responseText);
+				} if(xhr.status == 404) {
+					cb(null);
+				} else {
+					alert("error: got status "+xhr.status+" when doing basic auth GET on url "+keyToUrl(key));
+				}
+			}
 		}
+		xhr.send();
 	}
 	
-	dav.put = function(key, text) {
+	dav.put = function(key, text, cb) {
 		var wallet = getWallet();
 		var xhr = new XMLHttpRequest();
 		
-		//xhr.open("PUT", keyToUrl(key, wallet), false, wallet.userAddress, wallet.davToken);
+		//xhr.open("PUT", keyToUrl(key, wallet), true, wallet.userAddress, wallet.davToken);
 		//HACK:
-		xhr.open("PUT", keyToUrl(key, wallet), false);
+		xhr.open("PUT", keyToUrl(key, wallet), true);
 		xhr.setRequestHeader("Authorization", "Basic "+Base64.encode(wallet.userAddress +':'+ wallet.davToken));
 		//END HACK.
 
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				if(xhr.status != 200 && xhr.status != 201 && xhr.status != 204) {
+					alert("error: got status "+xhr.status+" when doing basic auth PUT on url "+keyToUrl(key));
+				} else {
+					cb();
+				}
+			}
+		}
 		xhr.withCredentials = "true";
 		xhr.send(text);
-		if(xhr.status != 200 && xhr.status != 201 && xhr.status != 204) {
-			alert("error: got status "+xhr.status+" when doing basic auth PUT on url "+keyToUrl(key));
-		}
 	}
 	return dav;
 }
@@ -63,20 +73,28 @@ var Unhosted = function() {
 	unhosted.getUserName = function() {
 		return getWallet().userAddress;
 	}
-	unhosted.get = function(key) {
+	unhosted.get = function(key, requirePwd, cb) {
 		var wallet = getWallet();
 		if(wallet.cryptoPwd == null) {
-			return JSON.parse(dav.get(key));
+			dav.get(key, function(str) {
+				try {
+					cb(JSON.parse(str));
+				} catch(e) {
+					requirePwd();
+				}
+			});
 		} else {
-			return JSON.parse(sjcl.decrypt(wallet.cryptoPwd, dav.get(key)));
+			dav.get(key, function(str) {
+				cb(JSON.parse(sjcl.decrypt(wallet.cryptoPwd, str));
+			}
 		}
 	}
-	unhosted.set = function(key, value) {
+	unhosted.set = function(key, value, cb) {
 		var wallet = getWallet();
 		if(wallet.cryptoPwd == null) {
-			dav.put(key, JSON.stringify(value));
+			dav.put(key, JSON.stringify(value), cb);
 		} else {
-			dav.put(key, sjcl.encrypt(wallet.cryptoPwd, JSON.stringify(value)));
+			dav.put(key, sjcl.encrypt(wallet.cryptoPwd, JSON.stringify(value)), cb);
 		}
 	}
 	unhosted.close = function() {
