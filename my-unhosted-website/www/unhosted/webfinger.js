6f846762dc8f3c56e33a8ef185a815995151488e
Michiel de Jong <michiel@unhosted.org>
2011-05-21 11:36:18 +0200

    move lrdd to jQuery

--- my-unhosted-website/www/unhosted/webfinger.js
+++ my-unhosted-website/www/unhosted/webfinger.js
@@ -49,14 +49,6 @@ var Webfinger = function() {
 	}
 	var processLrrd = function(lrrdXml, majorVersion, minMinorVersion, onError, cb) {
 		try {
-			var linkElts = lrrdXml.documentElement.getElementsByTagName('Link');
-			var i;
-			for(i=0; i < linkElts.length; i++) {
-				if(matchLinkRel(linkElts[i].attributes.getNamedItem('rel').value, majorVersion, minMinorVersion)) {
-					cb(linkElts[i].attributes.getNamedItem('href').value);
-					return;
-				}
-			}
 		} catch(e) {
 			onError();
 		}
@@ -64,33 +56,24 @@ var Webfinger = function() {
 	webFinger.getDavBaseUrl = function(userAddress, majorVersion, minMinorVersion, onError, cb) {
 		//get the WebFinger data for the user and extract the uDAVdomain:
 		getHostMeta(userAddress, 'lrdd', onError, function(template) {
-			try {
-				var xhr = new XMLHttpRequest();
-				var url = template.replace(/{uri}/, "acct:"+userAddress, true);
-				xhr.open("GET", url, true);
-				//WebFinger spec allows application/xml+xrd as the mime type,
-				//but we need it to be text/xml for xhr.responseXML to be non-null:
-				if (xhr.overrideMimeType)
-					xhr.overrideMimeType('text/xml');
-				xhr.onreadystatechange = function() {
-					if(xhr.readyState == 4) {
-						if(xhr.status == 200) {
-							try {
-								//HACK
-								var parser = new DOMParser();
-								var responseXML = parser.parseFromString(xhr.responseText, "text/xml");
-								//END HACK
-								processLrrd(responseXML, majorVersion, minMinorVersion, onError, cb);
-							} catch(e) {
-								onError();
+			$.ajax({
+				url: template.replace(/{uri}/, "acct:"+userAddress, true),
+				cache: false,
+				dataType: "xml",
+				success: function(xml){
+					try {
+						$(xml).find('Link').each(function() {
+							if(matchLinkRel($(this).attr('rel'), majorVersion, minMinorVersion)) {
+								cb($(this).attr('href'));
+								//TODO: should exit loop now that a matching result was found.								
 							}
-						}
+						});
+					} catch(e) {
+						onError();
 					}
-				}
-				xhr.send();
-			} catch(e) {
-				onError();
-			}
+				},
+				error: onError
+			});
 		});
 	}
 	return webFinger;
