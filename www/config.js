a92ff925b2729289bf648cc5ae5efce75ac309ca
Michiel@unhosted <michiel@unhosted.org>
2011-07-17 00:37:34 +0200

    that nice situation where some things are broken in chrome, and some in ff

--- www/config.js
+++ www/config.js
@@ -1,4 +1,7 @@
 var config = {
 	//appUrl: "https://myfavouritesandwich.org"
-	appUrl: "http://example.com/unhosted/www/"
+	appUrl: "http://example.com/unhosted/www/",
+	walletServiceUrl: "http://example.com/unhosted/www/wallet.php",
+	callbackUrl: "http://example.com/unhosted/www/cb.html",
+	clientId: "mfs"
 	};
---
+++ www/wallet.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'config.php';
+
+function createWallet($walletPath, $userAddress, $dataScope, $cryptoPwd) {
+	$wallet = json_encode(array(
+		"userAddress" => $userAddress,
+		"dataScope" => $dataScope,
+		"cryptoPwd" => $cryptoPwd
+		));
+	file_put_contents($walletPath, $wallet); 
+	return $wallet;
+}
+function getWallet($userAddress, $dataScope) {
+	$walletPath = Config::walletDir . bin2hex($userAddress)."_".bin2hex($dataScope);
+	if(file_exists($walletPath)) {
+		return file_get_contents($walletPath);
+	} else {
+		$cryptoPwd = sha1(mt_rand());
+		return createWallet($walletPath, $userAddress, $dataScope, $cryptoPwd);
+	}
+}
+function verifyBrowserId($assertion){
+	$url= 'https://browserid.org/verify?assertion='.urlencode($assertion).'&audience=example.com';
+	// is cURL installed yet?
+	if (!function_exists('curl_init')){
+		die('Sorry cURL is not installed!');
+	}
+ 
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$output = json_decode(curl_exec($ch), true);
+	curl_close($ch);
+ 	if($output["status"] == "okay") {
+		return $output["email"];
+	}
+	return false;
+}
+
+
+if($verifiedEmail = verifyBrowserId($_POST["browserIdAssertion"])) {
+	echo getWallet($verifiedEmail, $_POST["dataScope"]);
+}
