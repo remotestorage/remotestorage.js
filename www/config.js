6c056ac8ac01dfcfcdd419eec48795593e6380e8
Michiel@unhosted <michiel@unhosted.org>
2011-08-09 09:41:45 +0200

    removing old stuff

--- www/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-	appUrl: "https://myfavouritesandwich.org/",
-	walletServiceUrl: "https://myfavouritesandwich.org/wallet.php",
-	callbackUrl: "https://myfavouritesandwich.org/cb.html",
-	dataScope: "sandwiches",
-	clientId: "mfs"
-	};
--- www/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Config {
-	const walletDir = "../wallet/";
-}
