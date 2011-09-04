0850da50a621e44dfb7522491a0b8039536073bf
Michiel de Jong <michiel@unhosted.org>
2011-09-04 05:54:50 +0000

    move cb.html into index.html

--- apps/myfavouritesandwich.org/config.js
+++ apps/myfavouritesandwich.org/config.js
@@ -1,7 +1,7 @@
 var config = { appUrl: 'https://myfavouritesandwich.org/'
              , sessionServiceUrl: 'https://myfavouritesandwich.org/session'
              , hostedOwncloudBase: 'https://myfavouritesandwich.org:444'
-             , callbackUrl: 'https://myfavouritesandwich.org/cb.html'
+             , callbackUrl: 'https://myfavouritesandwich.org/'
              , dataScope: 'sandwiches'
              , clientId: 'mfs'
              }
