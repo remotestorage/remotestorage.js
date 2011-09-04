5109c1bd11459ef9698934f5d116a619ed71cf67
Michiel de Jong <michiel@unhosted.org>
2011-09-04 07:52:52 +0000

    add Sync it up button and fix identity.js

--- apptorrent/lib/identity.js
+++ apptorrent/lib/identity.js
@@ -1,41 +1,33 @@
 (function() {
   var url = require('url')
-    , fs = require('fs')
-
-  var files = {'/.well-known/host-meta': 'application/xml+xrd'
-    , '/webfinger': 'application/xml+xrd'
-  }
-
-  function serveAppCache(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/cache-manifest'})
-    res.write('CACHE MANIFEST\n\n#version: '+ appCacheTime +'\nNETWORK:\n\n*\nCACHE:\n')
-    for(var i in statics) {
-       res.write(i.substr(1) +'\n')
-    }
-    res.end()
-  }
 
   function handle(req, res) {
     var path = url.parse(req.url).pathname
-    if(path == '/') {
-      path = '/index.html'
+    console.log('200: '+path)
+    res.writeHead( 200
+                 , { 'Access-Control-Origin-Allow': '*'
+                 , 'Content-Type': 'application/xml+xrd'
+                 }
+    )
+    if(path == '/.well-known/host-meta') {
+      res.write('<?xml version="1.0" encoding="UTF-8"?>\n'
+        + '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">\n'
+        + '\t<hm:Host xmlns="http://host-meta.net/xrd/1.0">myfavouritesandwich.org</hm:Host>\n'
+        + '\t<Link rel="lrdd" template="https://myfavouritesandwich.org/webfinger?q={uri}">\n'
+        + '\t\t<Title>Resource Descriptor</Title>\n'
+        + '\t</Link>\n'
+        + '</XRD>\n'
+        )
+    } else if(path == '/webfinger') {
+      user = url.parse(req.url).search.substring(3) //TODO: parse this a bit more nicely
+      res.write('<?xml version="1.0" encoding="UTF-8"?>\n'
+        + '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">\n'
+        + '\t<hm:Host xmlns="http://host-meta.net/xrd/1.0">myfavouritesandwich.org</hm:Host>\n'
+        + '\t<Link rel="http://unhosted.org/spec/dav/0.1" href="https://myfavouritesandwich.org:444/apps/unhosted/compat.php/'+user+'/unhosted/"></Link>\n'
+        + '</XRD>\n'
+        )
     }
-    var contentType = files[path]
-    if(contentType) {
-      console.log('200: '+path)
-      res.writeHead( 200
-                   , { 'Access-Control-Origin-Allow': '*'
-                     , 'Content-Type': contentType
-                     }
-                   )
-      fs.createReadStream(config.staticsPath+path).pipe(res)
-    } else if(path == '/.appcache') {
-      serveAppCache(req, res)
-    } else {
-      console.log('404: '+path)
-      res.writeHead(404)
-      res.end()
-    }
+    res.end()
   }
 
   module.exports.handle = handle
