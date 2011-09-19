8ca5df48b1645c168cc26ce94d154f3737a0884f
Michiel@unhosted <michiel@unhosted.org>
2011-09-19 08:50:50 +0200

    now testing packager-to-player instead of packager-to-extractor

--- apptorrent/player/bootloader.js
+++ apptorrent/player/bootloader.js
@@ -1,64 +1,42 @@
 /* bootloader.js for appTorrent player. AGPL-licensed by the Unhosted project */
 
+$(document).ready(function(){
   ///////////////
  // fetch app //
 ///////////////
-var appTorrent = JSON.parse(syncStorage.getItem('a80bbb1b6496bba00681cc01eff3a7dbba840bed'))
-
+var appTorrent = JSON.parse(syncStorage.getItem(location.hash.substring(1)))
+if(location.hash.length < 2) {
+  alert('please specify a hash in the hash, e.g. ')
+} else if(appTorrent == null) {
+  alert('please sign in first')
+}
 
   //////////////////
  // extract html //
 //////////////////
 document.write(appTorrent.html)
 
-
   /////////////////
  // extract css //
 /////////////////
-var cssRulesNoClosingAccolade = appTorrent.css.replace(new RegExp( '[\\n\\r]', 'g' ), '').split('}')
-for(var i in cssRulesNoClosingAccolade) {
-  if(cssRulesNoClosingAccolade[i].length) {
-    document.getElementsByTagName('style')[0].sheet.insertRule(cssRulesNoClosingAccolade[i] + '}', i)
+for(var fileName in appTorrent.css) {
+  var cssRulesNoClosingAccolade = appTorrent.css[fileName].replace(new RegExp( '[\\n\\r]', 'g' ), '').split('}')
+  for(var i in cssRulesNoClosingAccolade) {
+    if(cssRulesNoClosingAccolade[i].length) {
+      document.getElementsByTagName('style')[0].sheet.insertRule(cssRulesNoClosingAccolade[i] + '}', i)
+    }
   }
 }
-
-
-  ///////////////////////////////////////////
- // replace localStorage with syncStorage //
-///////////////////////////////////////////
-
-//deal with lines like: localStorage['events'] = JSON.stringify(this.EVENTS);
-appTorrent.js = appTorrent.js.replace(
-  new RegExp('localStorage.\'([A-Za-z0-9\.]+)\'.([\ ]+)=([\ ]+)([A-Za-z0-9\.\(\)]+)','g')
-  , 'localStorage.setItem(\'$1\', $4)')
-
-//deal with lines like: localStorage.setup = 1
-appTorrent.js = appTorrent.js.replace(
-  new RegExp('localStorage\.'
-    +'(?!setItem)(?!getItem)(?!clear)(?!length)'
-    +'([A-Za-z0-9\.]+)([\ ]+)=([\ ]+)([A-Za-z0-9\.]+)','g')
-  , 'localStorage.setItem(\'$1\', $4)')
-
-//deal with lines like: this.EVENTS = localStorage['events']
-appTorrent.js = appTorrent.js.replace(
-  new RegExp('localStorage.\'([A-Za-z0-9\.]+)\'.','g')
-  , 'localStorage.getItem(\'$2\')')
-
-//deal with lines like: if(localStorage.setup) {
-appTorrent.js = appTorrent.js.replace(
-  new RegExp('localStorage\.'
-    +'(?!setItem)(?!getItem)(?!clear)(?!length)'
-    +'([A-Za-z0-9\.]+)','g')
-  , 'localStorage.getItem(\'$1\')')
-appTorrent.js = appTorrent.js.replace('localStorage', 'syncStorage')
-
 
   ////////////////
  // extract js //
 ////////////////
+for(var fileName in appTorrent.js) {
+   var script= document.createElement('script')
+   script.type= 'text/javascript'
+   script.id= fileName
+   script.innerHTML = localToSync(appTorrent.js[fileName])
+   document.getElementsByTagName('head')[0].appendChild(script)
+}
 
-//instead of just eval-ling, put script into DOM so we can inspect the code easily in bugzilla
-   var script= document.createElement('script');
-   script.type= 'text/javascript';
-   script.innerHTML = appTorrent.js;
-   document.getElementsByTagName('head')[0].appendChild(script);
+})
--- apptorrent/player/extractor.js
+++ apptorrent/player/extractor.js
@@ -42,19 +42,7 @@ for(var fileName in appTorrent.js) {
   var textarea= document.createElement('textarea')
   textarea.id = 'js_'+fileName
   if(shimSyncStorage) {
-    textarea.value = appTorrent.js[fileName]
-      //dot-notation, set and get:
-      .replace(new RegExp('localStorage\\\.(?!setItem)(?!getItem)(?!clear)(?!length)([A-Za-z0-9\.]+)([\ ]+)=([\ ]+)([A-Za-z0-9\.]+)','g'), 'localStorage.setItem(\'$1\', $4)')
-      .replace(new RegExp('localStorage\\\.(?!setItem)(?!getItem)(?!clear)(?!length)([A-Za-z0-9\.]+)','g'), 'localStorage.getItem(\'$1\')')
-      //bracket-notation, set and get:
-      .replace(new RegExp('localStorage\\\[\\\'([A-Za-z0-9\.]+)\\\'\\\]([\ ]+)=([\ ]+)([A-Za-z0-9\.\(\)]+)','g'), 'localStorage.setItem(\'$1\', $4)')
-      .replace(new RegExp('localStorage\\\[\\\'([A-Za-z0-9\.]+)\\\'\\\]','g'), 'localStorage.getItem(\'$1\')')
-      //normal class methods:
-      .replace('localStorage\.getItem', 'syncStorage.getItem')
-      .replace('localStorage\.setItem', 'syncStorage.setItem')
-      .replace('localStorage\.length', 'syncStorage.length')
-      .replace('localStorage\.key', 'syncStorage.key')
-      .replace('localStorage\.clear', 'syncStorage.clear')
+    textarea.value = localToSync(appTorrent.js[fileName])
   } else {
     textarea.value = appTorrent.js[fileName]
   }
@@ -73,6 +61,7 @@ textarea.value = '<!DOCTYPE html>\n<html>\n<head>\n'
 textarea.value += '\t<title>'+appTorrent.title+'</title>\n'
 if(shimSyncStorage) {
   textarea.value +='\t<script type="application/javascript" src="../jQuery.js"></script>\n'//shim in syncStorage
+  textarea.value +='\t<script type="application/javascript" src="https://browserid.org/include.js"></script>\n'//shim in syncStorage
   textarea.value +='\t<script type="application/javascript" src="../syncStorage4.js"></script>\n'//shim in syncStorage
 }
 for(var i in cssFiles) {
