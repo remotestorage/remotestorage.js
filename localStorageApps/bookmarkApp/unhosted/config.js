158fbd3006ea4d34682f61eb5b20a125448a0639
Michiel@unhosted <michiel@unhosted.org>
2011-09-21 11:19:39 +0200

    adding Jack Bowman's apps

---
+++ localStorageApps/bookmarkApp/unhosted/config.js
@@ -0,0 +1,13 @@
+var appBaseUrl = 'http://bm.jack-bowman.com';
+//var installationType = 'flower';
+
+var config = {
+	appUrl: appBaseUrl + '/',
+	doUrl: appBaseUrl + '/unhosted/do.php',
+	loginUrl: appBaseUrl + '/unhosted/login.html',
+	//registerUrl: appBaseUrl + '/unhosted/register.html',
+	callbackUrl: appBaseUrl + '/unhosted/callback.html',
+	clientId: 'bm.jack-bowman.com',
+	dataScope: 'jackbmarksapp',
+	homeDomain: 'bm.jack-bowman.com'
+}
---
+++ localStorageApps/bookmarkApp/unhosted/do.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'unhosted.php';
+function getString($paramName) {
+        if(!isset($_POST[$paramName])) {
+                die("Parameter $paramName not specified");
+        }
+        return $_POST[$paramName];
+}
+function getDomain($paramName) {
+        $domain = strtolower(getString($paramName));
+        if(!preg_match('|^[a-z0-9-]+(\.[a-z0-9-]+)*$|i', $domain)) {
+                die("Parameter $paramName should be a valid domain");
+        }
+	return $domain;
+}
+function getUserAddress($paramName) {
+        $userAddress = strtolower(getString($paramName));
+        if(!preg_match('|^[a-z0-9-]+(\.[a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*$|i', $userAddress)) {
+                die("Parameter $paramName should be a valid user address");
+        }
+	return $userAddress;
+}
+
+$unhostedAccount = new UnhostedAccount(getUserAddress("userAddress"), getString("pwd"));
+
+switch(getString("action")) {
+        case "getWallet":
+                $wallet = $unhostedAccount->getWallet(getDomain("dataScope"), getString("allowCreation")=="true");
+                if($wallet === false) {	
+			header("HTTP/1.0 404 Not Found");
+		} else {
+			echo $wallet;
+		}
+		break;
+        case "registerLocal":
+                echo $unhostedAccount->registerLocal(getDomain("dataScope"));
+                break;
+        case "addApp":
+                echo $unhostedAccount->addApp(getDomain("dataScope"));
+                break;
+}
+
