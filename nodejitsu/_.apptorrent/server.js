51b4a9c1cda35adab00a6af8b1b3a777c80a80ff
Michiel@unhosted <michiel@unhosted.org>
2011-10-07 20:14:58 +0200

    removing nodejitsu dir because it's confusing

--- nodejitsu/_.apptorrent/server.js
+++
@@ -1,4 +0,0 @@
-require('http').createServer(function(req, res) {
-  res.writeHead(200)
-  res.end('<html><head><script type="text/javascript" src="http://apptorrent.org/player.js"></script></head></html>')
-}).listen(80)
--- nodejitsu/apptorrent/localStorageApps/todoListApp/unhosted/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	doUrl: 'unhosted/do.php',
-	loginUrl: 'unhosted/login.html',
-	callbackUrlRelChars: 10, // length of 'login.html'
-	callbackUrl: 'callback.html',
-	clientId: 'todo.jack-bowman.com',
-	dataScope: 'jacktodoapp',
-	homeDomain: 'todo.jack-bowman.com'
-}
