2f75e0992075fcc76236bce78378b0ea1c7413d5
Michiel@unhosted <michiel@unhosted.org>
2011-10-09 16:28:10 +0200

    updating myfavouritsandwich.org to new version

---
+++ rackspace/domains/myfavouritesandwich.org/mfs.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  show();
+});
+
+function onsave() {
+  var sandwich = { ingredients: [ document.getElementById('firstIngredient').value
+                                , document.getElementById('secondIngredient').value
+                                ]
+                 }
+  remoteStorage.setItem('favSandwich', JSON.stringify(sandwich))
+  show()
+}
+
+function show() {
+  var sandwich = JSON.parse(remoteStorage.getItem('favSandwich'))
+  if(sandwich) {
+    document.getElementById('firstIngredient').value = sandwich.ingredients[0]
+    document.getElementById('secondIngredient').value = sandwich.ingredients[1]
+    for(var i=0;i < 2; i++) {
+      if(!(sandwich.ingredients[i])) {
+        sandwich.ingredients[i]='...'
+      }
+    }
+    document.getElementById('showIngredients').innerHTML = 'My favourite sandwich has <strong>'
+      +sandwich.ingredients[0]
+      +'</strong> and <strong>'
+      +sandwich.ingredients[1]
+      +'</strong> on it';
+  } else {
+    document.getElementById('showIngredients').innerHTML = 'My favourite sandwich has'
+    document.getElementById('firstIngredient').value = ''
+    document.getElementById('secondIngredient').value = ''
+  }
+}
