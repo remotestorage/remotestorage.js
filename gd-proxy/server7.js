bb9323e690179fa90d2c12d36ef66058d1c1603d
Michiel de Jong <michiel@unhosted.org>
2011-10-14 13:28:48 +0000

    server7.js

---
+++ gd-proxy/server7.js
@@ -0,0 +1,75 @@
+var http = require('http')
+  , https = require('https');
+
+  //////////////
+ // test app //
+//////////////
+
+http.createServer(function (req, res) {
+  res.writeHead(200);
+  res.end
+    ( '<html>\n'
+    + '<script>\n'
+    + 'function testPut() {\n'
+    + '  var token = window.location.hash.substring(15, 74);\n'
+    + '  var xhr = new XMLHttpRequest();\n'
+    + '  xhr.open(\'PUT\', \'http://myfavouritesandwich.org:9002/\', true);\n'
+    + '  xhr.onreadystatechange=function() {\n'
+    + '    if(xhr.status=200) {\n'
+    + '      \n'
+    + '      \n'
+    + '      \n'
+    + '    }\n'
+    + '  };\n'
+    + '  xhr.setRequestheader(\'Authorization: Bearer \'+token);\n'
+    + '  xhr.send(\'blabla\');\n'
+    + '}\n'
+    + '</script>\n'
+    + '<a onclick="testPut();">test PUT</a>\n'
+    + '<a href="'
+    + 'https://accounts.google.com/o/oauth2/auth?'
+    + 'client_id=709507725318-4h19nag3k4hv5osj1jvao0j3an3bu43t@developer.gserviceaccount.com&'
+    + 'redirect_uri=http://myfavouritesandwich.org:9000/&'
+    + 'scope=http://docs.google.com/feeds/&'
+    + 'response_type=token'
+    + '">click</a>\n'
+    + '</html>\n);
+}).listen(9000);
+
+  ////////////////////////////////
+ // gdata-remoteStorage bridge //
+////////////////////////////////
+
+http.createServer(function (req, res) {
+  console.log('REQ.URL:'+req.url);
+  console.log('REQ.HEADERS:'+JSON.stringify(req.headers));
+  var token = req.headers['authorization'];
+  console.log('TOKEN:'+token);
+  var options =
+    { 'host': 'docs.google.com'
+    , 'port': 443
+    , 'method': 'POST'
+    , 'path': '/feeds/default/private/full'
+    , 'headers':
+       { 'GData-Version': '3.0'
+       , 'Authorization': 'Bearer '+token
+       , 'Content-Length': '0'
+       , 'Content-Type': 'text/plain'
+       , 'Slug': 'LetsRide'
+       , 'X-Upload-Content-Length': '30'
+       , 'X-Upload-Content-Type': 'text/plain'
+       }
+    };
+  var req2 = https.request(options, function(res2) {
+    res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
+    res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
+    res.write('\nSTATUS: ' + res2.statusCode);
+    res.write('\nHEADERS: ' + JSON.stringify(res2.headers));
+    res2.setEncoding('utf8');
+    res2.on('data', function (chunk) {
+      res.write('\nBODY: ' + chunk);
+      res.end();
+    });
+  });
+  req2.end();
+}).listen(9002);
