90125088a55fe9d6afb332c80f4e57a4f92fd698
Michiel de Jong <michiel@unhosted.org>
2011-10-15 22:01:23 +0000

    oops :)

--- gd-proxy/server3.js
+++ gd-proxy/server3.js
@@ -3,7 +3,7 @@ var google = http.createClient(80, 'docs.google.com')
 var request = google.request('POST', '/feeds/default/private/full',
   { 'host': 'docs.google.com'
   , 'GData-Version': '3.0'
-  , 'Authorization': 'Bearer ya29.AHES6ZQIUliUulhSSuDLMv9XcHw6U1PHMoAYCfMFI2dU7kVXGxjt19A'
+  , 'Authorization': 'Bearer TOKEN'
   , 'Content-Length': '0'
   , 'Content-Type': 'text/plain'
   , 'Slug': 'LetsRide'
