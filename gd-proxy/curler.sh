cdfe689d6b1e356088fddd6c10835383a7bdaa0b
Michiel@unhosted <michiel@unhosted.org>
2011-10-16 00:32:17 +0200

    updating curler and static

--- gd-proxy/curler.sh
+++ gd-proxy/curler.sh
@@ -1,2 +1,4 @@
-echo $1
-curl -i -H "Content-Type: text/plain" -H "X-Upload-Content-Length: 30" -H "X-Upload-Content-Type: text/plain" -H "origin: http://myfavouritesandwich.org" -H "GData-Version: 3.0" -H "Authorization: Bearer $1" -H "Slug: remoteStorage-curl" -H "Content-Length: 0" -X POST -d "" https://docs.google.com/feeds/default/private/full
+echo token is $1
+echo initiate document directly at google ; curl -i -H "Content-Type: text/plain" -H "X-Upload-Content-Length: 30" -H "X-Upload-Content-Type: text/plain" -H "origin: http://myfavouritesandwich.org" -H "GData-Version: 3.0" -H "Authorization: Bearer $1" -H "Slug: remoteStorage-curl" -H "Content-Length: 0" -X POST -d "" https://docs.google.com/feeds/default/private/full
+
+#echo initiate document through proxy ; curl -i -H "Content-Type: text/plain" -H "X-Upload-Content-Length: 30" -H "X-Upload-Content-Type: text/plain" -H "origin: http://myfavouritesandwich.org" -H "GData-Version: 3.0" -H "Authorization: Bearer $1" -H "Slug: remoteStorage-curl" -H "Content-Length: 0" -X POST -d "" http://myfavouritesandwich.org:9002/feeds/default/private/full
