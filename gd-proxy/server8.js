087450c931a85d33fc89c902fee25ecd40eb3a24
Michiel de Jong <michiel@unhosted.org>
2011-10-15 16:50:18 +0000

    separate static page from server-side procxy

--- gd-proxy/server8.js
+++ gd-proxy/server8.js
@@ -1,43 +1,9 @@
 var http = require('http')
   , https = require('https');
 
-  //////////////
- // test app //
-//////////////
-
-http.createServer(function (req, res) {
-  console.log('WEB HIT');
-  res.writeHead(200);
-  res.end
-    ( '<html>\n'
-    + '<script>\n'
-    + 'function testPut() {\n'
-    + '  var token = window.location.hash.substring(14, 74);\n'
-    + '  var xhr = new XMLHttpRequest();\n'
-    + '  xhr.open(\'PUT\', \'http://myfavouritesandwich.org:9002/\', true);\n'
-    + '  xhr.onreadystatechange=function() {\n'
-    + '    if(xhr.status=200) {\n'
-    + '      alert(xhr.getResponseHeader(\'Location\'));\n'
-    + '    }\n'
-    + '  };\n'
-    + '  xhr.setRequestHeader(\'Authorization\', \'Bearer \'+token);\n'
-    + '  xhr.send(\'blabla\');\n'
-    + '}\n'
-    + '</script>\n'
-    + '<a href="" onclick="testPut();">test PUT</a>\n'
-    + '<a href="'
-    + 'https://accounts.google.com/o/oauth2/auth?'
-    + 'client_id=709507725318-4h19nag3k4hv5osj1jvao0j3an3bu43t@developer.gserviceaccount.com&'
-    + 'redirect_uri=http://myfavouritesandwich.org:9000/&'
-    + 'scope=http://docs.google.com/feeds/&'
-    + 'response_type=token'
-    + '">click</a>\n'
-    + '</html>\n');
-}).listen(9000);
-
-  ////////////////////////////////
- // gdata-remoteStorage bridge //
-////////////////////////////////
+  //////////////////////
+ // gdata CORS proxy //
+//////////////////////
 
 http.createServer(function (req, res) {
   console.log('REQ.URL:'+req.url);
