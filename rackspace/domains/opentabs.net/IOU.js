c0e11e5278626ef9549e22ab816dfc2c2d64a84c
Michiel de Jong <michiel@unhosted.org>
2011-10-20 16:28:51 +0000

    ticks and crosses

--- rackspace/domains/opentabs.net/IOU.js
+++ rackspace/domains/opentabs.net/IOU.js
@@ -271,10 +271,10 @@ for(i in sampleData) {
     document.write('<li style="background-color:yellow">[incoming IOU:] [?]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.proposer == 'you') && (iou.status == 'declined') && (iou.payer == 'you')) {
-    document.write('<li style="background-color:yellow">[declined your IOU:] [NO]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:yellow">[declined your IOU:] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.proposer == 'you') && (iou.status == 'declined') && (iou.payee == 'you')) {
-    document.write('<li style="background-color:yellow">[declined your invoice:] [NO]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:yellow">[declined your invoice:] [X]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
   }
 }
 document.write('</ul>');
@@ -288,7 +288,7 @@ for(var peer in peers) {
       document.write('<li style="background-color:pink">[hurry:] [!]'+iou.amount+iou.currency+'</li>');
     }
     if((iou.payer == peer) && (iou.status == 'sent')) {
-      document.write('<li style="background-color:green">[got it?] [X]'+iou.amount+iou.currency+'</li>');
+      document.write('<li style="background-color:green">[got it?] [&#10003;]'+iou.amount+iou.currency+'</li>');
     }
     if((iou.payer == peer) && (iou.status == 'requested')) {
       document.write('<li style="background-color:green">[you said hurry] [!]'+iou.amount+iou.currency+'</li>');
@@ -314,46 +314,46 @@ document.write('<h2>HISTORY</h2><ul>');
 for(i in sampleData) {
   var iou = sampleData[i];
   if((iou.proposer != 'you') && (iou.status == 'declined') && (iou.payee=='you')) {
-    document.write('<li style="background-color:yellow">[you declined] [NO]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:yellow">[you declined] [X]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.proposer != 'you') && (iou.status == 'declined') && (iou.payer=='you')) {
-    document.write('<li style="background-color:yellow">[you declined] [NO]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:yellow">[you declined] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.payer == 'you') &&(iou.status == 'closed')) {
-    document.write('<li style="background-color:yellow">[was declined] [NO]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:yellow">[was declined] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.payee == 'you') &&(iou.status == 'closed')) {
-    document.write('<li style="background-color:yellow">[was declined] [NO]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:yellow">[was declined] [X]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.payer == 'you') && (iou.status == 'sent')) {
-    document.write('<li style="background-color:pink">[you sent] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:pink">[you sent] [&#10003;]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.payer == 'you') && (iou.status == 'received')) {
-    document.write('<li style="background-color:pink">[they received] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:pink">[they received] [&#10003;]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
   }
   if((iou.payee == 'you') && (iou.status == 'received')) {
-    document.write('<li style="background-color:green">[you received] [X]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
+    document.write('<li style="background-color:green">[you received] [&#10003;]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
   }
 }
 
 document.write('</ul>');
 // IMPORTANT (highlighted, by timestamp)
 // [?]Y proposed (rcv)
-// [NO]Y declined (snd)
+// [X]Y declined (snd)
 
 // PER PEER (peers with ! items inside sorted on top, otherwise by timestamp)
 // [!]R requested (payer) highlighted
-// [X]G sent (payee) highlighted
+// [&#10003;]G sent (payee) highlighted
 //-------------------------------------
 // accepted (both)
 // [?]Y proposed (snd)
 // [!]G requested (payee)
 
 // HISTORY (greyed out, by timestamp)
-// [NO]Y declined (rcv)
-// [NO]Y closed (both)
-// [X]R sent (payer)
-// [X]R/G received (both)
+// [X]Y declined (rcv)
+// [X]Y closed (both)
+// [&#10003;]R sent (payer)
+// [&#10003;]R/G received (both)
 
 
 
