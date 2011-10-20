e2a9b7b8af3cc70bd3d10c53d676b589c574f788
Michiel de Jong <michiel@unhosted.org>
2011-10-20 11:00:10 +0000

    adding open new tab dialog

---
+++ rackspace/domains/opentabs.net/app.js
@@ -0,0 +1,9 @@
+function clickIou(i) {
+  window.location="/app-iou-clicked.html#"+i;
+}
+function clickOat() {
+  document.getElementById('addDialog').style.display="block";
+}
+function sendNewTab() {
+  document.getElementById('addDialog').style.display="none";
+}
