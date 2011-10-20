6be62060a4e84e7bd61cfbe01821d582b78623f8
Michiel de Jong <michiel@unhosted.org>
2011-10-20 14:24:05 +0000

    adding IOU.js

---
+++ rackspace/domains/opentabs.net/IOU.js
@@ -0,0 +1,72 @@
+//tab fields:
+// payer, payee, timestamp, amount, currency,
+//and optionally:
+// description, location
+
+function openNewTab(tab, signature) {
+  tab.status='proposed';
+  //add the first signature:
+  tab.signatures = [signature];
+  var tabId = localStorage.length;
+  localStorage.setItem(tabId, JSON.stringify(tab));
+  return tabId;
+}
+
+function declineTab(tabId, message, signature) {
+  var tab = JSON.parse(localStorage.getItem(tabId));
+  tab.status='declined';
+  tab.message=message;
+  //add the second signature:
+  tab.signatures.push(signature);
+  localStorage.setItem(tabId, JSON.stringify(tab));
+}
+
+function acceptTab(tabId, signature) {
+  var tab = JSON.parse(localStorage.getItem(tabId));
+  tab.status='accepted';
+  //add the second signature:
+  tab.signatures.push(signature);
+  localStorage.setItem(tabId, JSON.stringify(tab));
+}
+
+function requestPayment(tabId, signature) {
+  var tab = JSON.parse(localStorage.getItem(tabId));
+  tab.status='paymentRequested';
+  //add the third signature:
+  tab.signatures.push(signature);
+  localStorage.setItem(tabId, JSON.stringify(tab));
+}
+
+function markAsPaid(tabId, byWhom, signature) {
+  var tab = JSON.parse(localStorage.getItem(tabId));
+  if(byWhom=='payer') {
+    tab.status='paymentSent';
+  } else {
+    tab.status='paymentReceived';
+  }
+  //add the extra signature:
+  tab.signatures.push(signature);
+  localStorage.setItem(tabId, JSON.stringify(tab));
+}
+
+//INBOX:
+//proposed in
+//declined in
+//sent in
+
+//MAIN+'*':
+//requested in
+//requested out
+
+//MAIN:
+//proposed out
+//accepted in
+//accepted out
+
+//HISTORY:
+//declined out
+//closed in
+//closed out
+//sent out
+//received in
+//received out
