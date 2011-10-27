00512fe906462b8f6ef26cc9f12fd018be3c7aa5
Michiel de Jong <michiel@unhosted.org>
2011-10-27 09:05:42 +0000

    moving rackspace into seperate repo

--- rackspace/domains/opentabs.net/app.js
+++
@@ -1,9 +0,0 @@
-function clickIou(i) {
-  document.getElementById('iouActions'+i).style.display="block";
-}
-function clickOat() {
-  document.getElementById('addDialog').style.display="block";
-}
-function sendNewTab() {
-  document.getElementById('addDialog').style.display="none";
-}
--- rackspace/domains/unhost.it/attemptAtAppsApp.js
+++
@@ -1,1 +0,0 @@
-javascript:'<script>(function(){var str=\'"\'+location.hostname+\'":\');for(var i=0;i<localStorage.length;i++){str+=\'<a>\'+localStorage.key(i)+\'</a>\'};document.write(str)})()</script>'
