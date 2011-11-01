646fcc92d56aad9a2ca51aa8b289089c1a0cabe7
Michiel de Jong <michiel@unhosted.org>
2011-11-01 23:45:54 +0000

    correct build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cat files/start.js files/poorMansJQuery.js files/webfinger.js files/oauth.js files/getputdelete.js files/asyncSync.js files/asyncSyncQueue.js files/domApiShim.js files/end.js >> ../rackspace/domains/unhost.it/remoteStorage-future.js
+cat files/start.js files/poorMansJQuery.js files/webfinger.js files/oauth.js files/getputdelete.js files/asyncSync.js files/asyncSyncQueue.js files/domApiShim.js files/end.js > ../rackspace/domains/unhost.it/remoteStorage-future.js
