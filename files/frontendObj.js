b3fa40df9f4e8c67080857a981fba9b2577f09f5
Michiel de Jong <michiel@unhosted.org>
2011-11-16 20:39:21 +0100

    move stubs into unit (not sure yet where this is going)

--- files/frontendObj.js
+++ files/frontendObj.js
@@ -1,3 +1,10 @@
+if(!window) {
+  var window = {
+    localStorage: {
+    }
+  };
+}
+
 exports.frontendObj = (function() {
 
         //////////////////
@@ -6,8 +13,8 @@ exports.frontendObj = (function() {
 
       function calcLength() {
         var len = 0;
-        for(var i=0; i<localStorage.length; i++) {
-          if(localStorage.key(i).substring(0,15)=='_remoteStorage_') {
+        for(var i=0; i<window.localStorage.length; i++) {
+          if(window.localStorage.key(i).substring(0,15)=='_remoteStorage_') {
             len++;
           }
         }
@@ -29,10 +36,10 @@ exports.frontendObj = (function() {
         }
       }
       if(window.remoteStorage.options.userAddress) {
-        localStorage.setItem('_remoteStorageUserAddress', window.remoteStorage.options.userAddress);
+        window.localStorage.setItem('_remoteStorageUserAddress', window.remoteStorage.options.userAddress);
       }
       if(window.remoteStorage.options.token) {
-        localStorage.setItem('_remoteStorageOauthToken', window.remoteStorage.options.token);
+        window.localStorage.setItem('_remoteStorageOauthToken', window.remoteStorage.options.token);
       }
 
       if(NeedLoginBox()=='legacy') {
@@ -57,17 +64,17 @@ exports.frontendObj = (function() {
     length: calcLength(),
     _tryConnect: _tryConnect,
     key: function(req) {
-      for(var i=0; i<localStorage.length; i++) {
-        if(localStorage.key(i).substring(0,15)=='_remoteStorage_') {
+      for(var i=0; i<window.localStorage.length; i++) {
+        if(window.localStorage.key(i).substring(0,15)=='_remoteStorage_') {
           if(req == 0) {
-            return localStorage.key(i).substring(15);
+            return window.localStorage.key(i).substring(15);
           }
           req--;
         }
       }
     },
     getItem: function(k) {
-      var cacheObj = localStorage.getItem('_remoteStorage_'+k);
+      var cacheObj = window.localStorage.getItem('_remoteStorage_'+k);
       if(cacheObj) {
         try {
           return JSON.parse(cacheObj).value;
@@ -77,7 +84,7 @@ exports.frontendObj = (function() {
     },
     setItem: function(k,v) {
       var cacheObj = {};
-      var cacheStr = localStorage.getItem('_remoteStorage_'+k);
+      var cacheStr = window.localStorage.getItem('_remoteStorage_'+k);
       if(cacheStr) {
         try {
           var cacheObj = JSON.parse(cacheStr);
@@ -88,58 +95,58 @@ exports.frontendObj = (function() {
         }catch(e) {}
       }
       cacheObj.value=v;
-      localStorage.setItem('_remoteStorage_'+k, JSON.stringify(cacheObj));
+      window.localStorage.setItem('_remoteStorage_'+k, JSON.stringify(cacheObj));
       window.remoteStorage.length = calcLength();
       markDirty(k);
       work();
     },
     removeItem: function(k) {
-      localStorage.removeItem('_remoteStorage_'+k);
+      window.localStorage.removeItem('_remoteStorage_'+k);
       window.remoteStorage.length = calcLength();
       markDirty(k);
       work();
     },
     clear: function() {
       var keysToRemove = [];
-      for(var i=0;i<localStorage.length;i++) {
-        if(localStorage.key(i).substring(0,15)=='_remoteStorage_') {
-          keysToRemove.push(localStorage.key(i));
-          keysToRemove.push('_remoteStorageWorking_'+localStorage.key(i));
-          markDirty(localStorage.key(i));
+      for(var i=0;i<window.localStorage.length;i++) {
+        if(window.localStorage.key(i).substring(0,15)=='_remoteStorage_') {
+          keysToRemove.push(window.localStorage.key(i));
+          keysToRemove.push('_remoteStorageWorking_'+window.localStorage.key(i));
+          markDirty(window.localStorage.key(i));
         }
       }
       keysToRemove.forEach(function(key){
-        localStorage.removeItem(key);
+        window.localStorage.removeItem(key);
       });
       window.remoteStorage.length = 0;
       work();
     },
     isConnected: function() {
-      return (localStorage.getItem('_remoteStorageOauthToken') != null);
+      return (window.localStorage.getItem('_remoteStorageOauthToken') != null);
     },
     getUserAddress: function() {
-      return localStorage.getItem('_remoteStorageUserAddress');
+      return window.localStorage.getItem('_remoteStorageUserAddress');
     },
     disconnect: function() {
-      localStorage.removeItem('_remoteStorageUserAddress');
-      localStorage.removeItem('_remoteStorageCategory');
-      localStorage.removeItem('_remoteStorageKV');
-      localStorage.removeItem('_remoteStorageAPI');
-      localStorage.removeItem('_remoteStorageAuthAddress');
-      localStorage.removeItem('_remoteStorageOauthToken');
-      localStorage.removeItem('_remoteStorageDirties');
-      localStorage.removeItem('remoteStorageIndex');
+      window.localStorage.removeItem('_remoteStorageUserAddress');
+      window.localStorage.removeItem('_remoteStorageCategory');
+      window.localStorage.removeItem('_remoteStorageKV');
+      window.localStorage.removeItem('_remoteStorageAPI');
+      window.localStorage.removeItem('_remoteStorageAuthAddress');
+      window.localStorage.removeItem('_remoteStorageOauthToken');
+      window.localStorage.removeItem('_remoteStorageDirties');
+      window.localStorage.removeItem('remoteStorageIndex');
       var keysToRemove = [];
-      for(var i=0; i<localStorage.length; i++) {
-        if(localStorage.key(i).substring(0,15)=='_remoteStorage_') {
-          keysToRemove.push(localStorage.key(i));
+      for(var i=0; i<window.localStorage.length; i++) {
+        if(window.localStorage.key(i).substring(0,15)=='_remoteStorage_') {
+          keysToRemove.push(window.localStorage.key(i));
         }
       }
       keysToRemove.forEach(function(key){
         if(window.remoteStorage.options.onChange) {
-          remoteStorage.options.onChange(key.substring(15), localStorage.getItem(key), null);
+          remoteStorage.options.onChange(key.substring(15), window.localStorage.getItem(key), null);
         }
-        localStorage.removeItem(key);
+        window.localStorage.removeItem(key);
       });
     },
     _init: function() {
--- tests/testFrontendObj.js
+++ tests/testFrontendObj.js
@@ -1,8 +1,2 @@
-// stubs:
-
-function calcLength() {
-  return 0;
-}
-
 var frontendObj = require('../files/frontendObj.js');
 console.log(frontendObj.configure());
