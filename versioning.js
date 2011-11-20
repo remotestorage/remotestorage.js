fcc6fe59e63d01409f3e30d3de2878c56f18beb6
Michiel de Jong <michiel@unhosted.org>
2011-11-20 22:44:52 +0100

    revisit versioning data structures

--- versioning.js
+++ versioning.js
@@ -1,54 +1,16 @@
-//situations:
-// disconnected
-// fetching
-// delivering
-// connected
-// versioning should take care of creating local revisions and merging.
-// incoming remote: merge into existing remote; whenever there is news, pass it on to local
-// local change detected: create new local revision, and if connected and merged, push it out
-// ahead of remote
-// remote ahead of local
-//
-// when logging in, retrieve remote. merge in any keys that are ahead of local
-// every 10 seconds, maybe create new local. if so, push it and make it the new head.
-//
-// data structures:
-// _shadowState
-//   local: (timestamp) 
-//   remote: (timestamp)
-// _shadowRevision_(timestamp)
-//   (key): (timestamp)
-// _shadowItem_(key)_(timestamp) - contains value of that key at that timestamp. in the unlikely event of a timestamp-collision, local wins.
-
 exports.versioning = (function() {
   function takeLocalSnapshot() {
     var hasChanges = false;
     var now = ((new Date()).getTime())/1000;
-    var state = JSON.parse(localStorage.getItem('_shadowState'));
-    if(!state) {
-      state = {
-        local: 0,
-        remote: 0
-      };
-    }
-    var index;
-    if(state.local) {
-      index = JSON.parse(localStorage.getItem('_shadowRevision_'+state.local));
-    } else {
-      index = {};
-      hasChanges = true;
-    }
+    var shadowLocal = JSON.parse(localStorage.getItem('_shadowLocal')) || {};
+    var shadowRemote = JSON.parse(localStorage.getItem('_shadowRemote')) || {};
     for(var i = 0; i<localStorage.length; i++) {
       var thisKey = localStorage.key(i);
       if(thisKey.substring(0, 7) != '_shadow') {
         var val = localStorage.getItem(thisKey);
-        var shadowVal;
-        var previousTimestamp = index[thisKey];
-        if(previousTimestamp) {
-          shadowVal = localStorage.getItem('_shadowItem_'+thisKey+'_'+previousTimestamp);
-        }
+        var shadowVal = localStorage.getItem('_shadowItem_'+thisKey);
         if(val != shadowVal) {
-          localStorage.setItem('_shadowItem_'+thisKey+'_'+now, val);
+          localStorage.setItem('_shadowItem_'+thisKey, val);
           console.log('storing local version of item "'+thisKey+'" @'+now);
           index[thisKey] = now;
           hasChanges = true;
@@ -56,9 +18,7 @@ exports.versioning = (function() {
       }
     }
     if(hasChanges) {
-      state.local = now;
-      localStorage.setItem('_shadowState', JSON.stringify(state));
-      localStorage.setItem('_shadowRevision_'+now, JSON.stringify(index));
+      localStorage.setItem('_shadowLocal', JSON.stringify(index));
       console.log('storing local snapshot '+now);
       return now;
     } else {
