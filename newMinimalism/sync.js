6ac90df801327d75f06f5c71bd5e4b1100ee0e12
Michiel de Jong <michiel@unhosted.org>
2011-11-20 17:46:08 +0100

    trying to put in sync logic

--- newMinimalism/sync.js
+++ newMinimalism/sync.js
@@ -1,4 +1,18 @@
 exports.sync = (function() {
+  var backend = {
+    function get(key, err, cb, timeout) {
+      console.log('backend.get("'+key+'", "'+value+'", err, cb, '+timeout+');');
+    }
+    function set(key, value, err, cb, timeout) {
+      console.log('backend.set("'+key+'", "'+value+'", err, cb, '+timeout+');');
+    }
+    function remove(key, err, cb, timeout) {
+      console.log('backend.remove("'+key+'", "'+value+'", err, cb, '+timeout+');');
+    }
+  };
+  function setBackend(backendToSet) {
+    backend = backendToSet;
+  }
   function start() {
     localStorage.setItem('_shadowSyncStatus', 'pulling');
     console.log('setting sync status to pulling');
@@ -12,17 +26,65 @@ exports.sync = (function() {
       console.log('setting sync status to pushing revision '+timestamp);
     }
   }
-  function work(timeout) {
+  function resumePulling(timeout, cb) {
+    console.log('resume pulling');
+    backend.get('_shadowLatestRevision', function(msg) {
+      console.log('error retrieving _shadowLatestRevision');
+    }, function(value) {
+      localStorage.setItem('_shadowRemote', value);
+      localStorage.setItem('_shadowSyncStatus') == 'idle';
+      cb();
+    }, timeout);
+  }
+  function getItemToPush(next) {
+    var pushRev = localStorage.getItem('_shadowSyncPushRev');
+    var index = JSON.parse(localStorage.getItem('_shadowRevision_'+pushRev));
+    var entryToPush = localStorage.getItem('_shadowSyncCurrEntry');
+    if(entryToPush == null) {
+      entryToPush = 0;//leave as null in localStorage, no use updating that
+    }
+    if(next) {
+      entryToPush++;
+      localStorage.setItem('_shadowSyncCurrEntry', entryToPush);
+    }
+    if(entryToPush < index.length) {
+      return index[entryToPush];
+    } else if(entryToPush == index.length) {
+      return '_shadowRevision_'+pushRev;
+    } else {
+      localStorage.removeItem('_shadowSyncCurrEntry');
+      return false;
+    }
+  }
+
+  function resumePushing(timeout, cb) {
+    console.log('resume pushing');
+    var itemToPush = getItemToPush(false);
+    backend.set(nextItemToPush, localStorage.getItem(itemToPush), function(msg) {
+      console.log('error putting '+itemToPush);
+    }, function() {
+      if(getItemToPush(true)) {
+        var timeElapsed = (new Date().getTime()) - startTime;
+        work(timeout - timeElapsed, cb);
+      } else {
+        localStorage.setItem('_shadowSyncStatus') == 'idle';
+        cb();
+      }
+    }, timeout);
+  }
+  function work(timeout, cb) {
+    var startTime = (new Date().getTime());
     console.log('sync working for '+timeout+' milliseconds:');
     if(localStorage.getItem('_shadowSyncStatus') == 'pulling') {
-      console.log('resume pulling');
+      resumePulling(timeout, cb);
     } else if(localStorage.getItem('_shadowSyncStatus') == 'pushing') {
-      console.log('resume pushing');
+      resumePushing(timeout, cb);
     } else {
       console.log('nothing to work on.');
     }
   }
   return {
+    setBackend: setBackend,
     start: start,
     push: push,
     work: work
