35ba4cfcbd4f2357002f31748c150f1c6e41bd3d
Michiel de Jong <michiel@unhosted.org>
2011-12-05 13:01:31 +0700

    get rid of ugly 'modules' array

--- files/requirejs/remoteStorage.js
+++ files/requirejs/remoteStorage.js
@@ -15,19 +15,7 @@
     'controller',
     'button'
   ];
-  require(modules, function(main) {//, ajax, webfinger, oauth, session, couch, sync, versioning, controller, button) {
-/*    var exports = {
-      main: main,
-      ajax: ajax.ajax,
-      webfinger: webfinger.webfinger,
-      oauth: oauth.oauth,
-      session: session.session,
-      couch: couch.couch,
-      sync: sync.sync,
-      versioning: versioning.versioning,
-      controller: controller.controller,
-      button: button.button
-    };*/
+  require(modules, function(main) {
     main.go();
   });
 })();
