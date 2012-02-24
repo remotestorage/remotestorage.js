a7be041982ada99253901f85992335f67695f5e5
Leon Fedotov <duejohn@gmail.com>
2012-02-24 20:10:10 +0200

    refactoring & small touches

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,90 +1,88 @@
-define([
-  'require',
-  './lib/ajax',
-  './lib/couch',
-  './lib/dav',
-  './lib/webfinger'
-], function(require, ajax, couch, dav, webfinger) {
-  function onError(code, msg) {
-    alert(msg);
-  }
-  function getStorageInfo(userAddress, cb) {
-    webfinger.getAttributes(userAddress, {
-      allowHttpWebfinger: true,
-      allowSingleOriginWebfinger: false,
-      allowFakefinger: true
-    }, function(err, data) {
-      cb(err, null);
-    }, function(attributes) {
-      cb(0, attributes);
-      var storageAddresses = {};
-    });
-  }
-  function createOAuthAddress(storageInfo, categories, redirectUri) {
-     var terms =
-          ['redirect_uri='+encodeURIComponent(redirectUri),
+define(
+  ['require', './lib/ajax', './lib/couch', './lib/dav', './lib/webfinger'], 
+  function (require, ajax, couch, dav, webfinger) {
+    var onError = function (code, msg) {
+        console.log(msg);
+      },
+      getStorageInfo = function (userAddress, cb) {
+        webfinger.getAttributes(
+          userAddress, {
+            allowHttpWebfinger: true,
+            allowSingleOriginWebfinger: false,
+            allowFakefinger: true
+          }, 
+          function (err, data) {
+            cb(err, null);
+          }, 
+          function (attributes) {
+            cb(0, attributes);
+            var storageAddresses = {};
+          }
+        );
+      }, 
+      createOAuthAddress = function (storageInfo, categories, redirectUri) {
+        var terms = [
+          'redirect_uri='+encodeURIComponent(redirectUri),
           'scope='+encodeURIComponent(categories.join(',')),
           'response_type=token',
-          'client_id='+encodeURIComponent(redirectUri)];
-     if(storageInfo.auth.indexOf('?') == -1) {
-       return storageInfo.auth+'?'+terms.join('&');
-     } else {
-       return storageInfo.auth+'&'+terms.join('&');
-     }
-  }
-  function getDriver(api, cb) {
-    if(api == 'CouchDB') {
-      require(['./lib/couch'], cb);
-    } else {//'simple', 'WebDAV'
-      require(['./lib/dav'], cb);
-    }
-  }
-  function createClient(storageInfo, category, token) {
-    var storageAddress = webfinger.resolveTemplate(storageInfo.template, category)
-    return {
-      get: function(key, cb) {
-       getDriver(storageInfo.api, function(d) {
-         d.get(storageAddress, token, key, cb);
-       });
+          'client_id='+encodeURIComponent(redirectUri)
+        ];
+        return storageInfo.auth + (storageInfo.auth.indexOf('?') === -1?'?':'&') + terms.join('&');
       },
-      put: function(key, value, cb) {
-       getDriver(storageInfo.api, function(d) {
-         d.put(storageAddress, token, key, value, cb);
-       });
+      getDriver = function (api, cb) {
+        require([api === 'CouchDB'?'./lib/couch':'./lib/dav'], cb);
       },
-      delete: function(key, cb) {
-       getDriver(storageInfo.api, function(d) {
-         d.delete(storageAddress, token, key, cb);
-       });
-      }
-    };
-  }
-  function receiveToken() {
-    if(location.hash.length == 0) {
-      return null;
-    }
-    var params = location.hash.split('&');
-    for(var i = 0; i < params.length; i++){
-      if(params[i].length && params[i][0] =='#') {
-        params[i] = params[i].substring(1);
-      }
-      var kv = params[i].split('=');
-      if(kv.length >= 2) {
-        if(kv[0]=='access_token') {
-          var token = unescape(kv[1]);//unescaping is needed in chrome, otherwise you get %3D%3D at the end instead of ==
-          for(var i = 2; i < kv.length; i++) {
-            token += '='+kv[i];
+      createClient = function (storageInfo, category, token) {
+        var storageAddress = webfinger.resolveTemplate(storageInfo.template, category);
+        return {
+          get: function (key, cb) {
+            getDriver(storageInfo.api, function (d) {
+              d.get(storageAddress, token, key, cb);
+            });
+          },
+          put: function (key, value, cb) {
+            getDriver(storageInfo.api, function (d) {
+              d.put(storageAddress, token, key, value, cb);
+            });
+          },
+          'delete': function (key, cb) {
+            getDriver(storageInfo.api, function (d) {
+              d['delete'](storageAddress, token, key, cb);
+            });
           }
-          return token;
+        };
+      },
+      receiveToken = function () {
+        var params, kv;
+        /**
+          this needs more attention.
+        **/
+        if(location.hash.length > 0) { 
+          params = location.hash.split('&');
+          for(var i = 0, il = params.length; i < il; i++) {
+            if(params[i].length && params[i][0] ==='#') {
+              params[i] = params[i].substring(1);
+            }
+            kv = params[i].split('=');
+            if(kv.length >= 2) {
+              if(kv[0]==='access_token') {
+                ///XXX: ok im guessing its a base64 string and you somehow adding an = sign to the end of it ok, why?
+                var token = unescape(kv[1]);//unescaping is needed in chrome, otherwise you get %3D%3D at the end instead of ==
+                for(var j = 2,jl = kv.length; i < jl; i++) {
+                  token += '='+kv[i]; 
+                }
+                return token;
+              }
+            }
+          }
         }
-      }
-    }
-    return null;
-  }
+        return null;
+      };
+
   return {
-    getStorageInfo: getStorageInfo,
-    createOAuthAddress: createOAuthAddress,
-    createClient: createClient,
-    receiveToken: receiveToken
+    getStorageInfo     : getStorageInfo,
+    createOAuthAddress : createOAuthAddress,
+    createClient       : createClient,
+    receiveToken       : receiveToken
   };
 });
