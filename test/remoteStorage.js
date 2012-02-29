f3e80b0ece4a82d861c4b2bc79be3ac8ddffbe9f
Michiel de Jong <michiel@unhosted.org>
2012-02-29 22:01:39 +0000

    clean up receiveToken function and add tests for it

--- test/remoteStorage.js
+++ test/remoteStorage.js
@@ -54,25 +54,14 @@ define(
       },
       receiveToken = function () {
         var params, kv;
-        /**
-          this needs more attention.
-        **/
         if(location.hash.length > 0) {
           params = location.hash.split('&');
           for(var i = 0; i < params.length; i++) {
-            if(params[i].length && params[i][0] === '#') {
+            if(params[i][0]=='#') {
               params[i] = params[i].substring(1);
             }
-            kv = params[i].split('=');
-            if(kv.length >= 2) {
-              if(kv[0]==='access_token') {
-                ///XXX: ok im guessing its a base64 string and you somehow adding an = sign to the end of it ok, why?
-                var token = unescape(kv[1]);//unescaping is needed in chrome, otherwise you get %3D%3D at the end instead of ==
-                for(var i = 2; i < kv.length; i++) {
-                  token += '='+kv[i];
-                }
-                return token;
-              }
+            if(params[i].substring(0, 'access_token='.length)=='access_token=') {
+              return params[i].substring('access_token='.length);
             }
           }
         }
