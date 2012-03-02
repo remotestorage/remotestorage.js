13e5031037e9a4064dcf6da0a0577c78dc5fde55
Michiel de Jong <michiel@unhosted.org>
2012-03-02 15:10:51 +0000

    attempt to move tests 2 and 3

--- spec/remoteStorage.js
+++ spec/remoteStorage.js
@@ -14,6 +14,56 @@ require(['./test/remoteStorage'], function(remoteStorage){
         expect(ajaxCalls[0].timeout).toEqual(3000);
       });
     });
+    it("should fail on lrdd 403", function() {
+      ajaxResponses = [{
+        success: false,
+        err: 403
+      }, {
+        success: true,
+        data: '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n'
+          +'<XRD xmlns=\'http://docs.oasis-open.org/ns/xri/xrd-1.0\'\n'
+          +'     xmlns:hm=\'http://host-meta.net/xrd/1.0\'>\n'
+          +'          <Link rel=\'lrdd\''
+          +' template=\'http://unhosted.org/.well-known/{uri}.webfinger\'></Link></XRD>'
+      }];
+      ajaxCalls = [];
+      remoteStorage.getStorageInfo('a@b.c', function(err, storageInfo) {
+        expect(err).toEqual('the template doesn\'t contain "{uri}"');
+        expect(storageInfo).toEqual(null);
+        expect(ajaxCalls.length).toEqual(2); 
+        expect(ajaxCalls[0].url).toEqual('https://b.c/.well-known/host-meta');
+        expect(ajaxCalls[0].timeout ).toEqual(3000);
+        expect(ajaxCalls[1].url).toEqual('http://unhosted.org/.well-known/acct:a@b.c.webfinger');
+        expect(ajaxCalls[1].timeout).toEqual(3000);
+      });
+    });
+    it("should succeed in getting a valid xml-based webfinger record", function() {
+      ajaxResponses = [{
+        success: true,
+        data: '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<XRD xmlns=\'http://docs.oasis-open.org/ns/xri/xrd-1.0\' xmlns:hm=\'http://host-meta.net/xrd/1.0\'>\n'
+          +'<Link rel=\'remoteStorage\' api=\'simple\' auth=\'http://surf.unhosted.org:4000/_oauth/michiel@unhosted.org\''
+          +' template=\'http://surf.unhosted.org:4000/michiel@unhosted.org/{category}/\'></Link></XRD>'
+      }, {
+        success: true,
+        data: '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n'
+          +'<XRD xmlns=\'http://docs.oasis-open.org/ns/xri/xrd-1.0\'\n'
+          +'     xmlns:hm=\'http://host-meta.net/xrd/1.0\'>\n'
+          +'          <Link rel=\'lrdd\''
+          +' template=\'http://unhosted.org/.well-known/{uri}.webfinger\'></Link></XRD>'
+      }];
+      ajaxCalls = [];
+      remoteStorage.getStorageInfo('a@b.c', function(err, storageInfo) {
+        expect(err).toEqual(0);
+        expect(storageInfo.api).toEqual('simple');
+        expect(storageInfo.template).toEqual('http://surf.unhosted.org:4000/michiel@unhosted.org/{category}/');
+        expect(storageInfo.auth).toEqual('http://surf.unhosted.org:4000/_oauth/michiel@unhosted.org');
+        expect(ajaxCalls.length).toEqual(2);
+        expect(ajaxCalls[0].url).toEqual('https://b.c/.well-known/host-meta');
+        expect(ajaxCalls[0].timeout).toEqual(3000);
+        expect(ajaxCalls[1].url).toEqual('http://unhosted.org/.well-known/acct:a@b.c.webfinger');
+        expect(ajaxCalls[1].timeout).toEqual(3000);
+      });
+    });
   });
   describe("OAuth helpers", function() {
     it("should create an OAuth address", function() {//test 4
