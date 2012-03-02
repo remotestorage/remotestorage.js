a90502c3bbae0d74feaaad1364642d789ec08c96
Michiel de Jong <michiel@unhosted.org>
2012-03-02 14:53:09 +0000

    migrated tests 9,10 and 11 to jasmine

--- spec/remoteStorage.js
+++ spec/remoteStorage.js
@@ -1,8 +1,7 @@
 require(['./remoteStorage'], function(remoteStorage){
 
   describe("OAuth helpers", function() {
-
-    it("should create an OAuth address", function() {
+    it("should create an OAuth address", function() {//test 4
       var redirectUri = 'http://unhosted.org/asdf/qwer.html';
       var oauthAddress = remoteStorage.createOAuthAddress(
         {auth:'http://surf.unhosted.org:4000/_oauth/michiel@unhosted.org'},
@@ -13,7 +12,21 @@ require(['./remoteStorage'], function(remoteStorage){
         'http://surf.unhosted.org:4000/_oauth/michiel@unhosted.org?redirect_uri='+encodeURIComponent(redirectUri)+'&scope=asdf&response_type=token&client_id='+encodeURIComponent(redirectUri)
         );
     });
-
+    it("should receive a token from the fragment, first position", function() {//test 9
+      location.hash='#access_token=asdf&bla';
+      var token = remoteStorage.receiveToken();
+      expect(token).toEqual('asdf');
+    });
+    it("should receive a token from the fragment, middle position", function() {//test 10
+      location.hash='#a=b&access_token=asdf&bla=wa';
+      var token = remoteStorage.receiveToken();
+      expect(token).toEqual('asdf');
+    });
+    it("should receive a token from the fragment, middle position", function() {//test 11
+      location.hash='#foo=bar&access_token=asdf';
+      var token = remoteStorage.receiveToken();
+      expect(token).toEqual('asdf');
+    });
   });
 
 });
