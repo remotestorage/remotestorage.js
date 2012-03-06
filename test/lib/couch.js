008cc109c0c55f018feccf46787f5641d4af03d2
Michiel de Jong <michiel@unhosted.org>
2012-03-06 16:55:49 +0000

    passing tests

--- test/lib/couch.js
+++ test/lib/couch.js
@@ -14,7 +14,11 @@ define(['./ajax'], function(ajax) {
         url: key,
         method: method,
         error: function(err) {
-          cb(err, null);
+          if(err == 404) {
+            cb(null, undefined);
+          } else {
+            cb(err, null);
+          }
         },
         success: function(data) {
           cb(null, data);
@@ -62,7 +66,8 @@ define(['./ajax'], function(ajax) {
       });
     }
     function delete_(storageAddress, token, key, cb) {
-      doCall('DELETE', storageAddress+normalizeKey(key), null, token, cb);
+      var revision = localStorage.getItem('_shadowCouchRev_'+key);
+      doCall('DELETE', storageAddress+normalizeKey(key)+'?rev='+revision, null, token, cb);
     }
     return {
       get: get,
