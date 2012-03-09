820a748fb7ca7e2ed8790488263f6533f0099ae1
Michiel de Jong <michiel@unhosted.org>
2012-03-09 10:37:48 +0000

    move all the folders around

--- build.sh
+++
@@ -1,2 +0,0 @@
-cat lib/start.frag lib/ajax.js lib/couch.js lib/dav.js lib/webfinger.js lib/main.js lib/end.frag  > remoteStorage.js
-cat remoteStorage.js | uglifyjs > builds/latest/remoteStorage.js
