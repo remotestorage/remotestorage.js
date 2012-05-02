cf7eb87f632eda935f6f88eb19ea29b8aa955d2d
Michiel de Jong <michiel@unhosted.org>
2012-05-02 18:05:54 +0200

    deal with non-string user address in getStorageInfo and set storageInfo.rel to storageInfo.type

--- src/remoteStorage.js
+++ src/remoteStorage.js
@@ -2,15 +2,20 @@ define(
   ['require', './lib/platform', './lib/couch', './lib/dav', './lib/webfinger', './lib/hardcoded'],
   function (require, platform, couch, dav, webfinger, hardcoded) {
     var getStorageInfo = function (userAddress, cb) {
-        webfinger.getStorageInfo(userAddress, {timeout: 3000}, function(err, storageInfo) {
-          if(err) {
-            hardcoded.guessStorageInfo(userAddress, {timeout: 3000}, function(err2, data) {
-              cb(err2, data);
-            });
-          } else {
-            cb(err, storageInfo);
-          }
-        });
+        if(typeof(userAddress) != 'string') {
+          cb('user address should be a string');
+        } else {
+          webfinger.getStorageInfo(userAddress, {timeout: 3000}, function(err, storageInfo) {
+            if(err) {
+              hardcoded.guessStorageInfo(userAddress, {timeout: 3000}, function(err2, data) {
+                cb(err2, data);
+              });
+            } else {
+              storageInfo.rel=storageInfo.type;//support both while we settle down the syntax
+              cb(err, storageInfo);
+            }
+          });
+        }
       },
       createOAuthAddress = function (storageInfo, scopes, redirectUri) {
         if(storageInfo.type=='https://www.w3.org/community/rww/wiki/Simple') {
