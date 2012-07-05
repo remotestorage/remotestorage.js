b4343e7d1644db9cd6ff891129b4f6444dc5fc28
Michiel de Jong <michiel@unhosted.org>
2012-07-05 23:25:07 +0300

    1 spec 0 failures

--- test/spec/modules/tasks.js
+++ test/spec/modules/tasks.js
@@ -2,7 +2,14 @@
   describe("tasks", function() {
     it("should get a private list", function() {
       var list = module.exports.getPrivateList('a');
-      expect(list).toEqual('a');
+      expect(typeof(list.getIds)).toEqual('function');
+      expect(typeof(list.get)).toEqual('function');
+      expect(typeof(list.set)).toEqual('function');
+      expect(typeof(list.add)).toEqual('function');
+      expect(typeof(list.remove)).toEqual('function');
+      expect(typeof(list.markCompleted)).toEqual('function');
+      expect(typeof(list.getStats)).toEqual('function');
+      expect(typeof(list.on)).toEqual('function');
     });
   });
 })();
