11172b0d43f8cec519bdadb4d6691739dadf8ebf
Michiel de Jong <michiel@unhosted.org>
2012-07-07 17:31:28 +0300

    setting testing address to admin@mich.oc

--- src/lib/widget.js
+++ src/lib/widget.js
@@ -115,7 +115,7 @@ define(['./webfinger', './hardcoded', './session', './sync', './store', './platf
       +'  <a id="remotestorage-questionmark" href="http://unhosted.org/#remotestorage" target="_blank">?</a>'//question mark
       +'  <span class="infotext" id="remotestorage-infotext">This app allows you to use your own data storage!<br>Click for more info on the Unhosted movement.</span>'//info text
       //+'  <input id="remotestorage-useraddress" type="text" placeholder="you@remotestorage" autofocus >'//text input
-      +'  <input id="remotestorage-useraddress" type="text" value="michiel@5apps.com" placeholder="you@remotestorage" autofocus >'//text input
+      +'  <input id="remotestorage-useraddress" type="text" value="admin@mich.oc" placeholder="you@remotestorage" autofocus >'//text input
       +'  <a class="infotext" href="http://unhosted.org" target="_blank" id="remotestorage-devsonly">Local use only, no async sync yet. But modules work!<br>Click for more info on the Unhosted movement.</a>'
       +'</div>';
     platform.setElementHTML(connectElement, html);
