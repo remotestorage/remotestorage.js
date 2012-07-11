03f38ad506730dd7d0fac7dbd920badb4019278f
Michiel de Jong <michiel@unhosted.org>
2012-07-11 17:37:21 +0300

    xhr.getAllResponseHeaders()

--- build/latest/remoteStorage-debug.js
+++ build/latest/remoteStorage-debug.js
@@ -65,7 +65,7 @@ define('lib/platform',[], function() {
           window.clearTimeout(timer);
         }
         if(xhr.status==200 || xhr.status==201 || xhr.status==204 || xhr.status==207) {
-          params.success(xhr.responseText, xhr.responseHeaders);
+          params.success(xhr.responseText, xhr.getAllResponseHeaders());
         } else {
           params.error(xhr.status);
         }
