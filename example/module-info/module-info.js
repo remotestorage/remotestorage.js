a3b505014413fc62e6714c332530fda7c76d3f98
() <nilclass@riseup.net>
2012-07-31 07:34:09 +0200

    fixed module info. also some styling for it. not so beautiful, but better than before.

--- example/module-info/module-info.js
+++ example/module-info/module-info.js
@@ -1,27 +1,81 @@
 
 (function() {
 
-  var ul = document.createElement('ul');
-  var moduleNames = remoteStorage.getModuleList(), moduleName, module, li;
+  var h = {
+    div: function(className, content) {
+      var div = this.tag('div', content);
+      div.setAttribute('class', className);
+      return div;
+    },
+
+    tag: function(tagName, content) {
+      var tag = document.createElement(tagName);
+      if(content) {
+        tag.innerHTML = content;
+      }
+      return tag;
+    }
+  }
+
+  function renderDataHints(module) {
+    var hints = h.div('hints');
+
+    if(module.dataHints) {
+      var hint, key, value;
+      var hintKeys = Object.keys(module.dataHints);
+      var heading = document.createElement('h3');
+      heading.innerHTML = 'info';
+      hints.appendChild(heading);
+      for(var k in hintKeys) {
+        var hint = h.div('hint');
+        hint.appendChild(h.div('key', hintKeys[k]));
+        hint.appendChild(h.div('value', module.dataHints[ hintKeys[k] ]));
+        hints.appendChild(hint);
+      }
+    } else {
+      hints.appendChild(h.div(
+        'info',
+        "(No information provided)"
+      ));
+    }
+
+    return hints;
+  }
+
+  function renderMethods(moduleInstance) {
+    var methods = h.div('hints');
+
+    methods.appendChild(h.tag('h2', 'methods'));
+
+    var methodKeys = Object.keys(moduleInstance);
+    for(var m=0;m<methodKeys.length;m++) {
+      var method = h.div('hint');
+      method.appendChild(h.div('key', methodKeys[m]));
+      methods.appendChild(method);
+    }
+
+    return methods;
+  }
+
+  var ul = document.createElement('ul'), li;
+  var moduleNames = remoteStorage.getModuleList(), moduleName, module;
 
   for(var m in moduleNames) {
     moduleName = moduleNames[m];
 
     li = document.createElement('li');
-    ul.appendChild(li);
-    li.innerHTML  = '<h2>' + moduleName + '</h2>';
+    li.innerHTML  = '<h2>Module: ' + moduleName + '</h2>';
 
     module = remoteStorage.getModuleInfo(moduleName);
 
-    if(module.dataHints) {
-      li.innerHTML += '<dl>';
-      var hintKeys = Object.keys(module.dataHints);
-      for(var h in hintKeys) {
-        li.innerHTML += '<dt>' + hintKeys[h] + '</dt>';
-        li.innerHTML += '<dd>' + module.dataHints[ hintKeys[h] ] + '</dd>';
-      }
-      li.innerHTML += '</dl>';
-    }
+    li.appendChild(renderDataHints(module));
+
+    remoteStorage.loadModule(moduleName)
+    moduleInstance = remoteStorage[moduleName];
+
+    li.appendChild(renderMethods(moduleInstance));
+
+    ul.appendChild(li);
   }
 
   window.onload = function() {
