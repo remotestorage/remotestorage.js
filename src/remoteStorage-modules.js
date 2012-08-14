76396cdb3a848ace4985b7275e9dd80accc7505d
Niklas Cathor <niklas.cathor@loxi.biz>
2012-08-14 14:58:05 +0200

    added root module to remoteStorage-modules

--- src/remoteStorage-modules.js
+++ src/remoteStorage-modules.js
@@ -1,8 +1,9 @@
 define('remoteStorage-modules', [
-  './modules/calendar', 
+  './modules/root',
+  './modules/calendar',
   './modules/deps/vcardjs-0.2.js',
-  './modules/contacts', 
-  './modules/documents', 
+  './modules/contacts',
+  './modules/documents',
   './modules/money',
   './modules/tasks'
 ], function() {
