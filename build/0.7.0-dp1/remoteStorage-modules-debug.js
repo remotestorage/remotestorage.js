6ea868a139e0646c4e41748f6b438afdca67de53
Niklas Cathor <niklas.cathor@loxi.biz>
2012-08-14 14:58:52 +0200

    built remoteStorage

--- build/0.7.0-dp1/remoteStorage-modules-debug.js
+++ build/0.7.0-dp1/remoteStorage-modules-debug.js
@@ -39,9 +39,159 @@
     return exports[name].apply({}, modList);
   }
 
+remoteStorage.defineModule('public', function(client) {
+    function getPublicItems()
+    {
+        return client.getObject("publishedItems");
+    }
+
+    return {
+        exports: {
+            getPublicItems: getPublicItems,
+            getObject: client.getObject
+        }
+    }
+});
+
+remoteStorage.defineModule('root', function(myPrivateBaseClient, myPublicBaseClient) {
+    function setOnChange(cb)
+    {
+        myPrivateBaseClient.on('change', function(e)
+        {
+            console.log(e); cb(e);
+        });
+        myPublicBaseClient.on('change', function(e)
+        {
+            console.log(e); cb(e);
+        });
+    }
+
+    function addToPublicItems(path)
+    {
+        var data = myPublicBaseClient.getObject("publishedItems");
+        if(path[0] == "/")
+            path = path.substr(1);
+
+        if(data)
+        {
+            if(data.indexOf(path) == -1)
+            {
+                data.unshift(path);
+            }
+        }
+        else
+        {
+            data = [];
+            data.push(path);
+        }
+        myPublicBaseClient.storeObject('array', "publishedItems", data);
+    }
+
+    function removeFromPublicItems(path)
+    {
+        var data = myPublicBaseClient.getObject("publishedItems");
+        if(path[0] == "/")
+            path = path.substr(1);
+        if(data)
+        {
+            if($.inArray(path, data) != true)
+            {
+                data.pop(path);
+            }
+        }
+        else
+        {
+            data = [];
+        }
+        myPublicBaseClient.storeObject('array', "publishedItems", data);
+    }
+
+    function publishObject(path)
+    {
+        if(pathIsPublic(path))
+            return 'Object has already been made public';
+
+        var data = myPrivateBaseClient.getObject(path);
+        var publicPath = "/public" + path;
+        addToPublicItems(publicPath);
+        myPrivateBaseClient.remove(path);
+        myPrivateBaseClient.storeObject(data['@type'], publicPath, data);
+
+        return "Object " + path + " has been published to " + publicPath;
+    }
+
+    function archiveObject(path)
+    {
+        if(!pathIsPublic(path))
+            return 'Object has already been made private';
+
+        var data = myPrivateBaseClient.getObject(path);
+        var privatePath = path.substring(7, path.length);
+        removeFromPublicItems(path);
+        myPrivateBaseClient.remove(path);
+        myPrivateBaseClient.storeObject(data['@type'], privatePath, data);
+
+        return "Object " + path + " has been archived to " + privatePath;
+    }
+
+    function pathIsPublic(path)
+    {
+        if(path.substring(0, 8) == "/public/")
+            return true;
+        return false;
+    }
+
+    function getClient(path)
+    {
+        if(!pathIsPublic(path))
+            return myPrivateBaseClient;
+        return myPublicBaseClient;
+    }
+
+    function getObject(path, cb, contex)
+    {
+       var client = getClient(path);
+        return client.getObject(path, cb, contex);
+    }
+
+    function setObject(type, path, obj)
+    {
+        var client = getClient(path);
+        client.storeObject(type, path, obj);
+    }
+
+    function removeObject(path)
+    {
+        var client = getClient(path);
+        client.remove(path);
+    }
+
+    function getListing(path, cb, context)
+    {
+        var client = getClient(path);
+        return client.getListing(path, cb, context);
+    }
+
+    return {
+        exports: {
+            getListing: getListing,
+            getObject: getObject,
+            setObject: setObject,
+            removeObject: removeObject,
+            archiveObject: archiveObject,
+            publishObject: publishObject,
+            setOnChange:setOnChange
+        }
+    }
+});
+
+
+
+define("modules/root", function(){});
+
 remoteStorage.defineModule('calendar', function(privateBaseClient) {
   // callback expects a list of objects with the itemId and itemValue properties set
-  privateBaseClient.sync('/');
+  //privateBaseClient.sync('/');
   function getEventsForDay(day) {
     var ids = privateBaseClient.getListing(day+'/');
     var list = [];
@@ -1323,10 +1473,11 @@ remoteStorage.defineModule('tasks', function(myPrivateBaseClient, myPublicBaseCl
 define("modules/tasks", function(){});
 
 define('remoteStorage-modules', [
-  './modules/calendar', 
+  './modules/root',
+  './modules/calendar',
   './modules/deps/vcardjs-0.2.js',
-  './modules/contacts', 
-  './modules/documents', 
+  './modules/contacts',
+  './modules/documents',
   './modules/money',
   './modules/tasks'
 ], function() {
