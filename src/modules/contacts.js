a0cb67a18f944909d0e1280b3cea1d7a04f164a3
() <nilclass@riseup.net>
2012-09-25 12:39:43 +0200

    fixed dependency for contacts module

--- src/modules/contacts.js
+++ src/modules/contacts.js
@@ -3,8 +3,7 @@
  ** Skeleton for new modules
  **/
 
-define(['../remoteStorage', './deps/vcardjs-0.2'], function(remoteStorage, vCardJS) {
-
+define(['../remoteStorage', 'modules/deps/vcardjs-0.2'], function(remoteStorage, vCardJS) {
   var moduleName = "contacts";
 
   var VCard = vCardJS.VCard, VCF = vCardJS.VCF;
