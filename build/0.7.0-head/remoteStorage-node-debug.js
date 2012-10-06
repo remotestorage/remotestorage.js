125349d9bf0a1e9e3247c57702b2876dd7fb5b19
() <nilclass@riseup.net>
2012-10-06 18:47:52 +0200

    build: Sat Oct  6 16:47:52 UTC 2012

--- build/0.7.0-head/remoteStorage-node-debug.js
+++ build/0.7.0-head/remoteStorage-node-debug.js
@@ -1363,6 +1363,11 @@ define('lib/getputdelete',
     }
 
     function put(url, value, mimeType, token, cb) {
+
+      if(! value) {
+        cb("no value set");
+      }
+
       logger.info('calling PUT '+url, ' (' + value.length + ')');
       doCall('PUT', url, value, mimeType, token, function(err, data) {
         //logger.debug('cb from PUT '+url);
