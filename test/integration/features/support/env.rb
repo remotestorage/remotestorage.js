d12011d2bec8850c8d03cdffc7495643f73f48fb
() <nilclass@riseup.net>
2012-10-25 20:34:17 +0200

    configured cucumber env to run with selenium-server-standalone

--- test/integration/features/support/env.rb
+++ test/integration/features/support/env.rb
@@ -7,6 +7,17 @@ rescue Exception
   ## ignored
 end
 
+require 'selenium/webdriver'
+
+caps = Selenium::WebDriver::Remote::Capabilities.firefox
+caps.version = "8"
+
 Capybara.default_wait_time = 5
 Capybara.default_driver = :selenium
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :url => "http://localhost:4444/wd/hub",
+    :desired_capabilities => caps)
+end
 Capybara.app_host = 'http://localhost:3000'
