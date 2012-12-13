50c8683c6e7b1e179e81942cd15a399d15d3f107
() <nilclass@riseup.net>
2012-12-13 17:33:44 +0100

    change StorageAdapter interface to transactions: memory store & test

--- test/unit/store/memory-suite.js
+++ test/unit/store/memory-suite.js
@@ -36,19 +36,26 @@ define(['requirejs', 'fs'], function(requirejs, fs) {
         desc: "all methods return promises",
         run: function(env) {
           this.assertTypeAnd(
-            env.memory.get('/foo').then, 'function'
-          );
-          this.assertTypeAnd(
-            env.memory.set('/foo', { data: 'bar' }).then, 'function'
-          );
-          this.assertTypeAnd(
-            env.memory.remove('/foo').then, 'function'
+            env.memory.transaction(true, function() {}).then, 'function'
           );
           this.assertType(
             env.memory.forgetAll().then, 'function'
           );
         }
       },
+
+      {
+        desc: "transactions yield a get/set/remove object",
+        run: function(env) {
+          var _this = this;
+          env.memory.transaction(true, function(transaction) {
+            _this.assertTypeAnd(transaction.get, 'function');
+            _this.assertTypeAnd(transaction.set, 'function');
+            _this.assertType(transaction.remove, 'function');
+          });
+        }
+      },
+
       {
         desc: "results are consistent",
         run: function(env) {
@@ -59,29 +66,30 @@ define(['requirejs', 'fs'], function(requirejs, fs) {
             return curry(_this.assertAnd.bind(_this), a);
           }
 
-          env.memory.
+          env.memory.transaction(true, function(transaction) {
             // set
-            set('/one-path', 'data').
-            // get
-            then(curry(env.memory.get, '/one-path')).
-            then(assertAnd('data')).
-            then(curry(env.memory.get, '/other-path')).
-            then(assertAnd(undefined)).
-            // remove
-            then(curry(env.memory.remove, '/one-path')).
-            then(curry(env.memory.get, '/one-path')).
-            then(assertAnd(undefined)).
-            // forgetAll
-            then(curry(env.memory.set, '/a', 1)).
-            then(curry(env.memory.set, '/b', 2)).
-            then(env.memory.forgetAll).
-            then(curry(env.memory.get, '/a')).
-            then(assertAnd(undefined)).
-            then(curry(env.memory.get, '/b')).
-            then(assertAnd(undefined)).
-            // final result
-            then(curry(this.result.bind(this), true),   // success
-                 curry(this.result.bind(this), false)); // error
+            transaction.set('/one-path', 'data').
+              // get
+              then(curry(transaction.get, '/one-path')).
+              then(assertAnd('data')).
+              then(curry(transaction.get, '/other-path')).
+              then(assertAnd(undefined)).
+              // remove
+              then(curry(transaction.remove, '/one-path')).
+              then(curry(transaction.get, '/one-path')).
+              then(assertAnd(undefined)).
+              // forgetAll
+              then(curry(transaction.set, '/a', 1)).
+              then(curry(transaction.set, '/b', 2)).
+              then(env.memory.forgetAll).
+              then(curry(transaction.get, '/a')).
+              then(assertAnd(undefined)).
+              then(curry(transaction.get, '/b')).
+              then(assertAnd(undefined)).
+              // final result
+              then(curry(_this.result.bind(_this), true),   // success
+                   curry(_this.result.bind(_this), false)); // error
+          });
         }
       }
     ]
