ece37b1564711589a70b2c3a951e13cd81b8bdc9
() <nilclass@riseup.net>
2012-12-13 17:33:44 +0100

    change StorageAdapter interface to transactions: memory store & test

--- src/lib/store/memory.js
+++ src/lib/store/memory.js
@@ -11,21 +11,28 @@ define(['../util'], function(util) {
     return {
       on: function() {},
 
-      get: function(path) {
-        logger.info('get', path);
-        return util.getPromise().fulfillLater(nodes[path]);
-      },
+      transaction: function(write, body) {
+        return util.makePromise(function(promise) {
+          body({
+            get: function(path) {
+              logger.info('get', path);
+              return util.getPromise().fulfillLater(nodes[path]);
+            },
 
-      set: function(path, node) {
-        logger.info('set', path);
-        nodes[path] = node;
-        return util.getPromise().fulfillLater();
-      },
+            set: function(path, node) {
+              logger.info('set', path);
+              nodes[path] = node;
+              return util.getPromise().fulfillLater();
+            },
 
-      remove: function(path) {
-        logger.info('remove', path);
-        delete nodes[path];
-        return util.getPromise().fulfillLater();
+            remove: function(path) {
+              logger.info('remove', path);
+              delete nodes[path];
+              return util.getPromise().fulfillLater();
+            }
+          });
+          promise.fulfillLater();
+        });
       },
 
       forgetAll: function() {
