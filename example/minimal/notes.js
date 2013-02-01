49b1f93afb69c2677821ed54f1270edd8b554e1d
Michiel de Jong <michiel@unhosted.org>
2013-02-02 02:20:31 +0800

    restore getNote()

--- example/minimal/notes.js
+++ example/minimal/notes.js
@@ -5,6 +5,11 @@ remoteStorage.defineModule('notes', function(privateClient, publicClient) {
       setNote: function (text) {
         return privateClient.storeFile('text/plain', 'note.txt', text);
       },
+      getNote: function (cb) {
+        privateClient.getFile('note.txt').then(function(obj) {
+          cb(obj.data);
+        });
+      },
       onChange: function (cb) {
         privateClient.on('change', cb);
       }
