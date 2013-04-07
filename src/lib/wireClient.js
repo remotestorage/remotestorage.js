162330a0278fcdd5d15afe7f26905d793162f057
() <nilclass@riseup.net>
2013-04-07 15:27:46 +0200

    busy / unbusy events for wireClient to indicate PUT and DELETE requests in progress

--- src/lib/wireClient.js
+++ src/lib/wireClient.js
@@ -7,11 +7,27 @@ define(['./getputdelete', './util'], function (getputdelete, util) {
 
   var prefix = 'remote_storage_wire_';
 
-  var events = util.getEventEmitter('connected', 'disconnected', 'error');
+  var events = util.getEventEmitter('connected', 'disconnected', 'error', 'busy', 'unbusy');
 
   var state = 'anonymous';
 
   var settings = util.getSettingStore('remotestorage_wire');
+
+  var busyCounter = 0;
+
+  function setBusy() {
+    if(busyCounter === 0) {
+      events.emit('busy');
+    }
+    busyCounter++;
+  }
+
+  function releaseBusy() {
+    busyCounter--;
+    if(busyCounter === 0) {
+      events.emit('unbusy');
+    }
+  }
 
   function setSetting(key, value) {
     settings.set(key, value);
@@ -128,7 +144,9 @@ define(['./getputdelete', './util'], function (getputdelete, util) {
          !(typeof(valueStr) == 'object' && valueStr instanceof ArrayBuffer)) {
         valueStr = JSON.stringify(valueStr);
       }
-      return getputdelete.set(resolveKey(path), valueStr, mimeType, token);
+      setBusy();
+      return getputdelete.set(resolveKey(path), valueStr, mimeType, token).
+        then(releaseBusy);
     }
   }
 
@@ -144,7 +162,9 @@ define(['./getputdelete', './util'], function (getputdelete, util) {
       return cb(new Error("Foreign storage is read-only"));
     }
     var token = getSetting('bearerToken');
-    return getputdelete.set(resolveKey(path), undefined, undefined, token, cb);
+    setBusy();
+    return getputdelete.set(resolveKey(path), undefined, undefined, token, cb).
+      then(releaseBusy);
   }
 
   function getStorageHrefForUser(userAddress) {
