e34b46d8a4a2533d51ec05a9ec07b85426863e95
() <nilclass@riseup.net>
2013-06-24 19:49:38 +0200

    removed 'modules' from build server file

--- build/server.js
+++ build/server.js
@@ -1,42 +1,4 @@
 
-var MODULES = {
-  files: {
-    'promising.js': 'A Promises/A+ implementation',
-    'remotestorage.js': 'Basic GET/PUT/DELETE client',
-    'remotestorage.discover.js': 'Webfinger client',
-    'remotestorage.authorize.js': 'OAuth2 client',
-    'remotestorage.indexeddb.js': 'IndexedDB caching layer',
-    'remotestorage.sync.js': 'Synchronizes remotestorage and local cache',
-    'remotestorage.widget.js': '"Connect" widget, managing discovery and authorization',
-    'remotestorage.inspect.js': 'Debugging widget that displays local and remote data',
-  },
-  groups: {
-    core: {
-      label: "Core",
-      desc: "Basic requirements to connect to a remotestorage server and send / receive data.",
-      files: ['promising.js', 'remotestorage.js', 'remotestorage.discover.js', 'remotestorage.authorize.js'],
-      required: true
-    },
-    caching: {
-      label: "Caching",
-      desc: "Local caching layer and synchronization",
-      files: ['remotestorage.indexeddb.js', 'remotestorage.sync.js'],
-      initial: true
-    },
-    widget: {
-      label: "Widget",
-      desc: '"Connect" widget, managing discovery and authorization',
-      files: ['remotestorage.widget.js'],
-      initial: true
-    },
-    debug: {
-      label: "Debug",
-      desc: "Debugging tools, useful for development",
-      files: ['remotestorage.inspect.js']
-    }
-  }
-};
-
 var FILE_ROOT = "../"
 
 var http = require('http');
@@ -58,9 +20,10 @@ function extend(a, b) {
 }
 
 http.createServer(function(req, res) {
+  var components = JSON.parse(fs.readFileSync('components.json'));
   if(req.method === 'GET') {
     res.writeHead(200, extend({'Content-Type':'application/json'}, CORS_HEADERS));
-    res.write(JSON.stringify(MODULES));
+    res.write(JSON.stringify(components));
     res.end();
   } else if(req.method === 'POST') {
     var body = '';
@@ -71,15 +34,15 @@ http.createServer(function(req, res) {
       if(! (groups instanceof Array)) {
         groups = [groups];
       }
-      for(var key in MODULES.groups) {
-        if(MODULES.groups[key].required) {
+      for(var key in components.groups) {
+        if(components.groups[key].required) {
           groups.unshift(key);
         }
       }
       var files = [];
       groups.forEach(function(group) {
-        if(MODULES.groups[group]) {
-          files = files.concat(MODULES.groups[group].files);
+        if(components.groups[group]) {
+          files = files.concat(components.groups[group].files);
         }
       });
       res.writeHead(200, extend({'Content-Type':'text/javascript','Content-Disposition':'attachment;filename=remotestorage.js'}, CORS_HEADERS));
