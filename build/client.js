912ec65162fbb1430706fcf41afcab42f7594c44
() <nilclass@riseup.net>
2013-07-01 16:12:45 +0200

    builder

--- build/client.js
+++ build/client.js
@@ -1,60 +1,110 @@
 window.onload = function() {
+  var fileLinkBase = document.body.getAttribute('data-file-link-base');
   var buildServer = document.body.getAttribute('data-build-server');
   var form = document.createElement('form');
   form.method = 'POST';
   form.action = buildServer;
-  document.body.appendChild(form);
+  document.getElementById('wrapper').appendChild(form);
+
+  var groupTemplate = document.querySelector('script[data-template-name="group"]').
+    innerHTML;
+
   var xhr = new XMLHttpRequest();
   xhr.open('GET', buildServer, true);
+  var i = 0;
   xhr.onload = function() {
-    var modules = JSON.parse(xhr.responseText);
-    for(var key in modules.groups) {
-      var group = modules.groups[key];
+    var components = JSON.parse(xhr.responseText);
+    var defaults = [];
+    for(var key in components.groups) {
+      var group = components.groups[key];
       if(group.hidden) continue;
-      var groupWrapper = document.createElement('div');
-      groupWrapper.className = 'group';
-      var checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.name = 'groups';
-      checkbox.value = key;
-      checkbox.id = 'group-' + key;
-      if(group.required) {
-        checkbox.checked = 'checked';
-        checkbox.disabled = 'disabled';
-      } else if(group.initial) {
-        checkbox.checked = 'checked';
-      }
-      groupWrapper.appendChild(checkbox);
-      var label = document.createElement('label');
-      label.textContent = group.label;
-      label.setAttribute('for', 'group-' + key);
-      groupWrapper.appendChild(label);
-      var innerWrapper = document.createElement('div');
-      innerWrapper.className = 'group-inner';
-      var desc = document.createElement('p');
-      desc.textContent = group.desc;
-      innerWrapper.appendChild(desc);
-      var filesHeading = document.createElement('strong');
-      filesHeading.textContent = "Files:";
-      innerWrapper.appendChild(filesHeading);
-      var fileList = document.createElement('ul');
-      group.files.forEach(function(f) {
-        var li = document.createElement('li');
-        var span = document.createElement('span');
-        span.className = 'filename';
-        span.textContent = f + ': ';
-        li.appendChild(span);
-        li.appendChild(document.createTextNode(modules.files[f]));
-        fileList.appendChild(li);
+
+      group.name = key;
+
+      if(group.required || group.default) defaults.push(group.name);
+
+      i++;
+
+      group.class = i % 2 == 1 ? 'odd' : '';
+
+      group.files = group.files.map(function(fileName) {
+        return {
+          name: fileName,
+          desc: components.files[fileName] || '-',
+          href: fileLinkBase + fileName
+        };
       });
-      innerWrapper.appendChild(fileList);
-      groupWrapper.appendChild(innerWrapper);
-      form.appendChild(groupWrapper);
+
+      if(group.depends) {
+        group.depends = group.depends.map(function(dependName) {
+          var dependency = components.groups[dependName];
+          if(! dependency.reverseDepends) {
+            dependency.reverseDepends = [];
+          }
+          dependency.reverseDepends.push(group.name);
+          return {
+            name: dependName,
+            label: dependency.label,
+            desc: dependency.desc
+          };
+        });
+        console.log('group', group.name, 'depends', group.depends);
+      } else {
+        group.noDepends = true;
+      }
+
+      form.innerHTML += Mustache.render(groupTemplate, group);
     }
     var submit = document.createElement('input');
     submit.type = 'submit';
-    submit.value = 'Build';
+    submit.value = 'Download';
     form.appendChild(submit);
+
+    function setGroupState(groupName, state) {
+      console.log('set state', groupName, state);
+      var checkbox = document.getElementById('group-' + groupName)
+      if(checkbox) {
+        checkbox.checked = state;
+        updateGroupState({ target: checkbox });
+      }
+    }
+
+    function processDepends(groupName, value) {
+      var group = components.groups[groupName];
+      if(value && group.depends) {
+        group.depends.forEach(function(group) {
+          setGroupState(group.name, true);
+        });
+      } else if( !value && group.reverseDepends) {
+        group.reverseDepends.forEach(function(groupName) {
+          setGroupState(groupName, false);
+        });
+      }
+    }
+
+    function updateGroupState(event) {
+      var checkbox = event.target, classList = checkbox.parentElement.classList;
+      if(checkbox.checked) {
+        classList.remove('disabled');
+      } else {
+        classList.add('disabled');
+      }
+      processDepends(checkbox.getAttribute('value'), checkbox.checked);
+    }
+    
+    Array.prototype.forEach.call(
+      document.getElementsByClassName('group-control'), function(checkbox) {
+        checkbox.addEventListener('change', updateGroupState);
+      }
+    );
+
+    setTimeout(function() {
+
+    defaults.forEach(function(groupName) {
+      setGroupState(groupName, true);
+    });
+
+    }, 0);
   };
   xhr.send();
 }
