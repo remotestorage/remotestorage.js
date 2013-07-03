013cf1c26bdaa70db4467c33132671b48ada8ef9
() <nilclass@riseup.net>
2013-07-03 19:16:13 +0200

    instantiate modules implicitly when they are accessed.

--- src/modules.js
+++ src/modules.js
@@ -4,14 +4,25 @@
 
   RemoteStorage.defineModule = function(moduleName, builder) {
     RemoteStorage.MODULES[moduleName] = builder;
+
+    Object.defineProperty(RemoteStorage.prototype, moduleName, {
+      configurable: true,
+      get: function() {
+        var instance = this._loadModule(moduleName);
+        Object.defineProperty(this, moduleName, {
+          value: instance
+        });
+        return instance;
+      }
+    });
   };
 
-  RemoteStorage.prototype.loadModule = function(moduleName) {
+  RemoteStorage.prototype._loadModule = function(moduleName) {
     var builder = RemoteStorage.MODULES[moduleName];
     if(builder) {
       var module = builder(new RemoteStorage.BaseClient(this, '/' + moduleName + '/'),
                            new RemoteStorage.BaseClient(this, '/public/' + moduleName + '/'));
-      this[moduleName] = module.exports;
+      return module.exports;
     } else {
       throw "Unknown module: " + moduleName;
     }
@@ -20,7 +31,6 @@
   RemoteStorage.prototype.defineModule = function(moduleName) {
     console.log("remoteStorage.defineModule is deprecated, use RemoteStorage.defineModule instead!");
     RemoteStorage.defineModule.apply(RemoteStorage, arguments);
-    this.loadModule(moduleName);
   };
 
 })();
