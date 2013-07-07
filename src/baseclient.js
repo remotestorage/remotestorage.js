e0dd431712fc2fc522ee40a6b0b8a63d207df22d
() <nilclass@riseup.net>
2013-07-07 16:59:56 +0200

    BaseClient documentation

--- src/baseclient.js
+++ src/baseclient.js
@@ -8,27 +8,8 @@
 
   var RS = RemoteStorage;
 
-  RS.BaseClient = function(storage, base) {
-    if(base[base.length - 1] != '/') {
-      throw "Not a directory: " + base;
-    }
-    this.storage = storage;
-    this.base = base;
-
-    var parts = this.base.split('/');
-    if(parts.length > 2) {
-      this.moduleName = parts[1];
-    } else {
-      this.moduleName = 'root';
-    }
-
-    RS.eventHandling(this, 'change', 'conflict');
-    this.on = this.on.bind(this);
-    storage.onChange(this.base, this._fireChange.bind(this));
-  };
-
   /**
-   * Class: RS.BaseClient
+   * Class: RemoteStorage.BaseClient
    *
    * Provides a high-level interface to access data below a given root path.
    *
@@ -44,6 +25,38 @@
    * <remove> operates on either objects or files (but not folders, folders are
    * created and removed implictly).
    */
+  RS.BaseClient = function(storage, base) {
+    if(base[base.length - 1] != '/') {
+      throw "Not a directory: " + base;
+    }
+    /**
+     * Property: storage
+     *
+     * The <RemoteStorage> instance this <BaseClient> operates on.
+     */
+    this.storage = storage;
+    /**
+     * Property: base
+     *
+     * Base path this <BaseClient> operates on.
+     *
+     * For the module's privateClient this would be /<moduleName>/, for the
+     * corresponding publicClient /public/<moduleName>/.
+     */
+    this.base = base;
+
+    var parts = this.base.split('/');
+    if(parts.length > 2) {
+      this.moduleName = parts[1];
+    } else {
+      this.moduleName = 'root';
+    }
+
+    RS.eventHandling(this, 'change', 'conflict');
+    this.on = this.on.bind(this);
+    storage.onChange(this.base, this._fireChange.bind(this));
+  };
+
   RS.BaseClient.prototype = {
 
     // BEGIN LEGACY
@@ -65,12 +78,41 @@
       return this;
     },
 
+    /**
+     * Method: scope
+     *
+     * Returns a new <BaseClient> operating on a subpath of the current <base> path.
+     */
     scope: function(path) {
       return new RS.BaseClient(this.storage, this.makePath(path));
     },
 
     // folder operations
 
+    /**
+     * Method: getListing
+     *
+     * Get a list of child nodes below a given path.
+     *
+     * The callback semantics of getListing are identical to those of getObject.
+     *
+     * Parameters:
+     *   path     - The path to query. It MUST end with a forward slash.
+     *
+     * Returns:
+     *   A promise for an Array of keys, representing child nodes.
+     *   Those keys ending in a forward slash, represent *directory nodes*, all
+     *   other keys represent *data nodes*.
+     *
+     * Example:
+     *   (start code)
+     *   client.getListing('').then(function(listing) {
+     *     listing.forEach(function(item) {
+     *       console.log('- ' + item);
+     *     });
+     *   });
+     *   (end code)
+     */
     getListing: function(path) {
       if(path.length > 0 && path[path.length - 1] != '/') {
         throw "Not a directory: " + path;
@@ -80,6 +122,27 @@
       });
     },
 
+    /**
+     * Method: getAll
+     *
+     * Get all objects directly below a given path.
+     *
+     * Parameters:
+     *   path      - path to the direcotry
+     *   typeAlias - (optional) local type-alias to filter for
+     *
+     * Returns:
+     *   a promise for an object in the form { path : object, ... }
+     *
+     * Example:
+     *   (start code)
+     *   client.getAll('').then(function(objects) {
+     *     for(var key in objects) {
+     *       console.log('- ' + key + ': ', objects[key]);
+     *     }
+     *   });
+     *   (end code)
+     */
     getAll: function(path) {
       if(path.length > 0 && path[path.length - 1] != '/') {
         throw "Not a directory: " + path;
@@ -102,6 +165,34 @@
 
     // file operations
 
+    /**
+     * Method: getFile
+     *
+     * Get the file at the given path. A file is raw data, as opposed to
+     * a JSON object (use <getObject> for that).
+     *
+     * Except for the return value structure, getFile works exactly like
+     * getObject.
+     *
+     * Parameters:
+     *   path     - see getObject
+     *
+     * Returns:
+     *   A promise for an object:
+     *
+     *   mimeType - String representing the MIME Type of the document.
+     *   data     - Raw data of the document (either a string or an ArrayBuffer)
+     *
+     * Example:
+     *   (start code)
+     *   // Display an image:
+     *   client.getFile('path/to/some/image').then(function(file) {
+     *     var blob = new Blob([file.data], { type: file.mimeType });
+     *     var targetElement = document.findElementById('my-image-element');
+     *     targetElement.src = window.URL.createObjectURL(blob);
+     *   });
+     *   (end code)
+     */
     getFile: function(path) {
       return this.storage.get(this.makePath(path)).then(function(status, body, mimeType, revision) {
         return {
@@ -112,6 +203,41 @@
       });
     },
 
+    /**
+     * Method: storeFile
+     *
+     * Store raw data at a given path.
+     *
+     * Parameters:
+     *   mimeType - MIME media type of the data being stored
+     *   path     - path relative to the module root. MAY NOT end in a forward slash.
+     *   data     - string or ArrayBuffer of raw data to store
+     *
+     * The given mimeType will later be returned, when retrieving the data
+     * using <getFile>.
+     *
+     * Example (UTF-8 data):
+     *   (start code)
+     *   client.storeFile('text/html', 'index.html', '<h1>Hello World!</h1>');
+     *   (end code)
+     *
+     * Example (Binary data):
+     *   (start code)
+     *   // MARKUP:
+     *   <input type="file" id="file-input">
+     *   // CODE:
+     *   var input = document.getElementById('file-input');
+     *   var file = input.files[0];
+     *   var fileReader = new FileReader();
+     *
+     *   fileReader.onload = function() {
+     *     client.storeFile(file.type, file.name, fileReader.result);
+     *   };
+     *
+     *   fileReader.readAsArrayBuffer(file);
+     *   (end code)
+     *
+     */
     storeFile: function(mimeType, path, body) {
       return this.storage.put(this.makePath(path), body, mimeType).then(function(status, _body, _mimeType, revision) {
         if(status == 200 || status == 201) {
@@ -124,6 +250,25 @@
 
     // object operations
 
+    /**
+     * Method: getObject
+     * 
+     * Get a JSON object from given path.
+     * 
+     * Parameters:
+     *   path     - relative path from the module root (without leading slash)
+     * 
+     * Returns:
+     *   A promise for the object.
+     * 
+     * Example:
+     *   (start code)
+     *   client.getObject('/path/to/object').
+     *     then(function(object) {
+     *       // object is either an object or null
+     *     });
+     *   (end code)
+     */
     getObject: function(path) {
       return this.storage.get(this.makePath(path)).then(function(status, body, mimeType, revision) {
         if(typeof(body) == 'object') {
@@ -134,6 +279,47 @@
       });
     },
 
+    /**
+     * Method: storeObject
+     *
+     * Store object at given path. Triggers synchronization.
+     *
+     * Parameters:
+     *
+     *   type     - unique type of this object within this module. See description below.
+     *   path     - path relative to the module root.
+     *   object   - an object to be saved to the given node. It must be serializable as JSON.
+     *
+     * Returns:
+     *   A promise to store the object. The promise fails with a ValidationError, when validations fail.
+     *
+     *
+     * What about the type?:
+     *
+     *   A great thing about having data on the web, is to be able to link to
+     *   it and rearrange it to fit the current circumstances. To facilitate
+     *   that, eventually you need to know how the data at hand is structured.
+     *   For documents on the web, this is usually done via a MIME type. The
+     *   MIME type of JSON objects however, is always application/json.
+     *   To add that extra layer of "knowing what this object is", remoteStorage
+     *   aims to use <JSON-LD at http://json-ld.org/>.
+     *   A first step in that direction, is to add a *@context attribute* to all
+     *   JSON data put into remoteStorage.
+     *   Now that is what the *type* is for.
+     *
+     *   Within remoteStorage.js, @context values are built using three components:
+     *     http://remotestoragejs.com/spec/modules/ - A prefix to guarantee unqiueness
+     *     the module name     - module names should be unique as well
+     *     the type given here - naming this particular kind of object within this module
+     *
+     *   In retrospect that means, that whenever you introduce a new "type" in calls to
+     *   storeObject, you should make sure that once your code is in the wild, future
+     *   versions of the code are compatible with the same JSON structure.
+     *
+     * How to define types?:
+     *
+     *   See <declareType> or the calendar module (src/modules/calendar.js) for examples.
+     */
     storeObject: function(typeAlias, path, object) {
       this._attachType(object, typeAlias);
       return this.storage.put(this.makePath(path), object, 'application/json; charset=UTF-8').then(function(status, _body, _mimeType, revision) {
@@ -147,6 +333,14 @@
 
     // generic operations
 
+    /**
+     * Method: remove
+     *
+     * Remove node at given path from storage. Triggers synchronization.
+     *
+     * Parameters:
+     *   path     - Path relative to the module root.
+     */
     remove: function(path) {
       return this.storage.delete(this.makePath(path));
     },
