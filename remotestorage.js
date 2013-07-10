fa9c9990669f08dfafcb25b2368110cdd595c013
Niklas Cathor <nilclass@riseup.net>
2013-07-10 21:13:25 +0200

    builds

--- remotestorage.js
+++ remotestorage.js
@@ -1028,15 +1028,15 @@ RemoteStorage.Assets = {
     this.rs.on('ready', stateSetter(this, 'connected'));
     this.rs.on('disconnected', stateSetter(this, 'initial'));
     //this.rs.on('connecting', stateSetter(this, 'connecting'))
-    this.rs.on('authing', stateSetter(this, 'authing'))
-    this.rs.on('sync-busy', stateSetter(this, 'busy') );
-    this.rs.on('sync-done', stateSetter(this, 'connected'))
+    this.rs.on('authing', stateSetter(this, 'authing'));
+    this.rs.on('sync-busy', stateSetter(this, 'busy'));
+    this.rs.on('sync-done', stateSetter(this, 'connected'));
   };
 
   RemoteStorage.Widget.prototype = {
     display: function() {
       if(! this.view) {
-        this.setView(new View);
+        this.setView(new View());
       }
       this.view.display.apply(this.view, arguments);
       return this;
@@ -1044,15 +1044,9 @@ RemoteStorage.Assets = {
 
     setView: function(view) {
       this.view = view;
-      this.view.on( 'connect', function(a){
-        this.rs.connect(a);
-      }.bind(this) )
-      this.view.on( 'disconnect', function(){
-        this.rs.disconnect();
-      }.bind(this) )
-      this.view.on( 'sync', function(){
-        this.rs.sync()
-      }.bind(this) )
+      this.view.on('connect', this.rs.connect.bind(this));
+      this.view.on('disconnect', this.rs.disconnect.bind(this));
+      this.view.on('sync', this.rs.sync.bind(this));
     }
   };
 
@@ -1060,56 +1054,58 @@ RemoteStorage.Assets = {
     this.widget.display();
   };
 
-  RemoteStorage.Widget._rs_init = function(remoteStorage){
+  RemoteStorage.Widget._rs_init = function(remoteStorage) {
     remoteStorage.widget = new RemoteStorage.Widget(remoteStorage);
     window.addEventListener('load', function() {
       remoteStorage.displayWidget();
     });
-  }
+  };
 
-    function cEl(t){
-    return document.createElement(t);
-  }
-  function gCl(parent, className){
+  var cEl = document.createElement.bind(document);
+
+  function gCl(parent, className) {
     return parent.getElementsByClassName(className)[0];
   }
-  function gTl(parent, className){
+  function gTl(parent, className) {
     return parent.getElementsByTagName(className)[0];
   }
 
-  function toggle_bubble(widget){
+  function toggle_bubble(widget) {
     var el = gCl(widget,'bubble');
-    if(el.className.search('hidden') < 0){
+    if(el.className.search('hidden') < 0) {
       addClass(el, 'hidden');
-    }else{
+    } else {
       removeClass(el, 'hidden');
       gTl(widget, 'form').userAddress.focus();
     }
   }
-  function show(el, display){
-    if(typeof(display) === 'undefined'){
+
+  function show(el, display) {
+    if(typeof(display) === 'undefined') {
       display = 'block';
     }
-    el.style.display = display
+    el.style.display = display;
     return el;
   }
-  function hide(el){
+
+  function hide(el) {
     show(el,'none');
     return el;
   }
 
   // MAYBE do those two in a el.className.split(' ') way more safe
-  function removeClass(el, className){
-    el.className = el.className.split(' ').filter( function(val){
-      return val != className
+  function removeClass(el, className) {
+    el.className = el.className.split(' ').filter(function(val) {
+      return val != className;
     }).join(' ');
   }
-  function addClass(el, className){
-    el.className += ' '+className;
+
+  function addClass(el, className) {
+    el.className += ' ' + className;
   }
 
 
-  function View(){
+  function View() {
     if(typeof(document) === 'undefined') {
       throw "Widget not supported";
     }
@@ -1118,58 +1114,54 @@ RemoteStorage.Assets = {
                                 'disconnect',
                                 'sync',
                                 'reconnect',
-                                'display')
+                                'display');
 
     this.display = function() {
 
-        if( ! (typeof(this.widget) === 'undefined') )
-          return this.widget;
+      if(typeof(this.widget) !== 'undefined')
+        return this.widget;
 
-        var element = cEl('div')
-        var style = cEl('style');
-        style.innerHTML = RemoteStorage.Assets.widgetCss;
+      var element = cEl('div');
+      var style = cEl('style');
+      style.innerHTML = RemoteStorage.Assets.widgetCss;
 
-        element.id="remotestorage-widget"
+      element.id = "remotestorage-widget";
 
-        element.innerHTML = RemoteStorage.Assets.widget;
+      element.innerHTML = RemoteStorage.Assets.widget;
 
 
-        element.appendChild(style)
-        document.body.appendChild(element);
+      element.appendChild(style);
+      document.body.appendChild(element);
 
       var el;
-        //sync button
-      el = gCl(element, 'sync')
-      gTl(el, 'img').src = RemoteStorage.Assets.syncIcon
-      el.addEventListener('click', this.events.sync.bind(this))
+      //sync button
+      el = gCl(element, 'sync');
+      gTl(el, 'img').src = RemoteStorage.Assets.syncIcon;
+      el.addEventListener('click', this.events.sync.bind(this));
 
-        //disconnect button
-      el = gCl(element, 'disconnect')
-      gTl(el, 'img').src = RemoteStorage.Assets.disconnectIcon
-      el.addEventListener('click', this.events.disconnect.bind(this))
+      //disconnect button
+      el = gCl(element, 'disconnect');
+      gTl(el, 'img').src = RemoteStorage.Assets.disconnectIcon;
+      el.addEventListener('click', this.events.disconnect.bind(this));
 
-        //connect button
-      var cb = gCl(element,'connect')
-      gTl(cb, 'img').src=RemoteStorage.Assets.connectIcon;
+      //connect button
+      var cb = gCl(element,'connect');
+      gTl(cb, 'img').src = RemoteStorage.Assets.connectIcon;
       cb.addEventListener('click', this.events.connect.bind(this));
 
-        // input
+      // input
       el = gTl(element, 'form').userAddress;
-      el.addEventListener('keyup', function(event){
-
+      el.addEventListener('keyup', function(event) {
         if(event.target.value) cb.removeAttribute('disabled');
         else cb.setAttribute('disabled','disabled');
+      });
 
-      })
-
-        //the cube
+      //the cube
       el = gCl(element, 'cube');
-      el.src = RemoteStorage.Assets.remoteStorageIcon
-      el.addEventListener('click',
-                          function(){
-                            toggle_bubble(this.div)
-                          }.bind(this)
-                         )
+      el.src = RemoteStorage.Assets.remoteStorageIcon;
+      el.addEventListener('click', function() {
+        toggle_bubble(this.div);
+      }.bind(this));
 
 
       this.div = element;
@@ -1177,62 +1169,61 @@ RemoteStorage.Assets = {
       this.states.initial.call(this);
       this._emit('display');
       return this.div;
-    }
+    };
 
-    this.setState = function(state){
-      var s;
-	    if(typeof(this.states[state]) === 'undefined'){
-  	    throw "Bad State assigned to view"
+    this.setState = function(state) {
+      var s = this.states[state];
+      if(typeof(s) === 'undefined') {
+        throw new Error("Bad State assigned to view: " + state);
       }
-      this.states[state].call(this);
+      s.call(this);
+    };
 
-    }
-
-    this.setUserAddress = function(addr){
+    this.setUserAddress = function(addr) {
       this.userAddress = addr;
-    }
-  };
+    };
+  }
 
   View.prototype = {
-     // States:
-     //  initial      - not connected
-     //  authing      - in auth flow
-     //  connected    - connected to remote storage, not syncing at the moment
-     //  busy         - connected, syncing at the moment
-     //  offline      - connected, but no network connectivity
-     //  error        - connected, but sync error happened
-     //  unauthorized - connected, but request returned 401
+    // States:
+    //  initial      - not connected
+    //  authing      - in auth flow
+    //  connected    - connected to remote storage, not syncing at the moment
+    //  busy         - connected, syncing at the moment
+    //  offline      - connected, but no network connectivity
+    //  error        - connected, but sync error happened
+    //  unauthorized - connected, but request returned 401
     currentState : 'initial',
     states :  {
-      initial : function(){
-        this.div.className = "remotestorage-state-initial"
-        gCl(this.div,'status-text').innerHTML = "Connect <strong>remotestorage</strong>"
+      initial : function() {
+        this.div.className = "remotestorage-state-initial";
+        gCl(this.div, 'status-text').innerHTML = "Connect <strong>remotestorage</strong>";
       },
-      authing : function(){
-        this.div.className = "remotestorage-state-authing"
-        gCl(this.div,'status-text').innerHTML = "Connecting"
+      authing : function() {
+        this.div.className = "remotestorage-state-authing";
+        gCl(this.div, 'status-text').innerHTML = "Connecting";
         addClass(gCl(this.div, 'cube'), 'remotestorage-loading'); //TODO needs to be undone when is that neccesary
       },
-      connected : function(){
-        this.div.className = "remotestorage-state-connected"
-        gCl(this.div,'userAddress').innerHTML = this.userAddress;
-        var cube = gCl(this.div,'cube');
+      connected : function() {
+        this.div.className = "remotestorage-state-connected";
+        gCl(this.div, 'userAddress').innerHTML = this.userAddress;
+        var cube = gCl(this.div, 'cube');
         cube.src = RemoteStorage.Assets.remoteStorageIcon;
-        removeClass(cube,'remotestorage-loading');
+        removeClass(cube, 'remotestorage-loading');
       },
-      busy : function(){
+      busy : function() {
         this.div.className = "remotestorage-state-busy";
         addClass(gCl(this.div, 'cube'), 'remotestorage-loading'); //TODO needs to be undone when is that neccesary
       },
-      offline : function(){
+      offline : function() {
         this.div.className = "remotestorage-state-offline";
-        gCl(this.div,'cube').src = RemoteStorage.Assets.remoteStorageIconOffline;
+        gCl(this.div, 'cube').src = RemoteStorage.Assets.remoteStorageIconOffline;
       },
-      error : function(){
+      error : function() {
         this.div.className = "remotestorage-state-error";
-        gCl(this.div,'cube').src = RemoteStorage.Assets.remoteStorageIconError
+        gCl(this.div, 'cube').src = RemoteStorage.Assets.remoteStorageIconError;
       },
-      unauthorized : function(){
+      unauthorized : function() {
         this.div.className = "remotestorage-state-unauthorized";
       }
     },
@@ -1249,256 +1240,11 @@ RemoteStorage.Assets = {
         event.preventDefault();
         this._emit('disconnect');
       },
-      recconnect : function(){},
-      display : function(){
-      }
+      // FIXME: what are these???
+      reconnect : function() {},
+      display : function() {}
     }
-  }
-
-
-  // // the view.
-  // var view = defaultView;
-  // // options passed to displayWidget
-  // var widgetOptions = {};
-  // // passed to display() to avoid circular deps
-  // var remoteStorage;
-
-  // var reconnectInterval = 1000;
-
-  // var offlineTimer = null;
-
-  // var viewState;
-
-  // function calcReconnectInterval() {
-  //   var i = reconnectInterval;
-  //   if(reconnectInterval < 10000) {
-  //     reconnectInterval *= 2;
-  //   }
-  //   return i;
-  // }
-
-  // var stateActions = {
-  //   offline: function() {
-  //     if(! offlineTimer) {
-  //       offlineTimer = setTimeout(function() {
-  //         offlineTimer = null;
-  //         sync.fullSync().
-  //           then(function() {
-  //             schedule.enable();
-  //           });
-  //       }, calcReconnectInterval());
-  //     }
-  //   },
-  //   connected: function() {
-  //     if(offlineTimer) {
-  //       clearTimeout(offlineTimer);
-  //       offlineTimer = null;
-  //     }
-  //   }
-  // };
-
-
-  // function setState(state) {
-  //   if(state === viewState) {
-  //     return;
-  //   }
-  //   viewState = state;
-  //   var action = stateActions[state];
-  //   if(action) {
-  //     action.apply(null, arguments);
-  //   }
-  //   view.setState.apply(view, arguments);
-  //   events.emit('state', state);
-  // }
-
-  // function requestToken(authEndpoint) {
-  //   logger.info('requestToken', authEndpoint);
-  //   var redirectUri = (widgetOptions.redirectUri || view.getLocation());
-  //   var state;
-  //   var md = redirectUri.match(/^(.+)#(.*)$/);
-  //   if(md) {
-  //     redirectUri = md[1];
-  //     state = md[2];
-  //   }
-  //   var clientId = util.hostNameFromUri(redirectUri);
-  //   authEndpoint += authEndpoint.indexOf('?') > 0 ? '&' : '?';
-  //   var params = [
-  //     ['redirect_uri', redirectUri],
-  //     ['client_id', clientId],
-  //     ['scope', remoteStorage.access.scopeParameter],
-  //     ['response_type', 'token']
-  //   ];
-  //   if(typeof(state) === 'string' && state.length > 0) {
-  //     params.push(['state', state]);
-  //   }
-  //   authEndpoint += params.map(function(kv) {
-  //     return kv[0] + '=' + encodeURIComponent(kv[1]);
-  //   }).join('&');
-  //   console.log('redirecting to', authEndpoint);
-  //   return view.redirectTo(authEndpoint);
-  // }
-
-  // function connectStorage(userAddress) {
-  //   settings.set('userAddress', userAddress);
-  //   setState('authing');
-  //   return webfinger.getStorageInfo(userAddress).
-  //     then(remoteStorage.setStorageInfo, function(error) {
-  //       if(error === 'timeout') {
-  //         adjustTimeout();
-  //       }
-  //       setState((typeof(error) === 'string') ? 'typing' : 'error', error);
-  //     }).
-  //     then(function(storageInfo) {
-  //       if(storageInfo) {
-  //         requestToken(storageInfo.properties['auth-endpoint']);
-  //         schedule.enable();
-  //       }
-  //     }, util.curry(setState, 'error'));
-  // }
-
-  // function reconnectStorage() {
-  //   connectStorage(settings.get('userAddress'));
-  // }
-
-  // function disconnectStorage() {
-  //   schedule.disable();
-  //   remoteStorage.flushLocal();
-  //   events.emit('state', 'disconnected');
-  //   events.emit('disconnect');
-  // }
-
-  // // destructively parse query string from URI fragment
-  // function parseParams() {
-  //   var md = view.getLocation().match(/^(.*?)#(.*)$/);
-  //   var result = {};
-  //   if(md) {
-  //     var hash = md[2];
-  //     hash.split('&').forEach(function(param) {
-  //       var kv = param.split('=');
-  //       if(kv[1]) {
-  //         result[kv[0]] = decodeURIComponent(kv[1]);
-  //       }
-  //     });
-  //     if(Object.keys(result).length > 0) {
-  //       view.setLocation(md[1] + '#');
-  //     }
-  //   }
-  //   return result;
-  // }
-
-  // function processParams() {
-  //   var params = parseParams();
-
-  //   // Query parameter: access_token
-  //   if(params.access_token) {
-  //     wireClient.setBearerToken(params.access_token);
-  //   }
-  //   // Query parameter: remotestorage
-  //   if(params.remotestorage) {
-  //     view.setUserAddress(params.remotestorage);
-  //     setTimeout(function() {
-  //       if(wireClient.getState() !== 'connected') {
-  //         connectStorage(params.remotestorage);
-  //       }
-  //     }, 0);
-  //   } else {
-  //     var userAddress = settings.get('userAddress');
-  //     if(userAddress) {
-  //       view.setUserAddress(userAddress);
-  //     }
-  //   }
-  //   // Query parameter: state
-  //   if(params.state) {
-  //     view.setLocation(view.getLocation().split('#')[0] + '#' + params.state);
-  //   }
-  // }
-
-  // function handleSyncError(error) {
-  //   if(error.message === 'unauthorized') {
-  //     setState('unauthorized');
-  //   } else if(error.message === 'network error') {
-  //     setState('offline', error);
-  //   } else {
-  //     setState('error', error);
-  //   }
-  // }
-
-  // function adjustTimeout() {
-  //   var t = getputdelete.getTimeout();
-  //   if(t < maxTimeout) {
-  //     t *= timeoutAdjustmentFactor;
-  //     webfinger.setTimeout(t);
-  //     getputdelete.setTimeout(t);
-  //   }
-  // }
-
-  // function handleSyncTimeout() {
-  //   adjustTimeout();
-  //   schedule.disable();
-  //   setState('offline');
-  // }
-
-  // function initialSync() {
-  //   if(settings.get('initialSyncDone')) {
-  //     schedule.enable();
-  //     store.fireInitialEvents().then(util.curry(events.emit, 'ready'));
-  //   } else {
-  //     setState('busy', true);
-  //     sync.fullSync().then(function() {
-  //       schedule.enable();
-  //       settings.set('initialSyncDone', true);
-  //       setState('connected');
-  //       events.emit('ready');
-  //     }, handleSyncError);
-  //   }
-  // }
-
-  // function display(_remoteStorage, element, options) {
-  //   remoteStorage = _remoteStorage;
-  //   widgetOptions = options;
-  //   if(! options) {
-  //     options = {};
-  //   }
-
-  //   options.getLastSyncAt = function() {
-  //     return (sync.lastSyncAt || new Date()).getTime();
-  //   };
-
-  //   schedule.watch('/', 30000);
-
-  //   view.display(element, options);
-
-  //   view.on('sync', sync.forceSync);
-  //   view.on('connect', connectStorage);
-  //   view.on('disconnect', disconnectStorage);
-  //   view.on('reconnect', reconnectStorage);
-
-  //   wireClient.on('busy', util.curry(setState, 'busy'));
-  //   wireClient.on('unbusy', util.curry(setState, 'connected'));
-  //   wireClient.on('connected', function() {
-  //     setState('connected');
-  //     initialSync();
-  //   });
-  //   wireClient.on('disconnected', util.curry(setState, 'initial'));
-
-  //   BaseClient.on('error', util.curry(setState, 'error'));
-  //   sync.on('error', handleSyncError);
-  //   sync.on('timeout', handleSyncTimeout);
-
-  //   processParams();
-
-  //   wireClient.calcState();
-  // }
-
-  // RemoteStorage.widget = util.extend({
-  //   display : display,
-
-  //   clearSettings: settings.clear,
-
-  //   setView: function(_view) {
-  //     view = _view;
-  //   }
-  // }, events);
+  };
 
 })();
 
