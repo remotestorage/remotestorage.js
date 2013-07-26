4448847465e5b40173e7b87eccb33325af09e2eb
Niklas Cathor <nilclass@riseup.net>
2013-07-26 13:52:16 +0200

    set version to -head

--- src/version.js
+++ src/version.js
@@ -3,7 +3,7 @@ RemoteStorage.version = RemoteStorage.prototype.version = {
   product: 0,
   major: 8,
   minor: 0,
-  postfix: 'rc2'
+  postfix: 'head'
 };
 
 RemoteStorage.version.toString = function() {
