d717192970866bd1cb7d36126d45a95263a05ca2
Michiel de Jong <michiel@unhosted.org>
2013-10-13 17:49:09 +0300

    first attempt at #428

---
+++ example/minimal-head/notes.js
@@ -0,0 +1,17 @@
+RemoteStorage.defineModule('notes', function(privateClient, publicClient) {
+  return {
+    exports: {
+      setNote: function (text) {
+        return privateClient.storeFile('text/plain', 'note.txt', text);
+      },
+      getNote: function () {
+        return privateClient.getFile('note.txt').then(function(obj) {
+          return obj.data;
+        });
+      },
+      onChange: function (cb) {
+        privateClient.on('change', cb);
+      }
+    }
+  };
+});
