22ad5b09e803e3af2102536600d097540fe26fca
ggrin <ggrin@lavabit.com>
2013-10-21 00:06:48 +0200

    use configured port in webfinger of nodejs-exapmple server

--- server/nodejs-example.js
+++ server/nodejs-example.js
@@ -274,12 +274,12 @@ exports.server = (function() {
     }
     writeJson(res, {
       links:[{
-        href: config.protocol+'://'+config.host+'/storage/'+userName,
+        href: config.protocol+'://'+config.host+':'+config.port+'/storage/'+userName,
         rel: "remoteStorage",
         type: "https://www.w3.org/community/rww/wiki/read-write-web-00#simple",
         properties: {
           'auth-method': "https://tools.ietf.org/html/draft-ietf-oauth-v2-26#section-4.2",
-          'auth-endpoint': config.protocol+'://'+config.host+'/auth/'+userName
+          'auth-endpoint': config.protocol+'://'+config.host+':'+config.port+'/auth/'+userName
         }
       }]
     });
