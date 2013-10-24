c8b0247f3b939ba716f72efc382e56d97b0a20a9
Michiel de Jong <michiel@unhosted.org>
2013-10-24 11:55:14 +0300

    only descend if revision or listed revision is different

--- src/sync.js
+++ src/sync.js
@@ -50,10 +50,18 @@
     }
   }
 
-  function allKeys(a, b) {
+  function allDifferentKeys(a, b) {
     var keyObject = {};
-    for(var ak in a) keyObject[ak] = true;
-    for(var bk in b) keyObject[bk] = true;
+    for(var ak in a) {
+      if(a[ak] != b[ak]) {
+        keyObject[ak] = true;
+      }
+    }
+    for(var bk in b) {
+      if(a[ak] != b[ak]) {
+        keyObject[bk] = true;
+      }
+    }
     return Object.keys(keyObject);
   }
   function promiseDeleteLocal(local, path) {
@@ -110,9 +118,11 @@
           deleteLocal(local, path, promise);
         } else if(localStatus == 200 && remoteStatus == 200) {
           if(isDir(path)) {
-            local.setRevision(path, remoteRevision).then(function() {
-              descendInto(remote, local, path, allKeys(localBody, remoteBody), promise);
-            });
+            if(remoteRevision != localRevision) {
+              local.setRevision(path, remoteRevision).then(function() {
+                descendInto(remote, local, path, allDifferentKeys(localBody, remoteBody), promise);
+              });
+            }
           } else {
             updateLocal(remote, local, path, remoteBody, remoteContentType, remoteRevision, promise);
           }
