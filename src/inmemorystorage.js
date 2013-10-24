1abf28c5a35ebb7ff173dcd02b92c6563ce3bc23
ggrin <ggrin@lavabit.com>
2013-10-25 01:17:57 +0200

    fixing typos and minor stuff

--- src/inmemorystorage.js
+++ src/inmemorystorage.js
@@ -1,14 +1,14 @@
-
-function makeNode(path) {
-  var node = { path: path };
-  if(path[path.length - 1] == '/') {
-    node.body = {};
-    node.cached = {};
-    node.contentType = 'application/json';
+(function(global) {
+  function makeNode(path) {
+    var node = { path: path };
+    if(path[path.length - 1] == '/') {
+      node.body = {};
+      node.cached = {};
+      node.contentType = 'application/json';
+    }
+    return node;
   }
-  return node;
-}
-function applyRecursive(path, cb){
+  function applyRecursive(path, cb){
     var parts = path.match(/^(.*\/)([^\/]+\/?)$/);
     if(parts) {
       var dirname = parts[1];
@@ -19,130 +19,142 @@ function applyRecursive(path, cb){
     } else {
       throw new Error('inMemoryStorage encountered invalid path : '+path)
     }
-RemoteStorage.InMemoryStorage = function(rs){
-  this.rs = rs
-  RemoteStorage.eventHandling(this, 'change', 'conflict')
-  this._storage = {};
-  this._changes = {};
-}
+  }
 
-RemoteStorage.InMemoryStorage.prototype = {
-  get: function(path){
-    var node = this._storage[path];
-    if(node)
-      return promising().fulfill(200, node.body, node.contentType, node.revision);
-    else
-      return promising().fulfill(404);
-  },
-  put: function(path, body, contetType, incomming){
-    var oldNode = this._storage[path];
-    var node = {
-      path: path,
-      contentType: contentType,
-      body: body
-    }
-    this._storage = node;
-    this._addToParent(path);
-     if(!incoming) {
-      this._recordChange(path, {action: 'PUT' });
-    }
-    this._emit('change', {
-      path: path,
-      origin: incoming ? 'remote' : 'window',
-      oldValue: oldNode ? oldNode.body : undefined,
-      newValue: body
-    })
-    return promising().fulfill(200);
-  },
-  delete: function(path, incoming){
-    var oldNode = this._storage[path];
-    delete this._storage[path];
-    this._removeFromParent(path);
-    if(!incoming) {
-      this._recordChange(path, {action: 'DELETE' });
-    }
-    if(oldNode) {
+  RemoteStorage.InMemoryStorage = function(rs){
+    this.rs = rs
+    RemoteStorage.eventHandling(this, 'change', 'conflict')
+    this._storage = {};
+    this._changes = {};
+  }
+
+  RemoteStorage.InMemoryStorage.prototype = {
+    get: function(path){
+      var node = this._storage[path];
+      if(node)
+        return promising().fulfill(200, node.body, node.contentType, node.revision);
+      else
+        return promising().fulfill(404);
+    },
+    
+    put: function(path, body, contentType, incoming){
+      var oldNode = this._storage[path];
+      var node = {
+        path: path,
+        contentType: contentType,
+        body: body
+      }
+      this._storage = node;
+      this._addToParent(path);
+      if(!incoming) {
+        this._recordChange(path, {action: 'PUT' });
+      }
+      
       this._emit('change', {
         path: path,
         origin: incoming ? 'remote' : 'window',
-        oldValue: oldNode.body,
-        newValue: undefined
+        oldValue: oldNode ? oldNode.body : undefined,
+        newValue: body
       })
-    }
-    return promising().fulfill(200);
-  },
-  _addToParent: function(path){
-    applyRecursive(path, function(dirname, basename){
-      var node = this._storage[dirname] || makeNode(dirname);
-      node.body[basename] = true;
-      this._storage[node] = node;
-    })
-  },
-  _removeFromParent: function(path){
-    applyRecursive(path, function(basename, dirname){
-      var node = this._storage[dirname]
-      if(node) {
-        delete node[basename];
-        if(node.keys.length == 0)
-          delete this._storage[dirname]
+      return promising().fulfill(200);
+    },
+    
+    delete: function(path, incoming){
+      var oldNode = this._storage[path];
+      delete this._storage[path];
+      this._removeFromParent(path);
+      if(!incoming) {
+        this._recordChange(path, {action: 'DELETE' });
       }
-    })
-  },
-  _recordChage: function(path, attriubtes) {
-    var change = this._changes[path] || {};
-    for(var k in attributes)
-      change[k] = attributes[k];
-    change.path = path;
-    this._changes[path] = change;
-  },
-  clearChange: function(path) {
-    delete this._changes[path];
-    return promising().fulfill();
-  },
-  changesBelow: function(path) {
-    var changes = [];
-    var l = path.length; 
-    for(var k in this._changes){
-      if(k.substr(0,l) == path)
-        changes.push(this._changes[k]);
+      
+      if(oldNode) {
+        this._emit('change', {
+          path: path,
+          origin: incoming ? 'remote' : 'window',
+          oldValue: oldNode.body,
+          newValue: undefined
+        })
+      }
+      return promising().fulfill(200);
+    },
+    
+    _addToParent: function(path){
+      var storage = this._storage
+      applyRecursive(path, function(dirname, basename){
+        var node = storage[dirname] || makeNode(dirname);
+        node.body[basename] = true;
+        storage[node] = node;
+      })
+    },
+    _removeFromParent: function(path){
+      var storage = this._storage
+      applyRecursive(path, function(basename, dirname){
+        var node = storage[dirname]
+        if(node) {
+          delete node[basename];
+          if(node.keys.length == 0)
+            delete storage[dirname]
+        }
+      })
+    },
+    
+    _recordChage: function(path, attriubtes) {
+      var change = this._changes[path] || {};
+      for(var k in attributes)
+        change[k] = attributes[k];
+      change.path = path;
+      this._changes[path] = change;
+    },
+    clearChange: function(path) {
+      delete this._changes[path];
+      return promising().fulfill();
+    },
+    changesBelow: function(path) {
+      var changes = [];
+      var l = path.length; 
+      for(var k in this._changes){
+        if(k.substr(0,l) == path)
+          changes.push(this._changes[k]);
+      }
+      return promising().fulfill(changes);
+    },
+    setConflict: function(path, attributes) {
+      this._recordChange(path, { conflict: attributes });
+      var self = this;
+      var event = {path:path};
+      for(var k in attributes)
+        event[k] = attributes[k];
+      
+      event.resolve = function(resolution) {
+        if(resolution == 'remote'|| resolution == 'local') {
+          attributes.resolution = resolution;
+          self._recordChange(path, { conflict: attributes });
+        } else {
+          throw new Error('Invalid resolution: '+resolution);
+        } 
+      }
+      this._emit('conflict', event);
+    },
+
+    setRevision: function(path, revision){
+      var node = this._storage[path] || makeNode(path)
+      node.revision = revision;
+      this._storage[path] = node;
+      return promising().fulfill();
+    },
+    getRevision: function(path){
+      var rev;
+      if(this._storage[path])
+        rev = this._storage[path].revision;
+      return promising().fulfill(rev);
     }
-    return promising().fulfill(changes);
-  },
-  setConflict: function(path, attributes) {
-    this._recordChange(path, { conflict: attributes });
-    var self = this;
-    var event = {path:path};
-    for(var k in attributes)
-      event[k] = attributes[k];
-
-    event.resolve = function(resolution) {
-      if(resolution == 'remote'|| resolution == 'local') {
-        attributes.resolution = resolution;
-        self._recordChange(path, { conflict: attributes });
-      } else {
-        throw new Error('Invalid resolution: '+resolution);
-      } 
-    }
-    this._emit('conflict', event);
   }
-  setRevision: function(path, revision){
-    var node = this._storage[path] || makeNode(path)
-    node.revision = revision;
-    this._storage[path] = node;
-    return promising().fulfill();
-  },
-  getRevision: function(path){
-    var rev;
-    if(this._storage[path])
-      rev = this._storage[path].revision;
-    return promising().fulfill(rev);
+
+  RemoteStorage.InMemoryStorage._rs_init = function(){}
+  
+  RemoteStorage.InMemoryStorage._rs_supported = function(){
+    return true;
   }
-}
 
-RemoteStorage.InMemoryStorage._rs_init = function(){}
-
-RemoteStorage.InMemoryStorage._rs_supported = function(){
-  return true;
-}
-
-RemoteStorage.InMemoryStorage._rs_cleanup = function(){}
+  RemoteStorage.InMemoryStorage._rs_cleanup = function(){}
+})(typeof(window) !== 'undefined' ? window : global);
