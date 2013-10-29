48581d2cde1908b884d2642eec96787266204db0
ggrin <ggrin@lavabit.com>
2013-10-29 08:29:09 +0100

    first working nonsense test for sync

--- test/unit/sync-suite.js
+++ test/unit/sync-suite.js
@@ -4,29 +4,55 @@ if(typeof(define) !== 'function') {
 
 define([], function() {
   var suites = [];
+  
+  function FakeCaching(){}
+  function FakeRemote(){
+    this.connected = false;
+  }
   suites.push({
     name: "Sync Suite",
     desc: "testing the sync adapter instance",
     setup: function(env, test){
       require('./lib/promising');
-      global.RemoteStorage = function(){};
+      global.RemoteStorage = function(){
+        RemoteStorage.eventHandling(this, 'sync-busy', 'sync-done')
+      };
       require('./src/eventhandling');
       if( global.rs_eventhandling ){
         RemoteStorage.eventHandling = global.rs_eventhandling
       } else {
         global.rs_eventhandling = RemoteStorage.eventHandling;
       }
+      require('./src/inmemorystorage.js')
+      if(global.rs_ims) {
+        RemoteStorage.InMemoryCaching = global.rs_ims;
+      } else {
+        global.rs_ims = RemoteStorage.InMemoryStorage;
+      }
+
       require('src/sync.js');
       test.done();
     },
     beforeEach: function(env, test){
+      env.rs = new RemoteStorage();
+      env.rs.local = new RemoteStorage.InMemoryStorage();
+      env.rs.caching = new FakeCaching();
+      env.rs.remote = new FakeRemote();
       test.done();
     },
     tests: [
       {
-        desc: "test",
+        desc: "RemoteStorage.sync() returns imediatly if not connected",
         run: function(env,test){
-          test.done();
+          var failed = false;
+          env.rs.on('sync-busy', function(){
+            failed = true;
+          })
+          
+          env.rs.sync().then(function(){
+            test.assert(failed, false);
+          })
+          
         }
       }
     ]
