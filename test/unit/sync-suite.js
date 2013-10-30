fadb46431cd8fa16bf43eb581c8f2a13e4a43a5f
ggrin <ggrin@lavabit.com>
2013-10-30 15:22:43 +0100

    more syncing tests

--- test/unit/sync-suite.js
+++ test/unit/sync-suite.js
@@ -5,10 +5,29 @@ if(typeof(define) !== 'function') {
 define([], function() {
   var suites = [];
   
-  function FakeCaching(){}
-  function FakeRemote(){
-    this.connected = false;
+  function FakeCaching(){
+    this.rootPaths = [];
   }
+
+  function FakeRemote(){
+    function GPD(target, path,body, contentType, options) {
+      var args = Array.prototype.slice.call(arguments);
+      console.log('GPD called with : ',args)
+      this['_'+target+'s'].push([path, body, contentType, options]);
+      var p = promising();
+      var resp = this._responses[args] || [200];
+      return p.fulfill.apply(p, resp);
+    }
+    this.connected = true;
+    this._puts = [];
+    this.put = GPD.bind(this, 'put');
+    this._deletes = [];
+    this.delete = GPD.bind(this, 'delete');
+    this._gets = [];
+    this.get = GPD.bind(this, 'get');
+    this._responses = {};
+  }
+  
   suites.push({
     name: "Sync Suite",
     desc: "testing the sync adapter instance",
@@ -35,9 +54,9 @@ define([], function() {
     },
     beforeEach: function(env, test){
       env.rs = new RemoteStorage();
-      env.rs.local = new RemoteStorage.InMemoryStorage();
+      env.rs.local = env.local = new RemoteStorage.InMemoryStorage();
       env.rs.caching = new FakeCaching();
-      env.rs.remote = new FakeRemote();
+      env.rs.remote = env.remote = new FakeRemote();
       test.done();
     },
     tests: [
@@ -45,6 +64,7 @@ define([], function() {
         desc: "RemoteStorage.sync() returns imediatly if not connected",
         run: function(env,test){
           var failed = false;
+          env.rs.remote.connected = false;
           env.rs.on('sync-busy', function(){
             failed = true;
           })
@@ -54,7 +74,49 @@ define([], function() {
           })
           
         }
+      },
+      {
+        desc: "RemoteStorage.sync() emits busy and done when nothing to do",
+        run: function(env,test){
+          var busy = new test.Stub(function(){});
+          var done = new test.Stub(function(){});
+          env.rs.on('sync-busy', busy)
+          env.rs.on('sync-done', done)
+          env.rs.sync().then(function(){
+            test.assertAnd(busy.numCalled, 1);
+            test.assertAnd(done.numCalled, 1);
+            test.done()
+          })
+          
+        }
+      },
+
+      {
+        desc: "Sync.sync() pushes the changes in local first",
+        run: function(env, test) {
+          env.local.put('/foo/bar/baz', 'body', 'text/plain');
+          env.local.put('/foo/bar/bla', 'body', 'text/plain');
+          env.local.delete('/foo/bar/bla');
+          env.remote._responses[['get', '/foo/', 
+                                 { ifNoneMatch: undefined } ]] = 
+            [200, {'bar/': 123}, 'application/json', 123];
+          env.remote._responses[['get', '/foo/bar/', 
+                                 { ifNoneMatch: undefined } ]] = 
+            [200, {'baz': 123}, 'application/json', 123];
+          
+          env.remote._responses[['get', '/foo/bar/baz', 
+                                 { ifNoneMatch: undefined } ]] = 
+            [200, "body", 'text/plain', 123];
+          RemoteStorage.Sync.sync(env.remote, env.local, '/foo/').then(function(){
+            test.assertAnd(env.remote._puts[0],['/foo/bar/baz', 'body', 'text/plain', { ifNoneMatch: '*' }], 'gat '+JSON.stringify(env.remote._puts[0])+' for put instead');
+            test.assertAnd(env.remote._puts.length, 1, 'too many put requests here');
+            test.assertAnd(env.remote._deletes[0],['/foo/bar/bla', {}, null, null], 'gat '+JSON.stringify(env.remote._deletes[0])+' for delete instead');
+            test.assertAnd(env.remote._deletes.length, 1);
+            test.done();
+          })
+        }
       }
+
     ]
     
   })
