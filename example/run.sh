3cec298766efd3f959713ad84f60595c3c90ee12
Michiel de Jong <michiel@unhosted.org>
2013-11-17 11:50:18 -0800

    move server back into subdir

--- example/run.sh
+++ example/run.sh
@@ -1,4 +1,4 @@
 #echo Generating self-signed cert:
-#openssl req -subj '/CN=remoteStorage example server/' -newkey rsa:2048 -new -x509 -days 3652 -nodes -out tls.cert -keyout tls.key
+#openssl req -subj '/CN=remoteStorage example server/' -newkey rsa:2048 -new -x509 -days 3652 -nodes -out server/tls.cert -keyout server/tls.key
 echo Starting server:
-sudo node server.js
+cd server/ ; sudo node server.js
