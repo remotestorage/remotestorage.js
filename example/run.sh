c336899158841a1f1a205fca28ab98aea9eff929
Michiel de Jong <michiel@unhosted.org>
2013-11-21 17:25:44 +0000

    moved to dev-kit repo

--- example/run.sh
+++
@@ -1,4 +0,0 @@
-#echo Generating self-signed cert:
-#openssl req -subj '/CN=remoteStorage example server/' -newkey rsa:2048 -new -x509 -days 3652 -nodes -out server/tls.cert -keyout server/tls.key
-echo Starting server:
-cd server/ ; sudo node server.js
