c0449bb1ec75e106c841aa96b7e026c9618b7411
Michiel de Jong <michiel@unhosted.org>
2013-12-11 14:23:04 +0000

    add minimal googledrive-suite, reduced from existing dropbox-suite

---
+++ test/unit/googledrive-suite.js
@@ -0,0 +1,168 @@
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+define(['requirejs'], function(requirejs, undefined) {
+  var suites = [];
+
+  suites.push({
+    name: "GoogleDrive Client",
+    desc: "tests for the GoogleDrive backend",
+    setup: function(env, test) {
+      global.localStorage = {};
+      global.RemoteStorage = function() {
+        RemoteStorage.eventHandling(this, 'error');
+      };
+      RemoteStorage.log = function() {};
+      RemoteStorage.prototype = {
+        setBackend: function(b){
+          this.backend = b;
+        }
+      };
+      global.RemoteStorage.Unauthorized = function() {};
+      
+
+      require('./lib/promising');
+      require('./src/eventhandling');
+      
+      if(global.rs_eventhandling) {
+        RemoteStorage.eventHandling = global.rs_eventhandling;
+      } else {
+        global.rs_eventhandling = RemoteStorage.eventHandling;
+      }
+      require('./src/wireclient');
+      
+      if(global.rs_wireclient) {
+        RemoteStorage.WireClient = global.rs_wireclient;
+      } else {
+        global.rs_wireclient = RemoteStorage.WireClient;
+      }
+      require('./src/googledrive');
+
+      test.done();
+    },
+
+    beforeEach: function(env, test) {
+      global.XMLHttpRequest = function() {
+        XMLHttpRequest.instances.push(this);
+        this._headers = {};
+        this._responseHeaders = {};
+      };
+      XMLHttpRequest.instances = [];
+      XMLHttpRequest.prototype = {
+        open: function() {
+          this._open = Array.prototype.slice.call(arguments);
+        },
+        send: function() {
+          this._send = Array.prototype.slice.call(arguments);
+        },
+        setRequestHeader: function(key, value) {
+          this._headers[key] = value;
+        },
+        getResponseHeader: function(key) {
+          return this._responseHeaders[key];
+        }
+      };
+      ['load', 'abort', 'error'].forEach(function(cb) {
+        Object.defineProperty(XMLHttpRequest.prototype, 'on' + cb, {
+          configurable: true,
+          set: function(f) {
+            this['_on' + cb] = f;
+          }
+        });
+      });
+      env.rs = new RemoteStorage();
+      env.rs.apiKeys= { googledrive: {api_key: 'testkey'} };
+      env.client = new RemoteStorage.GoogleDrive(env.rs);
+      env.connectedClient = new RemoteStorage.GoogleDrive(env.rs);
+      env.baseURI = 'https://example.com/storage/test';
+      env.token = 'foobarbaz';
+      env.connectedClient.configure(
+        'gduser', env.baseURI, undefined, env.token
+      );
+      global.Blob = function(input, options) {
+        this.input = input;
+        this.options = options;
+        env.blob = this;
+      };
+      global.FileReader = function() {};
+      FileReader.prototype = {
+        _events: {
+          loadend: []
+        },
+        addEventListener: function(eventName, handler) {
+          this._events[eventName].push(handler);
+        },
+        readAsArrayBuffer: function(blob) {
+          setTimeout(function() {
+            this.result = env.fileReaderResult = Math.random();
+            this._events.loadend[0]();
+          }.bind(this), 0);
+        }
+      };
+
+      test.done();
+    },
+
+    afterEach: function(env, test) {
+      delete global.XMLHttpRequest;
+      delete global.Blob;
+      delete global.FileReader;
+      delete env.client;
+      delete env.blob;
+      delete env.fileReaderResult;
+      test.done();
+    },
+
+    tests: [
+      {
+        desc: "#configure sets 'connected' to true, once token is given",
+        run: function(env, test) {
+          env.client.configure(undefined, undefined, undefined, 'foobarbaz');
+          test.assert(env.client.connected, true);
+        }
+      },
+
+      {
+        desc: "#get sends the request",
+        run: function(env, test) {
+          env.connectedClient.get('/foo/bar');
+          var req = XMLHttpRequest.instances.shift();
+          test.assertType(req._send, 'object');
+        }
+      },
+
+      {
+        desc: "#get sets the 'Authorization' header correctly",
+        run: function(env, test) {
+          env.connectedClient.get('/foo/bar');
+          var request = XMLHttpRequest.instances.shift();
+          test.assert(request._headers['Authorization'], 'Bearer ' + env.token);
+        }
+      },
+
+      {
+        desc: "#get returns a promise",
+        run: function(env, test) {
+          var result = env.connectedClient.get('/foo/bar');
+          test.assertTypeAnd(result, 'object');
+          test.assertType(result.then, 'function');
+        }
+      },
+
+      {
+        desc: "#get installs onload and onerror handlers on the request",
+        run: function(env, test) {
+          env.connectedClient.get('/foo/bar/');
+          var req = XMLHttpRequest.instances.shift();
+          test.assertTypeAnd(req._onload, 'function');
+          test.assertTypeAnd(req._onerror, 'function');
+          test.done();
+        }
+      }
+      
+    ]
+  });
+
+  return suites;
+});
+
