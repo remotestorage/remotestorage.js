579d427d594b41db39d978b1ddfb2cee1af09517
Michiel@unhosted <michiel@unhosted.org>
2013-12-29 14:33:46 +0530

    rewriting

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,22 +1,13 @@
 (function(global) {
 
-  var NODES_PREFIX = "remotestorage:cache:nodes:";
+  var META_PREFIX = "remotestorage:cache:meta:";
+  var BODIES_PREFIX = "remotestorage:cache:bodies:";
   var CHANGES_PREFIX = "remotestorage:cache:changes:";
 
   RemoteStorage.LocalStorage = function() {
     RemoteStorage.cachingLayer(this);
     RemoteStorage.eventHandling(this, 'change', 'conflict');
   };
-
-  function makeNode(path) {
-    var node = { path: path };
-    if (path[path.length - 1] === '/') {
-      node.body = {};
-      node.cached = {};
-      node.contentType = 'application/json';
-    }
-    return node;
-  }
 
   function b64ToUint6 (nChr) {
     return nChr > 64 && nChr < 91 ?
@@ -51,6 +42,25 @@
     return taBytes;
   }
 
+  function parsePath(path) {
+    var parts, ret = {
+      isRoot: (path === '')
+    };
+    if(path.substr(-1) === '/') {
+      parts = path.substring(0, path.length-1).split('/');
+      ret.isFolder = true;
+      ret.itemName = parts[parts.length-1]+'/';
+    } else {
+      parts = path.split('/');
+      ret.isFolder = false;
+      ret.itemName = parts[parts.length-1];
+    }
+    parts.pop();
+    ret.containingFolder = parts.join('/')+ (parts.length ? '/' : '');
+    console.log('parsePath', path, parts, ret);
+    return ret;
+  }
+
   // Helper to decide if node body is binary or not
   function isBinary(node){
     return node.match(/charset=binary/);
@@ -70,59 +80,54 @@
     toArrayBuffer: base64DecToArr,
 
     put: function(path, body, contentType, incoming, revision) {
-      var oldNode = this._get(path);
-      if (isBinary(contentType)){
-        body = this.toBase64(body);
-      }
-      var node = {
-        path: path,
-        contentType: contentType,
-        body: body
-      };
-      localStorage[NODES_PREFIX + path] = JSON.stringify(node);
+      var oldBody = this._getBody(path),
+        pathObj = parsePath(path);
       this._emit('change', {
         path: path,
         origin: incoming ? 'remote' : 'window',
-        oldValue: oldNode ? oldNode.body : undefined,
+        oldValue: oldBody,
         newValue: body
       });
-      if (incoming) {
-        this._setRevision(path, revision);
+      if (isBinary(contentType)){
+        body = this.toBase64(body);
       }
+      localStorage[BODIES_PREFIX + path] = body;
+      this._addToParent(pathObj, revision, contentType, body.length);
       if (!incoming) {
         this._recordChange(path, { action: 'PUT' });
       }
       return promising().fulfill(200);
     },
 
-    putDirectory: function(path, body, revision) {
-      this._addDirectoryCacheNode(path, body);
-      this._addToParent(path, 'body');
-      this._setRevision(path, revision);
+    putFolder: function(path, items, revision) {
+      var pathObj = parsePath(path);
+      this._setMetas(path, items);
+      this._addToParent(pathObj, revision);
       return promising().fulfill();
     },
 
     get: function(path) {
-      var node = this._get(path);
-      if (node) {
-        if (isBinary(node.contentType)){
-          node.body = this.toArrayBuffer(node.body);
+      var body = this._getBody(path),
+        meta = this._getMeta(path);
+      if (body) {
+        if (isBinary(meta.contentType)){
+          body = this.toArrayBuffer(body);
         }
-        return promising().fulfill(200, node.body, node.contentType, node.revision);
+        return promising().fulfill(200, body, meta.contentType, meta.revision);
       } else {
         return promising().fulfill(404);
       }
     },
 
     'delete': function(path, incoming) {
-      var oldNode = this._get(path);
-      delete localStorage[NODES_PREFIX + path];
+      var oldBody = this._getBody(path);
+      delete localStorage[BODIES_PREFIX + path];
       this._removeFromParent(path);
-      if (oldNode) {
+      if (oldBody) {
         this._emit('change', {
           path: path,
           origin: incoming ? 'remote' : 'window',
-          oldValue: oldNode.body,
+          oldValue: oldBody,
           newValue: undefined
         });
       }
@@ -132,25 +137,40 @@
       return promising().fulfill(200);
     },
 
-    _setRevision: function(path, revision) {
-      var node = this._get(path) || makeNode(path);
-      node.revision = revision;
-      localStorage[NODES_PREFIX + path] = JSON.stringify(node);
-      this._addToParent(path, 'cached', revision);
+    setRevision: function(path, revision) {
+      var pathObj = parsePath(path);
+      this._addToParent(pathObj, revision);
       return promising().fulfill();
     },
 
     getRevision: function(path) {
-      var node = this._get(path);
-      return promising.fulfill(node ? node.revision : undefined);
+      var meta = this._getMeta(path);
+      return promising.fulfill(meta ? meta.ETag : undefined);
     },
 
-    _get: function(path) {
-      var node;
-      try {
-        node = JSON.parse(localStorage[NODES_PREFIX + path]);
-      } catch(e) { /* ignored */ }
-      return node;
+    _getBody: function(path) {
+      return localStorage[BODIES_PREFIX + path];
+    },
+
+    _getMeta: function(path) {
+      var pathObj = parsePath(path),
+        parentItems = this._getMetas(pathObj.containingFolder);
+      return parentItems[pathObj.itemName];
+    },
+
+    _getMetas: function(path) {
+      var str = localStorage[META_PREFIX + path], items;
+      if(str.length) {
+        try {
+          items = JSON.parse(str);
+        } catch(e) {
+        }
+      }
+      return items || {};
+    },
+
+    _setMetas: function(path, items) {
+      localStorage[BODIES_PREFIX + path] = JSON.stringify(items);
     },
 
     _recordChange: function(path, attributes) {
@@ -191,56 +211,45 @@
       this._emit('conflict', event);
     },
 
-    _addToParent: function(path, key, revision) {
-      var parts = path.match(/^(.*\/)([^\/]+\/?)$/);
-      if (parts) {
-        var dirname = parts[1], basename = parts[2];
-        var node = this._get(dirname) || makeNode(dirname);
-        node[key][basename] = revision || true;
-        localStorage[NODES_PREFIX + dirname] = JSON.stringify(node);
-        if (dirname !== '/') {
-          this._addToParent(dirname, key, true);
-        }
+    _addToParent: function(pathObj, revision, contentType, contentLength) {
+      var items = this._getMetas(pathObj.containingFolder);// should trigger creation up to the root
+      //creating this folder's path up to the root:
+      if(!pathObj.isRoot && items === {}) {
+        this._addToParent(parsePath(pathObj.containingFolder), true);
       }
+      if(!items[pathObj.itemName]) {
+        items[pathObj.itemName] = {};
+      }
+      if(revision) {
+        items[pathObj.itemName].ETag = revision;
+      }
+      if(contentType) {
+        items[pathObj.itemName]['Content-Type'] = contentType;
+      }
+      if(contentLength) {
+        items[pathObj.itemName]['Content-Length'] = contentLength;
+      }
+      this._setMetas(pathObj.containingFolder, items);
     },
 
-    _addDirectoryCacheNode: function(path, body) {
-      var node = this._get(path) || makeNode(path);
-      node.body = body;
-      localStorage[NODES_PREFIX + path] = JSON.stringify(node);
-    },
-
-    _removeFromParent: function(path) {
-      var parts = path.match(/^(.*\/)([^\/]+\/?)$/);
-      if (parts) {
-        var dirname = parts[1], basename = parts[2];
-        var node = this._get(dirname);
-        if (node) {
-          delete node.cached[basename];
-          if (Object.keys(node.cached).length > 0) {
-            localStorage[NODES_PREFIX + dirname] = JSON.stringify(node);
-          } else {
-            delete localStorage[NODES_PREFIX + dirname];
-            if (dirname !== '/') {
-              this._removeFromParent(dirname);
-            }
-          }
-        }
-      }
+    _removeFromParent: function(pathObj) {
+      var items = this._getMetas(pathObj.containingFolder);// should trigger creation up to the root
+      delete items[pathObj.itemName];
+      this._setMetas(pathObj.containingFolder, items);
     },
 
     fireInitial: function() {
-      var l = localStorage.length, npl = NODES_PREFIX.length;
+      var l = localStorage.length, bpl = BODIES_PREFIX.length;
       for(var i=0;i<l;i++) {
         var key = localStorage.key(i);
-        if (key.substr(0, npl) === NODES_PREFIX) {
-          var path = key.substr(npl);
-          var node = this._get(path);
+        if (key.substr(0, bpl) === BODIES_PREFIX) {
+          var path = key.substr(bpl);
+          var body = this._getBody(path);
           this._emit('change', {
             path: path,
             origin: 'local',
             oldValue: undefined,
-            newValue: node.body
+            newValue: body
           });
         }
       }
@@ -256,11 +265,12 @@
 
   RemoteStorage.LocalStorage._rs_cleanup = function() {
     var l = localStorage.length;
-    var npl = NODES_PREFIX.length, cpl = CHANGES_PREFIX.length;
+    var bpl = BODIES_PREFIX.length, mpl = META_PREFIX.length, cpl = CHANGES_PREFIX.length;
     var remove = [];
     for(var i=0;i<l;i++) {
       var key = localStorage.key(i);
-      if (key.substr(0, npl) === NODES_PREFIX ||
+      if (key.substr(0, bpl) === BODIES_PREFIX ||
+         key.substr(0, mpl) === META_PREFIX ||
          key.substr(0, cpl) === CHANGES_PREFIX) {
         remove.push(key);
       }
