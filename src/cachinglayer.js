e170c9bbd059b710529d3601d6c9e6f480942f63
Michiel@unhosted <michiel@unhosted.org>
2014-01-13 07:56:31 +0530

    draft with new data format

--- src/cachinglayer.js
+++ src/cachinglayer.js
@@ -12,17 +12,28 @@
       return path.substr(-1) === '/';
     },
     
+    _isDocument: function(path) {
+      return path.substr(-1) !== '/';
+    },
+    
+    _getLatest: function(node) {
+      var ret;
+      if (node.local) {
+        ret = node.local;
+      } else {
+        ret = node.agreed;
+      }
+      if (ret.body && ret.contentType) {
+        return ret;
+      }
+    },
+    
     //GPD interface:
     get: function(path) {
       var promise = promising();
       this.getNodes([path]).then(function(objs) {
-        var latest;
-        if (objs[path].local) {
-          latest = objs[path].local;
-        } else {
-          latest = objs[path].official;
-        }
-        if (latest && latest.body && latest.contentType) {
+        var latest = this._getLatest(objs[path]);
+        if (latest) {
             promise.fulfill(200, latest.body, latest.contentType);
         } else {
           promise.fulfill(404);
@@ -32,47 +43,61 @@
       });
       return promise;
     },
-    put: function(path, body, contentType) {
-      var i, now = new Date().getTime(), pathNodes = this._nodesFromRoot(path);
-      return this.getNodes(pathNodes).then(function(objs) {
-        for(i=0; i<pathNodes.lengh; i++) {
-          if (!objs[pathNodes[i]]) {
-            objs[pathNodes[i]]] = {
-              path: pathNodes[i],
-              official: {
-                timestamp: now
-              }
-            };
-            if(this._isFolder(pathNodes[i])) {
-              objs[pathNodes[i]]].official.items = {};
-            }
-          }
-          if(i === 0) {
-            //save the document itself
-            objs[path].local = {
-              body: body,
-              contentType: contentType,
-              timestamp: now
-            };
-          } else {
-            //add it to all parents
-            itemName = pathNodes[i-1].substring(pathNodes[i].length);
-            if (!objs[pathNodes[i]]].local) {
-              objs[pathNodes[i]]].local = objs[pathNodes[i]]].official;
-            }
-            objs[pathNodes[i]]].local.itemsMap[itemName] = true;
+    _updateNodes: function(nodePaths, cb) {
+       return this.getNodes(nodePaths).then(function(objs) {
+        var copyObjs = this._clone(objs);
+        objs = cb(objs);
+        for (i in objs) {
+          if (this._equal(objs[i], copyObjs[i])) {
+            delete objs[i];
           }
         }
         return this.setNodes(objs);
       });
     },
-    delete: function(path) {
+    _makeNode: function(path, now) {
+      var ret = {
+        path: path,
+        official: {
+          timestamp: now
+        }
+      };
+      if(this._isFolder(path)) {
+        ret.official.items = {};
+      }
+      return ret;
+    },
+    put: function(path, body, contentType) {
       var i, now = new Date().getTime(), pathNodes = this._nodesFromRoot(path);
-      return this.getNodes(pathNodes).then(function(objs) {
-        for(i=0; i<pathNodes.lengh; i++) {
+      return this._updateNodes(pathNodes, function(objs) {
+        for (i=0; i<pathNodes.lengh; i++) {
+          if (!objs[pathNodes[i]]) {
+            objs[pathNodes[i]]] = makeNode(now, path);
+          }
+          if (i === 0) {
+            //save the document itself
+            objs[path].local = { body: body, contentType: contentType, timestamp: now };
+          } else {
+            / /add it to all parents
+            itemName = pathNodes[i-1].substring(pathNodes[i].length);
+            if (!objs[pathNodes[i]]].local) {
+              objs[pathNodes[i]]].local = this._clone(objs[pathNodes[i]]].official);
+            }
+            objs[pathNodes[i]]].local.itemsMap[itemName] = true;
+          }
+        }
+        return objs;
+      });
+    },
+    delete: function(path) {
+      var pathNodes = this._nodesFromRoot(path);
+      return this._updateNodes(pathNodes, function(objs) {
+        var i, now = new Date().getTime();
+        for (i=0; i<pathNodes.lengh; i++) {
           if (!objs[pathNodes[i]]) {
             throw new Exception('cannot delete a non-existing node; retrieve its parent folder first');
           }
+          objs[pathNodes[i]] = this._migrateNode(objs[pathNodes[i]]);
           if(i === 0) {
             //delete the document itself
             objs[path].local = {
@@ -82,25 +107,20 @@
             //remove it from all parents
             itemName = pathNodes[i-1].substring(pathNodes[i].length);
             if (!objs[pathNodes[i]]].local) {
-              objs[pathNodes[i]]].local = objs[pathNodes[i]]].official;
+              objs[pathNodes[i]]].local = this._clone(objs[pathNodes[i]]].official);
             }
             delete objs[pathNodes[i]]].local.itemsMap[itemName];
           }
         }
-        return this.setNodes(objs);
+        return objs;
       });
     },
-
     fireInitial: function() {
       this.forAllNodes(function(node) {
         var latest;
-        if (this._isFolder(node.path)) {
-          if (node.local) {
-            latest = node.local;
-          } else {
-            latest = node.agreed;
-          }
-          if (latest && latest.body && latest.contentType) {
+        if (this._isDocument(node.path)) {
+          latest = this._getLatest(node);
+          if (latest) {
             this._emit('change', {
               path: node.path,
               origin: 'local',
@@ -112,176 +132,9 @@
           }
         }
       });
-    },
-    _disagree: function(path, remoteRevision) {
-    
-    },
-    handleResponse: function(path, method, status, revision) {
-      if (status ==== 412) {
-        _disagree(path, revision);
-      } else if (method === 'PUT') {
-        _agree(path, revision);  
-      } else if (method === 'DELETE')
+    }
   };
 
-
-  //1, a local document is created:
-  {
-    path: string,
-    official: { timestamp },
-    local: { body, contentType, timestamp }
-  }
-
-  //2, a push request is initiated:
-  {
-    path: string,
-    official: { timestamp },
-    sent: { body, contentType, timestamp },
-    local: { body, contentType, timestamp }
-  }
-
-  //3, it is successful:
-  {
-    path: string,
-    official: { body, contentType, revision, timestamp },
-    local: { body, contentType, timestamp }
-  }
-
-  //it errors or times out:
-  // back to 1
-  
-  //4, it's a conflict:
-  {
-    path: string,
-    official: { timestamp },
-    local: { body, contentType, timestamp },
-    remote: { revision, timestamp }
-  }
-  
-  //5, the remote version is retrieved:
-  {
-    path: string,
-    official: { timestamp },
-    local: { body, contentType, timestamp },
-    remote: { body, contentType, revision, timestamp }
-  }
-
-  //the module resolves the conflict as local-wins ([remote] moves to [agreed]):
-  {
-    path: string,
-    official: { body, contentType, revision, timestamp },
-    local: { body, contentType, timestamp }
-  }
-
-  //the module resolves the conflict as remote-wins ([local] is discarded and [remote] moves to [official] and [local]):
-  {
-    path: string,
-    official: { body, contentType, revision, timestamp },
-    local: { body, contentType, revision, timestamp }
-  }
-
-  //a local change is made while the outgoing push is still pending:
-  {
-    path: string,
-    official: { timestamp },
-    sent: { body, contentType, timestamp },
-    local: { body, contentType, timestamp }
-  }
-  
-  //during conflict, the parent folder is retrieved and the revision matches [remote]
-  {
-    path: string,
-    local: { body, contentType, timestamp },
-    agreed: {},
-    remote: { body, contentType, contentLength, revision, timestamp }
-  }
-  
-  //during conflict, the parent folder is retrieved and the revision doesn't match [remote]
-  {
-    path: string,
-    local: { body, contentType, timestamp },
-    agreed: {},
-    remote: { revision, contentType, contentLength, timestamp } //removing body, updating other fields, re-emitting conflict event
-  }
-  
-  //during agreed, the parent folder is retrieved and the revision matches [local]
-  {
-    path: string,
-    local: { body, contentType, contentLength, revision, timestamp },
-    agreed: { body, contentType, contentLength, revision, timestamp }
-  }
-  
-  //during agreed, the parent folder is retrieved and the revision doesn't match [local]
-  {
-    path: string,
-    local: { body, contentType, contentLength, revision, timestamp },
-    agreed: true,
-    remote: { revision, contentType, contentLength, timestamp } //removing body, updating other fields, re-emitting conflict event   
-  }
-  
-  //other fields:
-  {
-    path: string,
-    fetching: timestamp, // a fetch is initiated
-    syncedTree: timestamp,
-    syncedAll: timestamp
-  }
- 
-//in sync: 
-1  . . . . [official]
- 
-//fetching:
-2  . . . . [official]
-                \
-                 \ . . . . [fetch]
-  
-//dirty:
-3  . . . . [official]
-                \
-                 \ . . . . [remote]
- 
-//dirty and fetching:
-4  . . . . [official]
-                \
-                 \ . . . . [fetch] . . . . [remote]
- 
-//local change:
-5  . . . . [official] . . . . [local]
- 
-//local change and fetching:
-6  . . . . [official] . . . . [local]
-                \
-                 \ . . . . [fetch]
- 
-//local change and known dirty:
-7  . . . . [official] . . . . [local]
-                \
-                 \ . . . . [remote]
- 
-//conflict and dirty
-8  . . . . [official] . . . . [local]
-                \
-                 \ . . . . [fetch] . . . . [remote]
- 
-//local change and pushing:
-9  . . . . [official] . . . . [push] . . . . [local]
- 
-//local change, pushing and fetching (should abort the fetch):
-10  . . . . [official] . . . . [push] . . . . [local]
-                 \
-                  \ . . . . [fetch]
- 
-//local change, pushing, and known dirty (should abort the push):
-11  . . . . [official] . . . . [push] . . . . [local]
-                 \
-                  \ . . . . [remote]
- 
-//local change, pushing, known dirty, and fetching (should abort the push)
-12  . . . . [official] . . . . [push] . . . . [local]
-                 \
-                  \ . . . . [fetch] . . . . [remote]
- 
-  
   /**
    * Function: cachingLayer
    *
--- src/sync.js
+++ src/sync.js
@@ -1,233 +1,6 @@
 (function(global) {
 
-  //
-  // The synchronization algorithm is as follows:
-  //
-  // (for each path in caching.rootPaths)
-  //
-  // (1) Fetch all pending changes from 'local'
-  // (2) Try to push pending changes to 'remote', if that fails mark a
-  //     conflict, otherwise clear the change.
-  // (3) Folder items: GET a listing
-  //     File items: GET the file
-  // (4) Compare versions. If they match the locally cached one, return.
-  //     Otherwise continue.
-  // (5) Folder items: For each child item, run this algorithm starting at (3).
-  //     File items: Fetch remote data and replace locally cached copy.
-  //
-  // Depending on the API version the server supports, the version comparison
-  // can either happen on the server (through ETag, If-Match, If-None-Match
-  // headers), or on the client (through versions specified in the parent listing).
-  //
-
   var syncInterval = 10000;
-
-  function isFolder(path) {
-    return path[path.length - 1] === '/';
-  }
-
-  function descendInto(remote, local, path, keys, promise) {
-    var n = keys.length, i = 0;
-    if (n === 0) { promise.fulfill(); }
-    function oneDone() {
-      i++;
-      if (i === n) { promise.fulfill(); }
-    }
-    keys.forEach(function(key) {
-      synchronize(remote, local, path + key).then(oneDone);
-    });
-  }
-
-  function updateLocal(remote, local, path, body, contentType, revision, promise) {
-    if (isFolder(path)) {
-      local.putFolder(path, body, revision).then(function() {
-        descendInto(remote, local, path, Object.keys(body), promise);
-      });
-    } else {
-      local.put(path, body, contentType, true, revision).then(promise.fulfill);
-    }
-  }
-
-  function allDifferentKeys(a, b) {
-    var keyObject = {};
-    for (var ak in a) {
-      if (JSON.stringify(a[ak]) !== JSON.stringify(b[ak])) {
-        keyObject[ak] = true;
-      }
-    }
-    for (var bk in b) {
-      if (JSON.stringify(a[bk]) !== JSON.stringify(b[bk])) {
-        keyObject[bk] = true;
-      }
-    }
-    return Object.keys(keyObject);
-  }
-
-  function promiseDeleteLocal(local, path) {
-    var promise = promising();
-    deleteLocal(local, path, promise);
-    return promise;
-  }
-
-  function deleteLocal(local, path, promise) {
-    if (isFolder(path)) {
-      local.get(path).then(function(localStatus, localBody, localContentType, localRevision) {
-        var keys = [], failed = false;
-        for (var item in localBody) {
-          keys.push(item);
-        }
-        var n = keys.length, i = 0;
-        if (n === 0) { promise.fulfill(); }
-
-        function oneDone() {
-          i++;
-          if (i === n && !failed) { promise.fulfill(); }
-        }
-
-        function oneFailed(error) {
-          if (!failed) {
-            failed = true;
-            promise.reject(error);
-          }
-        }
-
-        keys.forEach(function(key) {
-          promiseDeleteLocal(local, path + key).then(oneDone, oneFailed);
-        });
-      });
-    } else {
-      local.delete(path, true).then(promise.fulfill, promise.reject);
-    }
-  }
-
-  function synchronize(remote, local, path, options) {
-    var promise = promising();
-    local.get(path).then(function(localStatus, localBody, localContentType, localRevision) {
-      remote.get(path, {
-        ifNoneMatch: localRevision
-      }).then(function(remoteStatus, remoteBody, remoteContentType, remoteRevision) {
-        if (remoteStatus === 401 || remoteStatus === 403) {
-          throw new RemoteStorage.Unauthorized();
-        } else if (remoteStatus === 412 || remoteStatus === 304) {
-          // up to date.
-          promise.fulfill();
-        } else if (localStatus === 404 && remoteStatus === 200) {
-          // local doesn't exist, remote does.
-          updateLocal(remote, local, path, remoteBody, remoteContentType, remoteRevision, promise);
-        } else if (localStatus === 200 && remoteStatus === 404) {
-          // remote doesn't exist, local does.
-          deleteLocal(local, path, promise);
-        } else if (localStatus === 200 && remoteStatus === 200) {
-          if (isFolder(path)) {
-            if (remoteRevision && remoteRevision === localRevision) {
-              promise.fulfill();
-            } else {
-              local.putFolder(path, remoteBody, remoteRevision).then(function() {
-                // TODO Factor in  `cached` items of folder cache node
-                var differentObjects = allDifferentKeys(localBody, remoteBody);
-                descendInto(remote, local, path, differentObjects, promise);
-              });
-            }
-          } else {
-            updateLocal(remote, local, path, remoteBody, remoteContentType, remoteRevision, promise);
-          }
-        } else {
-          promise.fulfill();
-        }
-      }).then(undefined, promise.reject);
-    }).then(undefined, promise.reject);
-    return promise;
-  }
-
-  function fireConflict(local, path, attributes) {
-    local.setConflict(path, attributes);
-  }
-
-  function pushChanges(remote, local, path) {
-    return local.changesBelow(path).then(function(changes) {
-      var n = changes.length, i = 0;
-      var promise = promising();
-      function oneDone(path) {
-        function done() {
-          i++;
-          if (i === n) { promise.fulfill(); }
-        }
-        if (path) {
-          // change was propagated -> clear.
-          local.clearChange(path).then(done);
-        } else {
-          // change wasn't propagated (conflict?) -> handle it later.
-          done();
-        }
-      }
-      if (n > 0) {
-        var errored = function(err) {
-          console.error("pushChanges aborted due to error: ", err, err.stack);
-          promise.reject(err);
-        };
-        changes.forEach(function(change) {
-          if (change.conflict) {
-            var res = change.conflict.resolution;
-            if (res) {
-              RemoteStorage.log('about to resolve', res);
-              // ready to be resolved.
-              change.action = (res === 'remote' ? change.conflict.remoteAction : change.conflict.localAction);
-              change.force = true;
-            } else {
-              RemoteStorage.log('conflict pending for ', change.path);
-              // pending conflict, won't do anything.
-              return oneDone();
-            }
-          }
-          switch(change.action) {
-          case 'PUT':
-            var options = {};
-            if (! change.force) {
-              if (change.revision) {
-                options.ifMatch = change.revision;
-              } else {
-                options.ifNoneMatch = '*';
-              }
-            }
-            local.get(change.path).then(function(status, body, contentType) {
-              if (status === 200) {
-                return remote.put(change.path, body, contentType, options);
-              } else {
-                return 200; // fake 200 so the change is cleared.
-              }
-            }).then(function(status) {
-              if (status === 412) {
-                fireConflict(local, change.path, {
-                  localAction: 'PUT',
-                  remoteAction: 'PUT'
-                });
-                oneDone();
-              } else {
-                oneDone(change.path);
-              }
-            }).then(undefined, errored);
-            break;
-          case 'DELETE':
-            remote.delete(change.path, {
-              ifMatch: change.force ? undefined : change.revision
-            }).then(function(status) {
-              if (status === 412) {
-                fireConflict(local, change.path, {
-                  remoteAction: 'PUT',
-                  localAction: 'DELETE'
-                });
-                oneDone();
-              } else {
-                oneDone(change.path);
-              }
-            }).then(undefined, errored);
-            break;
-          }
-        });
-        return promise;
-      }
-    });
-  }
 
   /**
    * Class: RemoteStorage.Sync
@@ -237,18 +10,18 @@
      * Method: sync
      **/
     sync: function(remote, local, path) {
-      return pushChanges(remote, local, path).
-        then(function() {
-          return synchronize(remote, local, path);
-        });
+      var promise = promising();
+      promise.fulfill();
+      return promise;
     },
+    
     /**
      * Method: syncTree
      **/
     syncTree: function(remote, local, path) {
-      return synchronize(remote, local, path, {
-        data: false
-      });
+      var promise = promising();
+      promise.fulfill();
+      return promise;
     }
   };
 
@@ -263,6 +36,7 @@
   RemoteStorage.prototype.getSyncInterval = function() {
     return syncInterval;
   };
+
   /**
    * Method: setSyncInterval
    *
