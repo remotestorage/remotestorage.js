2bfc96f23bc07c0d83ac80b27371cb45fb7c8323
Michiel B. de Jong <michiel@unhosted.org>
2014-01-21 10:20:22 +0000

    new caching unit tests

--- test/unit/caching-suite.js
+++ test/unit/caching-suite.js
@@ -4,6 +4,13 @@ if (typeof define !== 'function') {
 
 var promising = require('./lib/promising');
 
+//    this.FLUSH = 0;
+//    this.SEEN = 1;
+//    this.FOLDERS = 2;
+//    this.SEEN_AND_FOLDERS = 3;
+//    this.DOCUMENTS = 4;
+//    this.ALL = 7;
+// default: SEEN
 define(['requirejs', 'fs'], function(requirejs, fs, undefined) {
   var suites = [];
 
@@ -26,9 +33,10 @@ define(['requirejs', 'fs'], function(requirejs, fs, undefined) {
 
     tests: [
       {
-        desc: "#get() returns undefined for paths that haven't been configured",
+        desc: "#get() returns the default caching.SEEN for paths that haven't been configured",
         run: function(env, test) {
-          test.assertType(env.caching.get('/foo/'), 'undefined');
+          test.assertAnd(env.caching.get('/foo/'), env.caching.SEEN);
+          test.done();
         }
       },
 
@@ -38,18 +46,6 @@ define(['requirejs', 'fs'], function(requirejs, fs, undefined) {
           try {
             env.caching.get();
             test.result(false, "get() didn't fail");
-          } catch(e) {
-            test.result(true);
-          }
-        }
-      },
-
-      {
-        desc: "#remove() with no path given throws an error",
-        run: function(env, test) {
-          try {
-            env.caching.remove();
-            test.result(false, "remove() didn't fail");
           } catch(e) {
             test.result(true);
           }
@@ -81,206 +77,84 @@ define(['requirejs', 'fs'], function(requirejs, fs, undefined) {
       },
 
       {
-        desc: "#set() sets caching settings for given path",
+        desc: "#set() sets caching settings for given path and subtree",
         run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          test.assert(env.caching.get('/foo/'), { data: true });
-        }
-      },
-
-      {
-        desc: "#remove() removes caching settings from given path",
-        run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          test.assertTypeAnd(env.caching.get('/foo/'), 'object');
-          env.caching.remove('/foo/');
-          test.assertType(env.caching.get('/foo/'), 'undefined');
-        }
-      },
-
-      {
-        desc: "#descendIntoPath() with no path given throws an error",
-        run: function(env, test) {
-          try {
-            env.caching.descendIntoPath();
-            test.result(false, "descendIntoPath() didn't fail");
-          } catch(e) {
-            test.result(true);
-          }
-        }
-      },
-
-      {
-        desc: "#cachePath() with no path given throws an error",
-        run: function(env, test) {
-          try {
-            env.caching.cachePath();
-            test.result(false, "syncDataIn() didn't fail");
-          } catch(e) {
-            test.result(true);
-          }
-        }
-      },
-
-      {
-        desc: "#descendIntoPath() with a file path given throws an error",
-        run: function(env, test) {
-          try {
-            env.caching.descendIntoPath('/foo/bar');
-            test.result(false, "descendIntoPath() didn't fail");
-          } catch(e) {
-            test.result(true);
-          }
-        }
-      },
-
-      {
-        desc: "#descendIntoPath() works for configured paths",
-        run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          test.assertAnd(env.caching.descendIntoPath('/foo/'), true);
-          test.assert(env.caching.descendIntoPath('/bar/'), false);
-        }
-      },
-
-      {
-        desc: "#descendIntoPath() works for subfolders",
-        run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          test.assertAnd(env.caching.descendIntoPath('/foo/bar/'), true);
-          test.assert(env.caching.descendIntoPath('/foo/bar/baz/'), true);
+          env.caching.set('/foo/', env.caching.FOLDERS);
+          test.assertAnd(env.caching.get('/'), env.caching.SEEN);
+          test.assertAnd(env.caching.get('/bar'), env.caching.SEEN);
+          test.assertAnd(env.caching.get('/bar/'), env.caching.SEEN);
+          test.assertAnd(env.caching.get('/bar/foo'), env.caching.SEEN);
+          test.assertAnd(env.caching.get('/foo/'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.get('/foo/bar'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.get('/foo/bar/'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.get('/foo/bar/baz'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.get('/foo/bar/baz/'), env.caching.FOLDERS);
         }
       },
 
       {
         desc: "#rootPaths contains configured paths",
         run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          test.assert(env.caching.rootPaths, ['/foo/']);
+          env.caching.set('/foo/', env.caching.ALL);
+          test.assert(env.caching.rootPaths, {'/foo/': env.caching.ALL });
         }
       },
 
       {
-        desc: "#rootPaths doesn't contain paths that overlap entirely, but only the shortest one",
+        desc: "#rootPaths doesn't contain paths that set their inherited value",
         run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          env.caching.set('/bar/', { data: true });
-          env.caching.set('/foo/bar/baz/', { data: true });
-          test.assert(env.caching.rootPaths, ['/foo/', '/bar/']);
+          env.caching.set('/foo/', env.caching.ALL);
+          env.caching.set('/foo/bar/baz/', env.caching.ALL);
+          env.caching.set('/bar/foo/baz/', env.caching.FOLDERS);
+          env.caching.set('/bar/', env.caching.FOLDERS);
+          test.assert(env.caching.rootPaths, {
+            '/foo/': env.caching.ALL,
+            '/bar/': env.caching.FOLDERS
+          });
         }
       },
 
       {
-        desc: "#rootPaths doesn't contain paths that have been removed",
+        desc: "#checkPath returns value of tightest fitting rootPath",
         run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          env.caching.set('/foo/bar/baz/', { data: true });
-          env.caching.set('/bar/', { data: true });
-          env.caching.remove('/foo/');
-          test.assert(env.caching.rootPaths, ['/foo/bar/baz/', '/bar/']);
+          env.caching.set('/foo/', env.caching.ALL);
+          env.caching.set('/foo/bar/baz', env.caching.DOCUMENTS);
+          env.caching.set('/foo/baf/', env.caching.SEEN);
+          env.caching.set('/bar/', env.caching.FOLDERS);
+          test.assertAnd(env.caching.checkPaths('/foo/'), env.caching.ALL);
+          test.assertAnd(env.caching.checkPaths('/foo/1'), env.caching.ALL);
+          test.assertAnd(env.caching.checkPaths('/foo/2/'), env.caching.ALL);
+          test.assertAnd(env.caching.checkPaths('/foo/2/3'), env.caching.ALL);
+          test.assertAnd(env.caching.checkPaths('/foo/bar/'), env.caching.ALL);
+          test.assertAnd(env.caching.checkPaths('/foo/bar/baz'), env.caching.DOCUMENTS);
+          test.assertAnd(env.caching.checkPaths('/foo/baf/'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/foo/baf/1'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/foo/baf/2/'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/foo/baf/2/1'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/bar/'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.checkPaths('/bar/1'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.checkPaths('/bar/2/'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.checkPaths('/bar/2/3'), env.caching.FOLDERS);
+          test.assertAnd(env.caching.checkPaths('/'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/1'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/2/'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/2/3'), env.caching.SEEN);
+          test.assertAnd(env.caching.checkPaths('/2/3/'), env.caching.SEEN);
+          test.done();
         }
       },
 
       {
         desc: "#reset resets the state",
         run: function(env, test) {
-          env.caching.set('/foo/', { data: true });
-          env.caching.set('/bar/', { data: false });
+          env.caching.set('/foo/', env.caching.ALL);
+          env.caching.set('/foo/bar/baz/', env.caching.ALL);
+          env.caching.set('/bar/foo/baz/', env.caching.FOLDERS);
+          env.caching.set('/bar/', env.caching.FOLDERS);
           env.caching.reset();
-          test.assertTypeAnd(env.caching.get('/foo/'), 'undefined');
-          test.assertTypeAnd(env.caching.get('/bar/'), 'undefined');
+          test.assertTypeAnd(env.caching.get('/foo/'), env.caching.SEEN);
+          test.assertTypeAnd(env.caching.get('/bar/'), env.caching.SEEN);
           test.assert(env.caching.rootPaths, []);
-        }
-      },
-
-      {
-        desc: "waitForPath queues a promise if rootPath not ready and waitForRemote is true",
-        run: function(env, test) {
-          env.caching.enable('/foo/', true);
-          var promise = env.caching.waitForPath('/foo/bar');
-          test.assertAnd(env.caching.queuedPromises, {
-            '/foo/bar': [promise]
-          });
-          env.caching.set('/foo/', {data: true, ready: true});
-          test.assertAnd(env.caching.queuedPromises, {});
-          promise.then(function() {
-            test.done();
-          });
-        }
-      },
-
-      {
-        desc: "waitForPath doesn't queue a promise if rootPath not ready and waitForRemote is false",
-        run: function(env, test) {
-          env.caching.enable('/foo/', false);
-          var promise = env.caching.waitForPath('/foo/bar');
-          test.assertAnd(typeof(env.caching.queuedPromises), 'undefined');
-          promise.then(function() {
-            test.done();
-          });
-        }
-      },
-
-      {
-        desc: "waitForPath also fulfills the promise if rootPath ready beforehand",
-        run: function(env, test) {
-          env.caching.enable('/foo/');
-          env.caching.set('/foo/', {data: true, ready: true});
-          var promise = env.caching.waitForPath('/foo/bar');
-          test.assertAnd(env.caching.queuedPromises, undefined);
-          promise.then(function() {
-            test.done();
-          });
-        }
-      },
-
-      {
-        desc: "when setting waitForRemote to true, cachePathReady returns true if and only if ready is set",
-        run: function(env, test) {
-          env.caching.enable('/foo/', true);
-          env.caching.enable('/bar/', true);
-          test.assertAnd(env.caching.cachePathReady('/foo/'), false);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/'), false);
-          test.assertAnd(env.caching.cachePathReady('/foo/baz.txt'), false);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/baz.txt'), false);
-          test.assertAnd(env.caching.cachePathReady('/bar/'), false);
-          test.assertAnd(env.caching.cachePathReady('/bar/foo/'), false);
-          test.assertAnd(env.caching.cachePathReady('/bar/baz.txt'), false);
-          test.assertAnd(env.caching.cachePathReady('/bar/foo/baz.txt'), false);
-          env.caching.set('/bar/', { data: true, ready: true });
-          test.assertAnd(env.caching.cachePathReady('/foo/'), false);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/'), false);
-          test.assertAnd(env.caching.cachePathReady('/foo/baz.txt'), false);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/baz.txt'), false);
-          test.assertAnd(env.caching.cachePathReady('/bar/'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/foo/'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/baz.txt'), true);
-          test.assert(env.caching.cachePathReady('/bar/foo/baz.txt'), true);
-        }
-      },
-
-      {
-        desc: "when setting waitForRemote to false, cachePathReady always returns true",
-        run: function(env, test) {
-          env.caching.enable('/foo/', false);
-          env.caching.enable('/bar/', false);
-          test.assertAnd(env.caching.cachePathReady('/foo/'), true);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/'), true);
-          test.assertAnd(env.caching.cachePathReady('/foo/baz.txt'), true);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/baz.txt'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/foo/'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/baz.txt'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/foo/baz.txt'), true);
-          env.caching.set('/bar/', { data: true, ready: true });
-          test.assertAnd(env.caching.cachePathReady('/foo/'), true);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/'), true);
-          test.assertAnd(env.caching.cachePathReady('/foo/baz.txt'), true);
-          test.assertAnd(env.caching.cachePathReady('/foo/bar/baz.txt'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/foo/'), true);
-          test.assertAnd(env.caching.cachePathReady('/bar/baz.txt'), true);
-          test.assert(env.caching.cachePathReady('/bar/foo/baz.txt'), true);
         }
       }
     ]
