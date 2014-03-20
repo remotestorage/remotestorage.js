f4bd4293834b533659007c31d8a011ee8dcb7a98
Sebastian Kippe <sebastian@kip.pe>
2014-03-20 18:44:54 +0400

    Improve defineModule docs

--- src/modules.js
+++ src/modules.js
@@ -1,32 +1,39 @@
 (function() {
 
   RemoteStorage.MODULES = {};
+
   /*
-     Method: RemoteStorage.defineModule
-
-     The defineModule method takes a module name and a builder function as parameters.
-
-     The function should return an object containing an object called exports,
-     which will be exported to any remoteStorage instance under the module's name.
-
-     So when having an a locations module, like in the example, it would be accessible
-     via remoteStorage.locations, which would have a features and collections property.
-
-     The function gets a private and a public client, which are both
-     scopes. In this example the scope of privateClient is /locations and the
-     scope of publicClient is /public/locations.
-
-     Example:
-     (start code)
-     RemoteStorage.defineModule('locations', function(privateClient, publicClient) {
-       return {
-         exports: {
-           features: privateClient.scope('features/').defaultType('feature'),
-           collections: privateClient.scope('collections/').defaultType('feature-collection')
-         }
-       };
-     });
-     (end code)
+   * Method: RemoteStorage.defineModule
+   *
+   * Method for defining a new remoteStorage data module
+   *
+   * Parameters:
+   *   moduleName - Name of the module
+   *   builder    - Builder function defining the module
+   *
+   * The module builder function should return an object containing another
+   * object called exports, which will be exported to any <RemoteStorage>
+   * instance under the module's name. So when defining a locations module,
+   * like in the example below, it would be accessible via
+   * `remoteStorage.locations`, which would in turn have a `features` and a
+   * `collections` property.
+   *
+   * The function receives a private and a public client, which are both
+   * instances of <RemoteStorage.BaseClient>. In the following example, the
+   * scope of privateClient is `/locations` and the scope of publicClient is
+   * `/public/locations`.
+   *
+   * Example:
+   *   (start code)
+   *   RemoteStorage.defineModule('locations', function(privateClient, publicClient) {
+   *     return {
+   *       exports: {
+   *         features: privateClient.scope('features/').defaultType('feature'),
+   *         collections: privateClient.scope('collections/').defaultType('feature-collection')
+   *       }
+   *     };
+   *   });
+   * (end code)
   */
 
   RemoteStorage.defineModule = function(moduleName, builder) {
