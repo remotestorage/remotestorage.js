22b1e71b982283b52671238c95ff5a7c797fbeca
Sebastian Kippe <sebastian@kip.pe>
2014-03-20 18:18:01 +0400

    Improve RemoteStorage.Caching documentation, fix double header

--- src/caching.js
+++ src/caching.js
@@ -1,24 +1,26 @@
-/**
- * Class: RemoteStorage.Caching
- *
- * Caching strategies:
- *
- *   For each subtree, you can set the caching strategy to 'ALL',
- *   'SEEN' (default), and 'FLUSH'.
- *
- *   - 'ALL' means that once all outgoing changes have been pushed, sync
- *         will start retrieving nodes to cache pro-actively. If a local
- *         copy exists of everything, it will check on each sync whether
- *         the ETag of the root folder changed, and retrieve remote changes
- *         if they exist.
- *   - 'SEEN' does this only for documents and folders that have been either
- *         read from or written to at least once since connecting to the current
- *         remote backend, plus their parent/ancestor folders up to the root
- *         (to make tree-based sync possible).
- *   - 'FLUSH' will only cache outgoing changes, and forget them as soon as
- *         they have been saved to remote successfully.
- *
- **/
+  /**
+   * Class: RemoteStorage.Caching
+   *
+   * Holds/manages caching configuration.
+   *
+   * Caching strategies:
+   *
+   *   For each subtree, you can set the caching strategy to 'ALL',
+   *   'SEEN' (default), and 'FLUSH'.
+   *
+   *   - 'ALL' means that once all outgoing changes have been pushed, sync
+   *         will start retrieving nodes to cache pro-actively. If a local
+   *         copy exists of everything, it will check on each sync whether
+   *         the ETag of the root folder changed, and retrieve remote changes
+   *         if they exist.
+   *   - 'SEEN' does this only for documents and folders that have been either
+   *         read from or written to at least once since connecting to the current
+   *         remote backend, plus their parent/ancestor folders up to the root
+   *         (to make tree-based sync possible).
+   *   - 'FLUSH' will only cache outgoing changes, and forget them as soon as
+   *         they have been saved to remote successfully.
+   *
+   **/
 
 (function(global) {
   var SETTINGS_KEY = "remotestorage:caching";
@@ -34,18 +36,12 @@
     return path.replace(/\/+/g, '/').replace(/[^\/]+\/?$/, '');
   }
 
-  /**
-   * Class: RemoteStorage.Caching
-   *
-   * Holds caching configuration.
-   */
   RemoteStorage.Caching = function() {
     this.reset();
   };
 
   RemoteStorage.Caching.prototype = {
     pendingActivations: [],
-
 
     /**
      * Method: set
@@ -55,8 +51,12 @@
      * Not needed when using <enable>/<disable>.
      *
      * Parameters:
-     *   path
-     *   value
+     *   path  - Path to cache
+     *   value - Caching strategy. One of 'ALL', 'SEEN', or 'FLUSH'.
+     *
+     * Example:
+     *   (start code)
+     *   remoteStorage.caching.set('/bookmarks/archive')
      */
     set: function(path, value) {
       if (typeof(path) !== 'string') {
@@ -85,7 +85,7 @@
      * Uses caching strategy 'ALL'.
      *
      * Parameters:
-     *   path
+     *   path - Path to enable caching for
      */
     enable: function(path) {
       this.set(path, 'ALL');
@@ -100,7 +100,7 @@
      * successfully pushed to the remote).
      *
      * Parameters:
-     *   path
+     *   path - Path to disable caching for
      */
     disable: function(path) {
       this.set(path, 'FLUSH');
@@ -131,7 +131,7 @@
      * with a caching strategy set.
      *
      * Parameters:
-     *   path
+     *   path - Path to retrieve setting for
      **/
     checkPath: function(path) {
       if (this._rootPaths[path] !== undefined) {
