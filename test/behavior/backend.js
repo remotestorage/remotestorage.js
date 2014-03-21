f4f916f17f4f9ef869deaccb9645901611be1c30
Michiel de Jong <michiel@unhosted.org>
2014-03-21 10:20:26 +0000

    check stopWaitingForToken is a function

--- test/behavior/backend.js
+++ test/behavior/backend.js
@@ -6,6 +6,12 @@ define([], function() {
         run: function(env, test) {
           test.assert(env.client.connected, false);
         }
+      },
+      {
+        desc: "stopWaitingForToken is a function",
+        run: function(env, test) {
+          test.assert(env.client.stopWaitingForToken(), undefined);
+        }
       }
     ]
   };
