44756bb1e915ccd7fb7829cba6399e7bf12d2d77
Michiel@unhosted <michiel@unhosted.org>
2014-04-25 15:26:39 +0300

    introduce softMaxAge and make it the default with maxAge of 2*syncInterval

--- src/cachinglayer.js
+++ src/cachinglayer.js
@@ -111,22 +111,28 @@
 
   var methods = {
 
-    get: function(path, maxAge) {
+    get: function(path, maxAge, softMaxAge) {
       var promise = promising();
 
       this.getNodes([path]).then(function(objs) {
         var node = getLatest(objs[path]);
         if ((typeof(maxAge) === 'number') && isOutdated(node, maxAge)) {
           remoteStorage.sync.queueGetRequest(path, promise);
-        }
-
-        if (node) {
+        } else if (node) {
           promise.fulfill(200, node.body || node.itemsMap, node.contentType);
         } else {
           promise.fulfill(404);
         }
       }.bind(this), function(err) {
-        promise.reject(err);
+        if (softMaxAge) {
+          if (node) {
+            promise.fulfill(200, node.body || node.itemsMap, node.contentType);
+          } else {
+            promise.fulfill(404);
+          }
+        } else {
+          promise.reject(err);
+        }
       }.bind(this));
 
       return promise;
