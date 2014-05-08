47f8c07ec2c658915cb39b0c4e18eb103daa4e0b
Michiel de Jong <michiel@unhosted.org>
2014-05-08 12:33:17 +0000

    manual rebase of arpa2:browser-implied-authentication

--- src/wireclient.js
+++ src/wireclient.js
@@ -209,7 +209,9 @@
       var reqType;
       var self = this;
 
-      headers['Authorization'] = 'Bearer ' + token;
+      if (token !== IMPLIED_FAKE_TOKEN) {
+        headers['Authorization'] = 'Bearer ' + token;
+      }
 
       this._emit('wire-busy', {
         method: method,
