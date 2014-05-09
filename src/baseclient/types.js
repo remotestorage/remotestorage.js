ab9ebf2a8318dca2f03cfd0e9fc762f270783681
Michiel de Jong <michiel@unhosted.org>
2014-05-09 09:18:22 +0000

    add test for default type URI

--- src/baseclient/types.js
+++ src/baseclient/types.js
@@ -138,7 +138,7 @@
     },
 
     _defaultTypeURI: function(alias) {
-      return 'http://remotestorage.io/spec/modules/' + this.moduleName + '/' + alias;
+      return 'http://remotestorage.io/spec/modules/' + encodeURIComponent(this.moduleName) + '/' + encodeURIComponent(alias);
     },
 
     _attachType: function(object, alias) {
--- test/unit/baseclient/types-suite.js
+++ test/unit/baseclient/types-suite.js
@@ -9,14 +9,25 @@ define([], function() {
     desc: "Type and schema handling",
     setup: function(env, test) {
       global.RemoteStorage = function() {};
-      RemoteStorage.BaseClient = function() {};
-      RemoteStorage.BaseClient.prototype.extend = function() {};
+      RemoteStorage.BaseClient = function() {
+        this.moduleName = 'foo';
+      };
+      RemoteStorage.BaseClient.prototype.extend = function(obj) {
+        console.log('extending', obj);
+        for (var field in obj) {
+          this[field] = obj[field];
+        }
+        return this;
+      };
       require('./src/baseclient/types');
       if (global.rs_types) {
         RemoteStorage.BaseClient.Types = global.rs_types;
       } else {
         global.rs_types = RemoteStorage.BaseClient.Types;
       }
+      env.storage = new RemoteStorage();
+      env.client = new RemoteStorage.BaseClient(env.storage, '/foo/');
+      console.log('client', env.client);
       test.done();
     },
 
@@ -41,6 +52,22 @@ define([], function() {
           test.assertTypeAnd(barResult['http://foo/b'], 'undefined');
           test.assertTypeAnd(barResult['http://bar/c'], 'object');
 
+          test.done();
+        }
+      },
+
+      {
+        desc: "default @context is http://remotestorage.io/spec/modules/{module}/{type}",
+        run: function(env, test) {
+          var obj = {
+            some: 'value'
+          };
+          env.client._attachType(obj, 'ba/F');
+          console.log('obj', obj);
+          test.assertAnd(obj, {
+            some: 'value',
+            '@context': 'http://remotestorage.io/spec/modules/foo/ba%2FF'
+          });
           test.done();
         }
       }
