87f8380e3dfc0e3095533dd06f10b762186a5600
Michiel de Jong <michiel@unhosted.org>
2014-09-14 16:42:50 +0200

    only define RemoteStorage.prototype.util if not defined yet (fixes wireclient test)

--- src/util.js
+++ src/util.js
@@ -187,10 +187,12 @@
 
   };
 
-  Object.defineProperty(RemoteStorage.prototype, 'util', {
-    get: function() {
-      console.log('DEPRECATION WARNING: remoteStorage.util was moved to RemoteStorage.util');
-      return RemoteStorage.util;
-    }
-  });
+  if (!RemoteStorage.prototype.util) {
+    Object.defineProperty(RemoteStorage.prototype, 'util', {
+      get: function() {
+        console.log('DEPRECATION WARNING: remoteStorage.util was moved to RemoteStorage.util');
+        return RemoteStorage.util;
+      }
+    });
+  }
 })();
