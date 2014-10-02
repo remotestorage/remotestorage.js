c5d78f7da7e7171a567f5b5604dfd4a3ecccb62a
Nick Jennings <nick@silverbucket.net>
2014-10-02 19:43:31 +0200

    these functions moved to node-main.js

--- src/nodejs_ext.js
+++
@@ -1,7 +0,0 @@
-(function (global) {
-  global.XMLHttpRequest = require('xhr2');
-
-  RemoteStorage.WireClient.readBinaryData = function (content, mimeType, callback) {
-    callback(content);
-  };
-}(global));
