e3c825bf09c3f85c94ec0329bbdd1e79e4164af3
Nick Jennings <nick@silverbucket.net>
2014-10-02 17:51:43 +0200

    add a  method to check for active features, also fireready when there is no Authorize feature, we will never get it otherwise. with tests.
    Conflicts:
    	src/remotestorage.js

--- src/remotestorage.js
+++ src/remotestorage.js
@@ -465,6 +465,10 @@
             fireReady();
             self._emit('connected');
           }
+
+          if (!this.hasFeature('Authorize')) {
+            this.remote.stopWaitingForToken();
+          }
         }
 
         this._collectCleanupFunctions();
@@ -608,7 +612,31 @@
       });
     },
 
-    localStorageAvailable: function () {
+    /**
+     * Method: hasFeature
+     *
+     * Checks whether a feature is enabled or not within remoteStorage.
+     * Returns a boolean.
+     *
+     * Parameters:
+     * name - Capitalized name of the feature. ie. Authorize, or IndexedDB
+     *
+     * Example:
+     *     if (remoteStorage.hasFeature('LocalStorage')) {
+     *       console.log('LocalStorage is enabled!');
+     *     }
+     *
+     */
+    hasFeature: function (feature) {
+      for (var i = this.features.length - 1; i >= 0; i--) {
+        if (this.features[i].name === feature) {
+          return this.features[i].supported;
+        }
+      }
+      return false;
+    },
+
+    localStorageAvailable: function() {
       try {
         return !!global.localStorage;
       } catch(error) {
