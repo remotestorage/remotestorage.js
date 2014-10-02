5cc64669bff9b4324b74801ce3a68dffa1cf9039
Nick Jennings <nick@silverbucket.net>
2014-10-02 19:40:12 +0200

    add simple node init script which will be automatically called when the remotestoragejs package is required

---
+++ node-main.js
@@ -0,0 +1,32 @@
+(function (global) {
+  global.XMLHttpRequest = require('xhr2');
+
+  global.RemoteStorage = require('./src/remotestorage.js');
+  global.Promise = require('bluebird');
+  require('./src/util.js');
+  require('./src/eventhandling.js');
+  require('./src/wireclient.js');
+  require('./src/discover.js');
+  require('./src/cachinglayer.js');
+  require('./src/indexeddb.js');
+  require('./src/inmemorystorage.js');
+  require('./src/localstorage.js');
+  require('./src/sync.js');
+  require('./src/access.js');
+  require('./src/caching.js');
+  require('./src/modules.js');
+  require('./src/baseclient.js');
+  require('./src/baseclient/types.js');
+  require('./src/i18n.js');
+  require('./src/env.js');
+  require('./src/googledrive.js');
+  require('./src/dropbox.js');
+  require('./src/nodejs_ext.js');
+
+  RemoteStorage.WireClient.readBinaryData = function (content, mimeType, callback) {
+    callback(content);
+  };
+
+  module.exports = RemoteStorage;
+
+}(global));
