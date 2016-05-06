74d13dc290a757ff92cf04db4fefe622ad2899f0
Garret Alfert <alfert@wevelop.de>
2016-05-06 16:53:44 +0200

    Spec for cleanup functions not bloating up
    This tests the bugfix introduced in 906b3cd.

--- test/unit/remotestorage-suite.js
+++ test/unit/remotestorage-suite.js
@@ -22,7 +22,6 @@ define(['bluebird', 'requirejs', 'tv4'], function (Promise, requirejs, tv4) {
   }
 
   function fakeRequest(path) {
-    console.log('GET CALLED');
     if (path === '/testing403') {
       return Promise.resolve({statusCode: 403});
     } else {
@@ -225,6 +224,40 @@ define(['bluebird', 'requirejs', 'tv4'], function (Promise, requirejs, tv4) {
           });
 
           env.rs.disconnect();
+        }
+      },
+
+      {
+        desc: "cleanup functions don't bloat up on repeated initialization",
+        run: function(env, test) {
+          var initsCalled = 0;
+
+          // Mock feature to be loaded on initialization
+          RemoteStorage.Sync = {
+            _rs_init: function() {},
+            _rs_cleanup: function() {}
+          };
+
+          var loadedHandler = function() {
+            initsCalled++;
+
+            if (initsCalled === 1) { // ignore first init, as that's from original initialization
+              test.assertAnd(env.rs._cleanups.length, 0);
+            } else {
+              test.assertAnd(env.rs._cleanups.length, 1);
+            }
+
+            if (initsCalled === 2) {
+              env.rs._init();
+            } else if (initsCalled === 3) {
+              env.rs.removeEventListener('features-loaded', loadedHandler);
+              delete RemoteStorage.Sync;
+              test.done();
+            }
+          }
+
+          env.rs.on('features-loaded', loadedHandler);
+          env.rs._init();
         }
       },
 
