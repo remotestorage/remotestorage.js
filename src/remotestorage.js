ccf9d765960e6fae1233dd02b1c17d3e4b58efaf
lesion <lesion@autistici.org>
2016-09-24 01:28:21 +0200

    move SyncedGetPutDelete to it's own file

--- src/remotestorage.js
+++ src/remotestorage.js
@@ -30,69 +30,8 @@
     return Promise.resolve(r);
   }
 
-  function shareFirst(path) {
-    return ( this.backend === 'dropbox' &&
-             path.match(/^\/public\/.*[^\/]$/) );
-  }
 
-  var SyncedGetPutDelete = {
-    get: function (path, maxAge) {
-      var self = this;
-      if (this.local) {
-        if (maxAge === undefined) {
-          if ((typeof this.remote === 'object') &&
-               this.remote.connected && this.remote.online) {
-            maxAge = 2*this.getSyncInterval();
-          } else {
-            RemoteStorage.log('Not setting default maxAge, because remote is offline or not connected');
-            maxAge = false;
-          }
-        }
-        var maxAgeInvalid = function (maxAge) {
-          return maxAge !== false && typeof(maxAge) !== 'number';
-        };
-
-        if (maxAgeInvalid(maxAge)) {
-          return Promise.reject('Argument \'maxAge\' must be false or a number');
-        }
-        return this.local.get(path, maxAge, this.sync.queueGetRequest.bind(this.sync));
-      } else {
-        return this.remote.get(path);
-      }
-    },
-
-    put: function (path, body, contentType) {
-      if (shareFirst.bind(this)(path)) {
-        return SyncedGetPutDelete._wrapBusyDone.call(this, this.remote.put(path, body, contentType));
-      }
-      else if (this.local) {
-        return this.local.put(path, body, contentType);
-      } else {
-        return SyncedGetPutDelete._wrapBusyDone.call(this, this.remote.put(path, body, contentType));
-      }
-    },
-
-    'delete': function (path) {
-      if (this.local) {
-        return this.local.delete(path);
-      } else {
-        return SyncedGetPutDelete._wrapBusyDone.call(this, this.remote.delete(path));
-      }
-    },
-
-    _wrapBusyDone: function (result) {
-      var self = this;
-      this._emit('wire-busy');
-      return result.then(function (r) {
-        self._emit('wire-done', { success: true });
-        return Promise.resolve(r);
-      }, function (err) {
-        self._emit('wire-done', { success: false });
-        return Promise.reject(err);
-      });
-    }
-  };
-
+  var SyncedGetPutDelete = require('./syncedgetputdelete')
   /**
    * Class: RemoteStorage
    *
