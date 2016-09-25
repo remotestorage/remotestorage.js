908dcbf5c3255ae8b36bf923ec88a40be3ce069c
lesion <lesion@autistici.org>
2016-09-25 12:30:58 +0200

    npm script to run webpack in dev/production mode (start/dist)

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
+var webpack = require('webpack');
+var isProd = (process.env.NODE_ENV === 'production');
+
+var plugins = isProd ? [new webpack.optimize.UglifyJsPlugin({minimize: true})] : []
+
 module.exports = {
   entry: './src/init.js',
-  devtool: 'source-map',
+  devtool: !isProd && 'source-map',
   output: {
     filename: __dirname + '/build/build.js',
-    library: 'remotestoragejs',
+    library: 'RemoteStorage',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
   externals: [ 'xmlhttprequest' ],
   resolve: {
     extensions: ['', '.js']
-  }
+  },
+  plugins: plugins
 };
