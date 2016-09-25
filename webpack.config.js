d99317c5a340495bd8ba6fe2299ea8c833c2566c
lesion <lesion@autistici.org>
2016-09-25 12:36:31 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 var webpack = require('webpack');
 var isProd = (process.env.NODE_ENV === 'production');
 
+// minimize only in production
 var plugins = isProd ? [new webpack.optimize.UglifyJsPlugin({minimize: true})] : []
 
 module.exports = {
   entry: './src/init.js',
+  // source map not in production
   devtool: !isProd && 'source-map',
   output: {
     filename: __dirname + '/build/build.js',
