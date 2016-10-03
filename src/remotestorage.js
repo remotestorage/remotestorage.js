d37416d1670bded74bad94587cf658dda674551f
theWebalyst <github@happybeing.com>
2016-10-03 17:49:23 +0100

    Adjust whitespace to match other code

--- src/remotestorage.js
+++ src/remotestorage.js
@@ -576,8 +576,8 @@
           RemoteStorage.GoogleDrive._rs_init(this);
         }
       } else if (type === 'safenetwork' && (typeof this.safenetwork === 'undefined' ||
-          this.safenetwork.clientId !== keys.clientId)) {
-        RemoteStorage.SafeNetwork._rs_init(this);
+                                              this.safenetwork.clientId !== keys.clientId)) {
+          RemoteStorage.SafeNetwork._rs_init(this);
       } else {
         delete this.apiKeys[type];
       }
