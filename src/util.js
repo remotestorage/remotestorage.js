eb75e07ad6db1db05df2b2001590f06cf82bca4c
lesion <lesion@autistici.org>
2017-04-11 18:56:27 +0200

    remove defer wrapper

--- src/util.js
+++ src/util.js
@@ -1,21 +1,6 @@
 // Reusable utility functions
 
 
-// wrapper to implement defer() functionality
-Promise.defer = function () {
-  var resolve, reject;
-  var promise = new Promise(function() {
-    resolve = arguments[0];
-    reject = arguments[1];
-  });
-  return {
-      resolve: resolve,
-    reject: reject,
-    promise: promise
-  };
-};
-
-
   /**
    * Function: fixArrayBuffers
    *
