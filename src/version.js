9b0793e99ef6c558030b58b83718da6a95d3d10f
Sebastian Kippe <sebastian@kip.pe>
2017-08-06 14:34:54 +0200

    1.0.0-alpha5
    Forgot to add the build and update the version in package.json for
    alpha4. Usually we'd use `npm version`, but it doesn't deal with
    prerelease suffixes like ours.

--- src/version.js
+++ src/version.js
@@ -4,7 +4,7 @@ RemoteStorage.version = RemoteStorage.prototype.version = {
   product: 1,
   major: 0,
   minor: 0,
-  postfix: 'alpha3'
+  postfix: 'alpha5'
 };
 
 RemoteStorage.version.toString = function () {
