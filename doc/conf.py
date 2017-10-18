ba89dc2008616d1fb11a187484596cb7af0ec29f
Sebastian Kippe <sebastian@kip.pe>
2017-10-18 21:48:20 +0200

    Add autosectionlabel extension
    Enables :ref:`Some title` for linking sections

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_js', 'sphinx_issues']
+extensions = [
+  'sphinx.ext.autosectionlabel',
+  'sphinx_js',
+  'sphinx_issues'
+]
+
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
