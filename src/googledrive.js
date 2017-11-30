6cb604dd104d2a7b3769894c1ab7ad7030186991
Garret Alfert <alfert@wevelop.de>
2017-11-30 04:01:41 +0100

    Extract parsing JSON from localStorage into method

--- src/googledrive.js
+++ src/googledrive.js
@@ -33,6 +33,7 @@ const isFolder = util.isFolder;
 const cleanPath = util.cleanPath;
 const shouldBeTreatedAsBinary = util.shouldBeTreatedAsBinary;
 const readBinaryData = util.readBinaryData;
+const getJSONFromLocalStorage = util.getJSONFromLocalStorage;
 
 let hasLocalStorage;
 
@@ -142,12 +143,7 @@ const GoogleDrive = function (remoteStorage, clientId) {
   hasLocalStorage = util.localStorageAvailable();
 
   if (hasLocalStorage){
-    let settings;
-    try {
-      settings = JSON.parse(localStorage.getItem(SETTINGS_KEY));
-    } catch(e) {
-      // no settings stored
-    }
+    const settings = getJSONFromLocalStorage(SETTINGS_KEY);
     if (settings) {
       this.configure(settings);
     }
