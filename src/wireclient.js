3ac224ecfbaf3c76308919159e7496223664770a
P. Douglas Reeder <reeder.29@gmail.com>
2018-10-25 22:31:50 -0400

    wireclient uses fetch() if available, instead of XHR

--- src/wireclient.js
+++ src/wireclient.js
@@ -7,7 +7,7 @@ const Authorize = require('./authorize');
 const config = require('./config');
 
 /**
- * This file exposes a get/put/delete interface on top of XMLHttpRequest.
+ * This file exposes a get/put/delete interface on top of fetch() or XMLHttpRequest.
  * It requires to be configured with parameters about the remotestorage server to
  * connect to.
  * Each instance of WireClient is always associated with a single remotestorage
@@ -490,8 +490,81 @@ WireClient.prototype = {
 WireClient.isArrayBufferView = isArrayBufferView;
 
 // Shared request function used by WireClient, GoogleDrive and Dropbox.
-// TODO: Should we use fetch ?
 WireClient.request = function (method, url, options) {
+  if (typeof fetch !== 'undefined') {
+    return WireClient._fetchRequest(method, url, options);
+  } else if (typeof XMLHttpRequest !== 'undefined') {
+    return WireClient._xhrRequest(method, url, options);
+  } else {
+    log('[WireClient] no support for HTTP requests');
+    return Promise.reject('[WireClient] no support for HTTP requests');
+  }
+};
+
+/** options includes body, headers and responseType */
+WireClient._fetchRequest = function (method, url, options) {
+  var syntheticXhr;
+  var responseHeaders = {};
+  var abortController;
+  if (typeof AbortController === 'function') {
+    abortController = new AbortController();
+  }
+  var networkPromise = fetch(url, {
+    method: method,
+    headers: options.headers,
+    body: options.body,
+    signal: abortController ? abortController.signal : undefined
+  }).then(function (response) {
+    log('[WireClient fetch]', response);
+
+    response.headers.forEach(function (value, headerName) {
+      responseHeaders[headerName.toUpperCase()] = value;
+    });
+
+    syntheticXhr = {
+      readyState: 4,
+      status: response.status,
+      statusText: response.statusText,
+      response: undefined,
+      getResponseHeader: function (headerName) {
+        return responseHeaders[headerName.toUpperCase()] || null;
+      },
+      // responseText: 'foo',
+      responseType: options.responseType,
+      responseURL: url,
+    };
+    switch (options.responseType) {
+      case 'arraybuffer':
+        return response.arrayBuffer();
+      case 'blob':
+        return response.blob();
+      case 'json':
+        return response.json();
+      case '':
+      case 'text':
+        return response.text();
+      default:   // document
+        throw new Error("responseType 'document' is not currently supported using fetch");
+    }
+  }).then(function (processedBody) {
+    syntheticXhr.response = processedBody;
+    return syntheticXhr;
+  });
+
+  var timeoutPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      log('[WireClient] request timed out', url);
+      reject('timeout');
+      if (abortController) {
+        abortController.abort();
+      }
+    }, config.requestTimeout);
+  });
+
+  return Promise.race([networkPromise, timeoutPromise]);
+};
+
+WireClient._xhrRequest = function (method, url, options) {
   return new Promise ((resolve, reject) => {
 
     log('[WireClient]', method, url);
@@ -554,7 +627,7 @@ WireClient._rs_init = function (remoteStorage) {
 };
 
 WireClient._rs_supported = function () {
-  return !! XMLHttpRequest;
+  return typeof fetch !== 'undefined' || typeof XMLHttpRequest !== 'undefined';
 };
 
 WireClient._rs_cleanup = function () {
--- test/unit/node-wireclient-suite.js
+++ test/unit/node-wireclient-suite.js
@@ -5,116 +5,234 @@ define(['./src/wireclient', './src/remotestorage'], function (WireClient, Remote
   var suites = [];
 	var oldReadBinaryData;
 
-  suites.push({
-    name: "WireClient NodeJS",
-    desc: "Low-level remotestorage client used in NodeJS",
-    setup: function(env, test) {
+  function setup(env, test) {
+    test.assertType(RemoteStorage, 'function');
+  }
 
-      test.assertType(RemoteStorage, 'function');
-    },
+  function takedown(env, test) {
+    if (typeof WireClient !== 'undefined') {
+      WireClient.readBinaryData = oldReadBinaryData;
+    }
+    test.done();
+  }
 
-    takedown: function(env, test) {
-      if (typeof WireClient !== 'undefined') {
-         WireClient.readBinaryData = oldReadBinaryData;
-      }
-      test.done();
-    },
+  function beforeEach(env, test) {
+    env.rs = new RemoteStorage();
+    env.connectedClient = new WireClient(env.rs);
+    env.baseURI = 'https://example.com/storage/test';
+    env.token = 'foobarbaz';
+    env.connectedClient.configure({
+      href: env.baseURI,
+      token: env.token
+    });
+    test.done();
+  }
 
-    beforeEach: function(env, test) {
-      global.XMLHttpRequest = function() {
-        XMLHttpRequest.instances.push(this);
-        this._headers = {};
-        this._responseHeaders = {};
-      };
-      XMLHttpRequest.instances = [];
-      XMLHttpRequest.prototype = {
-        open: function() {
-          this._open = Array.prototype.slice.call(arguments);
-        },
-        send: function() {
-          this._send = Array.prototype.slice.call(arguments);
-        },
-        setRequestHeader: function(key, value) {
-          this._headers[key] = value;
-        },
-        getResponseHeader: function(key) {
-          return this._responseHeaders[key];
-        }
-      };
-      ['load', 'abort', 'error'].forEach(function(cb) {
-        Object.defineProperty(XMLHttpRequest.prototype, 'on' + cb, {
-          configurable: true,
-          set: function(f) {
-            this['_on' + cb] = f;
-          }
-        });
-      });
-      env.rs = new RemoteStorage();
-      env.connectedClient = new WireClient(env.rs);
-      env.baseURI = 'https://example.com/storage/test';
-      env.token = 'foobarbaz';
-      env.connectedClient.configure({
-        href: env.baseURI,
-        token: env.token
-      });
-      test.done();
-    },
+  function beforeEachXHR(env, test) {
+    beforeEach(env, test);
 
-    afterEach: function(env, test) {
-      delete global.XMLHttpRequest;
-      delete env.connectedClient;
-      test.done();
-    },
-
-    tests: [
-      {
-        desc: "GET requests for binary data respond with the proper content",
-        run: function(env, test) {
-          env.connectedClient.get('/foo/bar').
-            then(function (r) {
-              test.assertAnd(r.statusCode, 200);
-              test.assertAnd(r.body, 'response content');
-              test.assert(r.contentType, 'image/png; charset=binary');
-            }, function (err) {
-              test.result(false, err);
-            });
-          var req = XMLHttpRequest.instances.shift();
-          req._responseHeaders['Content-Type'] = 'image/png; charset=binary';
-          req.status = 200;
-          req.response = 'response content';
-          req._onload();
-        }
+    global.XMLHttpRequest = function() {
+      XMLHttpRequest.instances.push(this);
+      this._headers = {};
+      this._responseHeaders = {};
+    };
+    XMLHttpRequest.instances = [];
+    XMLHttpRequest.prototype = {
+      open: function() {
+        this._open = Array.prototype.slice.call(arguments);
       },
-
-      {
-        desc: "GET requests for text data respond with the proper content",
-        run: function(env, test) {
-          function string2ArrayBuffer(str) {
-            var buf = new ArrayBuffer(str.length); // assuming str only contains 1-byte UTF characters
-            var bufView = new Uint8Array(buf);
-            for (var i=0, strLen=str.length; i<strLen; i++) {
-              bufView[i] = str.charCodeAt(i);
-            }
-            return buf;
-          }
-
-          env.connectedClient.get('/foo/bar').
-            then(function (r) {
-              test.assertAnd(r.statusCode, 200);
-              test.assertAnd(r.body, 'response content');
-              test.assert(r.contentType, 'text/plain');
-            }, function (err) {
-              test.result(false, err);
-            });
-
-          var req = XMLHttpRequest.instances.shift();
-          req._responseHeaders['Content-Type'] = 'text/plain';
-          req.status = 200;
-          req.response = string2ArrayBuffer('response content');
-          req._onload();
-        }
+      send: function() {
+        this._send = Array.prototype.slice.call(arguments);
+      },
+      setRequestHeader: function(key, value) {
+        this._headers[key] = value;
+      },
+      getResponseHeader: function(key) {
+        return this._responseHeaders[key] || null;
       }
-    ]
+    };
+    ['load', 'abort', 'error'].forEach(function(cb) {
+      Object.defineProperty(XMLHttpRequest.prototype, 'on' + cb, {
+        configurable: true,
+        set: function(f) {
+          this['_on' + cb] = f;
+        }
+      });
+    });
+
+    global.mockRequestSuccess = function (param) {
+      var req = XMLHttpRequest.instances.shift();
+      req._responseHeaders = param.responseHeaders || {};
+      req.status = param.status;
+      req.response = param.arrayBuffer;
+      req._onload();
+    };
+
+    global.mockRequestFail = function (errMsg) {
+      var req = XMLHttpRequest.instances.shift();
+      req._onerror(errMsg);
+    };
+  }
+
+  function beforeEachFetch(env, test) {
+    beforeEach(env, test);
+
+    var fetchesData = [];
+
+    global.fetch = function (url, init) {
+      init = init || {};
+      return new Promise(function (resolve, reject) {
+        fetchesData.push({
+          url: url,
+          method: init.method || 'GET',
+          requestHeaders: init.headers || {},
+          resolve: resolve,
+          reject: reject});
+      });
+    };
+
+    global.mockRequestSuccess = function (param) {
+      var fetchData = fetchesData.shift();
+
+      var responseHeaders = {   // mock Headers obj
+        _headers: param.responseHeaders || {},   // POJSO
+        forEach: function (callback, thisArg) {
+          var thisObj = thisArg || responseHeaders;
+          for (headerName in responseHeaders._headers) {
+            callback.call(thisObj, responseHeaders._headers[headerName], headerName, responseHeaders);
+          }
+        }
+      };
+      var response = {   // mock Response obj
+        headers: responseHeaders,
+        ok: param.status >= 200 && param.status < 300,
+        redirected: false,
+        status: param.status,
+        statusText: param.statusText || '',
+        type: param.corsResponseType || 'basic',
+        url: fetchData.url,
+        useFinalURL: true,
+        body: function () {   // getter for ReadableStream
+          throw new Error("not implemented in mock");
+        },
+        bodyUsed: false,
+        arrayBuffer: function () {
+          return Promise.resolve(param.arrayBuffer);
+        },
+        blob: function () {
+          throw new Error("not implemented in mock");
+        },
+        text: function () {
+          throw new Error("not implemented in mock");
+        },
+        json: function () {
+          throw new Error("not implemented in mock");
+        }
+      };
+
+      fetchData.resolve(response);
+    };
+
+    global.mockRequestFail = function (errMsg) {
+      var fetchData = fetchesData.shift();
+      fetchData.reject(errMsg);
+    };
+  }
+
+  function afterEach(env, test) {
+    delete global.XMLHttpRequest;
+    delete global.fetch;
+    delete global.getMockRequestMethod;
+    delete global.getMockRequestUrl;
+    delete global.getMockRequestHeader;
+    delete global.mockRequestSuccess;
+    delete global.mockRequestFail;
+    delete env.connectedClient;
+    test.done();
+  }
+
+  var tests = [
+    {
+      desc: "GET requests for binary data respond with the proper content",
+      run: function(env, test) {
+        function ArrayBufferMock(str) {
+          return {
+            iAmA: 'ArrayBufferMock',
+            content: str
+          };
+        }
+
+        env.connectedClient.get('/foo/bar').
+        then(function (r) {
+          test.assertAnd(r.statusCode, 200);
+          test.assertAnd(r.body, {
+            iAmA: 'ArrayBufferMock',
+            content: 'response content'
+          });
+          test.assert(r.contentType, 'image/png; charset=binary');
+        }, function (err) {
+          test.result(false, err);
+        });
+        mockRequestSuccess({
+          responseHeaders: {'Content-Type': 'image/png; charset=binary'},
+          status: 200,
+          arrayBuffer: new ArrayBufferMock('response content')
+        });
+      }
+    },
+
+    {
+      desc: "GET requests for text data respond with the proper content",
+      run: function(env, test) {
+        function string2ArrayBuffer(str) {
+          var buf = new ArrayBuffer(str.length); // assuming str only contains 1-byte UTF characters
+          var bufView = new Uint8Array(buf);
+          for (var i=0, strLen=str.length; i<strLen; i++) {
+            bufView[i] = str.charCodeAt(i);
+          }
+          return buf;
+        }
+
+        env.connectedClient.get('/foo/bar').
+        then(function (r) {
+          test.assertAnd(r.statusCode, 200);
+          test.assertAnd(r.body, 'response content');
+          test.assert(r.contentType, 'text/plain');
+        }, function (err) {
+          test.result(false, err);
+        });
+
+        mockRequestSuccess({
+          responseHeaders: {'Content-Type': 'text/plain'},
+          status: 200,
+          arrayBuffer: string2ArrayBuffer('response content')
+        });
+      }
+    }
+  ];
+
+  suites.push({
+    name: "WireClient NodeJS (using fetch())",
+    desc: "Low-level remotestorage client used in NodeJS",
+    setup: setup,
+    beforeEach: beforeEachFetch,
+    takedown: takedown,
+
+    afterEach: afterEach,
+
+    tests: tests
+  });
+
+  suites.push({
+    name: "WireClient NodeJS (using XHR)",
+    desc: "Low-level remotestorage client used in NodeJS",
+    setup: setup,
+    beforeEach: beforeEachXHR,
+    takedown: takedown,
+
+    afterEach: afterEach,
+
+    tests: tests
   });
 
   return suites;
