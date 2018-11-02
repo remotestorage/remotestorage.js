3675e7f0243e45567e58e41417d1be21b3b953b5
Garret Alfert <alfert@wevelop.de>
2018-11-02 12:04:57 +0100

    Make removeQuotes only remove surrounding quotes

--- src/googledrive.js
+++ src/googledrive.js
@@ -102,7 +102,7 @@ function googleDrivePath (path) {
  * @private
  */
 function removeQuotes (string) {
-  return string.substring(1, string.length-1);
+  return string.replace(/^["'](.*)["']$/, "$1");
 }
 
 /**
