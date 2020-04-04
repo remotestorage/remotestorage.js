04205b0ebba3fe79d783fc24a87d5b229450fde5
Johannes Millan <johannes.millan@gmail.com>
2020-04-04 23:26:42 +0200

    Port log to typescript

---
+++ src/log.ts
@@ -0,0 +1,18 @@
+import * as config from './config';
+
+/**
+ * Log using console.log, when remoteStorage logging is enabled.
+ *
+ * You can enable logging with ``RemoteStorage#enableLog``.
+ *
+ * (In node.js you can also enable logging during remoteStorage object
+ * creation. See: {@link RemoteStorage}).
+ */
+function log() {
+  if (config.logging) {
+    // eslint-disable-next-line no-console
+    console.log.apply(console, arguments);
+  }
+}
+
+module.exports = log;
