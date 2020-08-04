3dc8e1d421b17adcc298f5b11beb876b3a7a9a41
Sebastian Kippe <sebastian@kip.pe>
2020-08-04 22:05:48 +0200

    Port tests to Mocha and Chai
    Jaribu is a bit dated now, and it's still using AMD modules.
    
    This sets up Mocha as a test runner and Chai for assertions, as well as
    ES modules/imports. A first working test file is included.
    
    The new tests end in `.test.js` instead of `-suite.js`, and they can be
    run via `npm run test:mocha` for now.

---
+++ test/unit/modules.test.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import RemoteStorage from './build/remotestorage.js';
+
+describe('RemoteStorage modules', function() {
+  const env = {
+    rs: new RemoteStorage()
+  }
+
+  before(function() {
+    RemoteStorage.prototype.remote = { connected: false };
+    RemoteStorage.BaseClient = function() {};
+  });
+
+  describe('addModule()', function() {
+    it('creates a module', function() {
+      env.rs.addModule({name: 'foo', builder: function() {
+        return { exports: { it: 'worked' } };
+      }});
+
+      expect(env.rs.foo.it).to.equal('worked');
+    });
+
+    it('allows hyphens in module names', function() {
+      env.rs.addModule({name: 'foo-bar', builder: function() {
+        return { exports: { it: 'worked' } };
+      }});
+
+      expect(env.rs.foo.it).to.equal('worked');
+    });
+
+    it('is called when passing a module to the RemoteStorage constructor', function() {
+      const rs = new RemoteStorage({modules: [{ name: 'bar', builder: function() {
+        return { exports: { it: 'worked' } };
+      }}]});
+      expect(rs.bar.it).to.equal('worked');
+    });
+  });
+})
