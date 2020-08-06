f2443375e15df19869a441180cf01f93d3b8a342
Sebastian Kippe <sebastian@kip.pe>
2020-08-06 20:29:58 +0200

    Formatting / syntax

--- test/unit/modules.test.ts
+++ test/unit/modules.test.ts
@@ -4,36 +4,41 @@ import 'mocha';
 import { expect } from 'chai';
 import RemoteStorage from '../../src/remotestorage';
 
-describe('RemoteStorage module initialization', function() {
+describe('RemoteStorage module initialization', () => {
   const env = {
     rs: new RemoteStorage()
   };
 
-  before(function() {
+  before(() => {
     RemoteStorage.prototype.remote = { connected: false };
   });
 
-  describe('addModule()', function() {
-    it('creates a module', function() {
-      env.rs.addModule({name: 'foo', builder: function() {
+  describe('addModule()', () => {
+    it('creates a module', () => {
+      env.rs.addModule({ name: 'foo', builder: function() {
         return { exports: { it: 'worked' } };
       }});
 
       expect(env.rs.foo.it).to.equal('worked');
     });
 
-    it('allows hyphens in module names', function() {
-      env.rs.addModule({name: 'foo-bar', builder: function() {
+    it('allows hyphens in module names', () => {
+      env.rs.addModule({ name: 'foo-bar', builder: function() {
         return { exports: { it: 'worked' } };
       }});
 
       expect(env.rs.foo.it).to.equal('worked');
     });
 
-    it('is called when passing a module to the RemoteStorage constructor', function() {
-      const rs = new RemoteStorage({modules: [{ name: 'bar', builder: function() {
-        return { exports: { it: 'worked' } };
-      }}]});
+    it('is called when passing a module to the RemoteStorage constructor', () => {
+      const rs = new RemoteStorage({ modules: [
+        {
+          name: 'bar', builder: function() {
+            return { exports: { it: 'worked' } };
+          }
+        }
+      ]});
+
       expect(rs.bar.it).to.equal('worked');
     });
   });
