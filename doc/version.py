329135849f7ea1e35fe2e3acd0565c2189a5b71e
Sebastian Kippe <sebastian@kip.pe>
2020-09-03 15:16:08 +0200

    2.0.0-alpha.1

--- doc/version.py
+++ doc/version.py
@@ -1,1 +1,1 @@
-__version__ = '2.0.0-alpha.0'
+__version__ = '2.0.0-alpha.1'
--- release/types/util.d.ts
+++ release/types/util.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="mocha" />
 /// <reference types="node" />
 import Global = NodeJS.Global;
 export declare const logError: (error: string | Error) => void;
