b06c2c884cc5603e6795d76bf2ce33889da6b0ac
Râu Cao <raucao@kip.pe>
2023-03-29 17:30:11 +0200

    Add specs for sync-done, use spies for event expectations

--- test/unit/sync.test.ts
+++ test/unit/sync.test.ts
@@ -1,5 +1,5 @@
 import 'mocha';
-import chai, {expect} from 'chai';
+import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import RemoteStorage from "../../src/remotestorage";
 import Sync from "../../src/sync";
@@ -8,11 +8,16 @@ import InMemoryStorage from "../../src/inmemorystorage";
 let rs, tasks;
 
 describe("Sync", () => {
+  const sandbox = sinon.createSandbox();
 
-  before(() => {
+  beforeEach(() => {
     rs = new RemoteStorage();
     rs.local = new InMemoryStorage();
     rs.sync = new Sync(rs);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
   });
 
   describe("#finishTask", () => {
@@ -42,23 +47,66 @@ describe("Sync", () => {
       rs.sync.doTasks = () => { return; }
     });
 
+    afterEach(() => {
+      rs.stopSync();
+    })
+
     describe("successfully completed", () => {
       it("emits 'sync-req-done' with the number of remaining tasks", async () => {
-        const eventHandler = evt => { expect(evt.tasksRemaining).to.equal(1); }
-        rs.on('sync-req-done', eventHandler);
+        const rsEmit = sinon.spy(rs, '_emit');
+
         await rs.sync.finishTask(tasks["/example/one"], false);
-        rs.removeEventListener('sync-req-done', eventHandler);
-        rs.stopSync();
+
+        expect(rsEmit.callCount).to.equal(1);
+        expect(rsEmit.getCall(0).args[0]).to.equal('sync-req-done');
+        expect(rsEmit.getCall(0).args[1]).to.have.property('tasksRemaining', 1);
+      });
+
+      describe("last task", () => {
+        beforeEach(() => {
+          rs.sync._tasks = { "/example/one": tasks["/example/one"] }
+        });
+
+        it("marks the sync as done", async () => {
+          await rs.sync.finishTask(tasks["/example/one"], false);
+          expect(rs.sync.done).to.be.true;
+        });
+
+        it("emits 'sync-done' with positive 'completed' status", async () => {
+          const rsEmit = sinon.spy(rs, '_emit');
+
+          await rs.sync.finishTask(tasks["/example/one"], false);
+
+          expect(rsEmit.callCount).to.equal(2);
+          expect(rsEmit.getCall(1).args[0]).to.equal('sync-done');
+          expect(rsEmit.getCall(1).args[1]).to.have.property('completed', true);
+        });
       });
     });
 
     describe("failed to complete", () => {
       it("emits 'sync-req-done' with the number of remaining tasks", async () => {
-        const eventHandler = evt => { expect(evt.tasksRemaining).to.equal(2); }
-        rs.on('sync-req-done', eventHandler);
+        const rsEmit = sinon.spy(rs, '_emit');
+
         await rs.sync.finishTask(tasks["/example/two"], false);
-        rs.removeEventListener('sync-req-done', eventHandler);
-        rs.stopSync();
+
+        expect(rsEmit.callCount).to.equal(3); // 'error', 'sync-req-done', 'sync-done'
+        expect(rsEmit.getCall(1).args[0]).to.equal('sync-req-done');
+        expect(rsEmit.getCall(1).args[1]).to.have.property('tasksRemaining', 2);
+      });
+
+      it("marks the sync as done", async () => {
+        await rs.sync.finishTask(tasks["/example/two"], false);
+        expect(rs.sync.done).to.be.true;
+      });
+
+      it("emits 'sync-done' with negative 'completed' status", async () => {
+        const rsEmit = sinon.spy(rs, '_emit');
+
+        await rs.sync.finishTask(tasks["/example/two"], false);
+
+        expect(rsEmit.getCall(2).args[0]).to.equal('sync-done');
+        expect(rsEmit.getCall(2).args[1]).to.have.property('completed', false);
       });
     });
   });
