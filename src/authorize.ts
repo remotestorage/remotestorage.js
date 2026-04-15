6294531849347fa95f616e0bcad048e328bed54c
Râu Cao <raucao@kosmos.org>
2026-04-15 11:55:44 +0400

    Update eslint deps, fix lint errors

--- src/authorize.ts
+++ src/authorize.ts
@@ -361,7 +361,7 @@ export class Authorize {
       );
 
       switch (xhr.status) {
-        case 200:
+        case 200: {
           log(`[Authorize] access token good for ${xhr?.response?.expires_in} seconds`);
           const settings = {
             token: xhr?.response?.access_token,
@@ -378,6 +378,7 @@ export class Authorize {
           }
           sessionStorage.removeItem('remotestorage:codeVerifier');
           break;
+        }
         default:
           if (typeof remoteStorage._forgetPendingScope === 'function') {
             remoteStorage._forgetPendingScope();
